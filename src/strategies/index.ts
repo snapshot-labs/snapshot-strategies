472d2893c19cd942ffbd706755763861d53a294f
G2 <bracy.gawain2014@gmail.com>
2021-08-17 11:05:41 -0400

    [poap-with-weight] Poap with weight (#29)
    * POAP-with-weight
    
    * imported changes
    
    * Updated according
    
    @bonustrack asked for revisions
    - import aboutFile and examplesFIle
    - Remove change to package-lock.json
    
    * Update src/strategies/poap-with-weight/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update src/strategies/poap-with-weight/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update src/strategies/poap-with-weight/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update src/strategies/poap-with-weight/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update index.ts
    
    * Update src/strategies/poap-with-weight/examples.json
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update src/strategies/poap-with-weight/README.md
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * API endpoint update
    
    * [anti-whale] Add anti-whale strategy (#5)
    
    * Add anti-whale strategy
    
    * remove param strategy.network
    use same top stratey network for the inner strategy
    
    * Update src/strategies/anti-whale/index.ts
    
    Co-authored-by: João Jesus <jjesus@eshopworld.com>
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Strategies examples.json auto import (#14)
    
    * Strategies examples.json auto import
    
    * Fixes
    
    * Fix
    
    * [biswap] Add biswap strategy (#13)
    
    * init biswap strategy
    
    * changes
    
    * Update src/strategies/biswap/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update src/strategies/biswap/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update src/strategies/biswap/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update index.ts
    
    Co-authored-by: biswapcom <defisysadmin@protonmail.com>
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    # Conflicts:
    #	src/strategies/index.ts
    
    * [l2-deversifi] L2-deversifi strategy (#11)
    
    * init biswap strategy
    
    * changes
    
    * Update src/strategies/biswap/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update src/strategies/biswap/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update src/strategies/biswap/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update index.ts
    
    Co-authored-by: biswapcom <defisysadmin@protonmail.com>
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Fix poap-with-weight
    
    * Fix readme
    
    Co-authored-by: Rashmi-278 <rashmiabbigeri@gmail.com>
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    Co-authored-by: João Jesus <1743407+joaomajesus@users.noreply.github.com>
    Co-authored-by: João Jesus <jjesus@eshopworld.com>
    Co-authored-by: biswapcom <defisysadmin@protonmail.com>

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -125,6 +125,7 @@ import * as tombFinance from './tomb-finance';
 import * as trancheStakingSLICE from './tranche-staking-slice';
 import * as unipoolSameToken from './unipool-same-token';
 import * as unipoolUniv2Lp from './unipool-univ2-lp';
+import * as poapWithWeight from './poap-with-weight';
 import * as uniswapV3 from './uniswap-v3';
 import * as l2Deversifi from './l2-deversifi';
 import * as vestedDeversifi from './vested-deversifi';
@@ -258,6 +259,7 @@ const strategies = {
   'tranche-staking-slice': trancheStakingSLICE,
   'unipool-same-token': unipoolSameToken,
   'unipool-univ2-lp': unipoolUniv2Lp,
+  'poap-with-weight': poapWithWeight,
   'uniswap-v3': uniswapV3,
   'l2-deversifi': l2Deversifi,
   'vested-deversifi': vestedDeversifi,
