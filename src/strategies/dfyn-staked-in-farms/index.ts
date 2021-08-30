500f9bb9039f26c62d25fc659f63a6e9d7c7f140
Vatsal Gupta <48270372+vatsalgupta13@users.noreply.github.com>
2021-08-30 22:03:20 +0530

    Update author in src/strategies/dfyn-staked-in-farms/index.ts
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/strategies/dfyn-staked-in-farms/index.ts
+++ src/strategies/dfyn-staked-in-farms/index.ts
@@ -2,7 +2,7 @@ import { formatUnits } from '@ethersproject/units';
 import { multicall } from '../../utils';
 import { subgraphRequest } from '../../utils';
 
-export const author = 'vatsal';
+export const author = 'vatsalgupta13';
 export const version = '0.1.0';
 
 
