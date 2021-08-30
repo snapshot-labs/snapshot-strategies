22d6a9cd17d600dab2347983a117d9bb37abe9f5
Meir Bank <10442269+meirbank@users.noreply.github.com>
2021-08-30 03:54:00 -0400

    [has-rock] Rock NFT voting strategy (#46)
    * Rock Voting Strategy
    
    * Rock Voting Strategy
    
    * Update src/strategies/has-rock/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update src/strategies/has-rock/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * Update src/strategies/has-rock/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

---
+++ src/strategies/has-rock/index.ts
@@ -0,0 +1,50 @@
+import { multicall } from '../../utils';
+
+export const author = 'AngelDAO';
+export const version = '0.1.0';
+
+const abi = [
+  'function rocks(uint256) view returns (address owner, bool currentlyForSale, uint256 price, uint256 timesSold)'
+];
+
+export async function strategy(
+  space,
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+) {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+
+  let calls = [] as  any;
+  for (var i=0; i< 100; i++){
+    calls.push([
+      options.address,
+      'rocks',
+      [i]
+    ])
+  }
+
+  const response = await multicall(
+    network,
+    provider,
+    abi,
+    calls,
+    { blockTag }
+  );
+
+  let result = {} as  any;
+
+  addresses.forEach((address, x)=> {
+    let addressRocks = 0;
+    response.forEach((rockObject, i) => {
+        if (rockObject.owner == address){
+          addressRocks++;
+        }
+    })
+    result[address] = addressRocks
+  })
+
+  return result;
+}
