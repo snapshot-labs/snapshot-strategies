85f27b15335e3501bd3f1a199c62f123760a71fa
Vatsal Gupta <48270372+vatsalgupta13@users.noreply.github.com>
2021-08-31 08:22:36 +0530

    [dfyn-staked-in-vaults] dfyn-staked-in-vaults strategy added (#49)
    * dfyn-staked-in-vaults strategy added
    
    * Change author in src/strategies/dfyn-staked-in-vaults/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    Co-authored-by: Vatsal Gupta <vatsalgupta-13@users.noreply.github.com>
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

---
+++ src/strategies/dfyn-staked-in-vaults/index.ts
@@ -0,0 +1,56 @@
+import { formatUnits } from '@ethersproject/units';
+import { multicall } from '../../utils';
+
+export const author = 'vatsalgupta13';
+export const version = '0.1.0';
+
+function chunk(array, chunkSize) {
+  let tempArray: any[] = [];
+  for (let i = 0, len = array.length; i < len; i += chunkSize)
+    tempArray.push(array.slice(i, i + chunkSize));
+  return tempArray;
+}
+
+export async function strategy(
+  space,
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+) {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+  var callData: [any, string, [any]][] = []
+  addresses.map((userAddress: any) => { options.contractAddresses.map((vaultAddress: any) => { callData.push([vaultAddress, options.methodABI.name, [userAddress]]) }) })
+  callData = [...chunk(callData, 2000)] // chunking the callData into multiple arrays of 2000 requests
+  let response: any[] = []
+  for (let i = 0; i < callData.length; i++) {
+    let tempArray = await multicall(
+      network,
+      provider,
+      [options.methodABI],
+      callData[i],
+      { blockTag }
+    );
+    response.push(...tempArray)
+  }
+  if (options.contractAddresses.length > 1) {
+    // grouping all balances of a particular address together
+    let result: any = [];
+    response = [].concat.apply([], response);
+    for (let i = addresses.length; i > 0; i--) {
+      result.push(response.splice(0, Math.ceil(response.length / i)));
+    }
+    // performing summation over all balances of the user
+    response = []
+    result.map((item, index) => {
+      response.push(result[index].reduce((a, b) => [a.add(b)]))
+    })
+  }
+  return Object.fromEntries(
+    response.map((value, i) => [
+      addresses[i],
+      options.scoreMultiplier * parseFloat(formatUnits(value.toString(), options.decimals))
+    ])
+  );
+}
