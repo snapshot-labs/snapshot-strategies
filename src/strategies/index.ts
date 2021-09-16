f9e87fec6a92b194a87c65714b6dc3f0e42382de
sunrisedao <sunrisedao@gmail.com>
2021-09-16 13:36:30 +0700

    add sunrisegaming-staking

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -141,6 +141,7 @@ import * as snowswap from './snowswap';
 import * as meebitsdao from './meebitsdao';
 import * as crucibleERC20BalanceOf from './crucible-erc20-balance-of';
 import * as hasrock from './has-rock';
+import * as sunriseGamingStaking from './sunrisegaming-staking';
 
 const strategies = {
   coordinape,
@@ -283,6 +284,7 @@ const strategies = {
   snowswap,
   meebitsdao,
   'crucible-erc20-balance-of': crucibleERC20BalanceOf,
+  'sunrisegaming-staking': sunriseGamingStaking,
   'has-rock': hasrock
 };
 
---
+++ src/strategies/sunrisegaming-staking/index.ts
@@ -0,0 +1,75 @@
+import { multicall } from '../../utils';
+import { formatUnits } from '@ethersproject/units';
+import { BigNumber } from '@ethersproject/bignumber';
+
+export const author = 'sunrisegaming';
+export const version = '0.1.0';
+
+const masterChefAbi = [
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256'
+      },
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address'
+      }
+    ],
+    name: 'userInfo',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256'
+      },
+      {
+        internalType: 'uint256',
+        name: 'rewardDebt',
+        type: 'uint256'
+      }
+    ],
+    stateMutability: 'view',
+    type: 'function'
+  }
+];
+
+function bn(num: any): BigNumber {
+  return BigNumber.from(num.toString());
+}
+
+export async function strategy(
+  _space,
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+) {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+
+  // Get staked LP in staking constract
+  let stakedRes = await multicall(
+    network,
+    provider,
+    masterChefAbi,
+    [
+      ...addresses.map((address: any) => [
+        options.stakingAddress,
+        'userInfo',
+        [options.poolIndex, address]
+      ])
+    ],
+    { blockTag }
+  );
+
+  return Object.fromEntries(
+    stakedRes.map((stakedInfo, i) => {
+      const parsedAmount = parseFloat(formatUnits(bn(stakedInfo.amount), options.decimal));
+      return [addresses[i], parsedAmount];
+    })
+  );
+}
