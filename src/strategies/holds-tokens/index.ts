2e8c9a1f812ebd8324d5db15db86f5935943f379
lightninglu10 <lightning.lu7@gmail.com>
2021-09-29 10:53:58 -0700

    fetching the block number if we are not using latest

--- src/strategies/holds-tokens/index.ts
+++ src/strategies/holds-tokens/index.ts
@@ -1,5 +1,5 @@
 import { formatUnits } from '@ethersproject/units';
-import { multicall, getProvider } from '../../utils';
+import { multicall, getProvider, subgraphRequest } from '../../utils';
 
 export const author = 'lightninglu10';
 export const version = '0.1.0';
@@ -8,7 +8,60 @@ const abi = [
   'function balanceOf(address account) external view returns (uint256)'
 ];
 
+const defaultGraphs = {
+  '56': 'https://api.thegraph.com/subgraphs/name/apyvision/block-info',
+  '137': 'https://api.thegraph.com/subgraphs/name/sameepsi/maticblocks'
+};
 
+async function getChainBlockNumber(
+  timestamp: number,
+  graphURL: string
+): Promise<number> {
+  const query = {
+    blocks: {
+      __args: {
+        first: 1,
+        orderBy: 'number',
+        orderDirection: 'desc',
+        where: {
+          timestamp_lte: timestamp
+        }
+      },
+      number: true,
+      timestamp: true
+    }
+  };
+  const data = await subgraphRequest(graphURL, query);
+  return Number(data.blocks[0].number);
+}
+
+async function getChainBlocks(
+  snapshot,
+  provider,
+  options,
+  network
+): Promise<any> {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+  const block = await provider.getBlock(blockTag);
+  const chainBlocks = {};
+  for (const strategy of options.tokenAddresses) {
+    if (chainBlocks[strategy.network]) {
+      continue;
+    }
+    if (blockTag === 'latest' || strategy.network === network) {
+      chainBlocks[strategy.network] = blockTag;
+    } else {
+      const graph =
+        options.graphs?.[strategy.network] || defaultGraphs[strategy.network];
+      chainBlocks[strategy.network] = await getChainBlockNumber(
+        block.timestamp,
+        graph
+      );
+    }
+  }
+
+  return chainBlocks;
+}
 
 export async function strategy(
   space,
@@ -28,17 +81,29 @@ export async function strategy(
     return bool ? 1 : 0
   }
 
+  const chainBlocks = await getChainBlocks(
+    snapshot,
+    provider,
+    options,
+    network
+  );
+
   const allAddresses = {};
 
   for (let i = 0; i < tokens.length; i++) {
-    const {address, network, snapshot} = tokens[i];
-    const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+    const {address, network} = tokens[i];
 
     const multicallAddresses = addresses.map((userAddress: any) => [address, 'balanceOf', [userAddress]]);
+    const chainProvider = getProvider(network);
+
+    let blockTag = chainBlocks[network]
+    if (options.snapshot === 'latest') {
+      blockTag = chainProvider.getBlockNumber();
+    }
 
     const response = await multicall(
       network,
-      getProvider(network),
+      chainProvider,
       abi,
       multicallAddresses,
       { blockTag }
