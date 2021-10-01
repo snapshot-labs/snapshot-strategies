3d80587aaca0d1c03e4248ddeac8222f22d5940e
Chaitanya <yourchaitu@gmail.com>
2021-10-01 22:22:27 +0530

    Update src/strategies/cyberkongz/index.ts

--- src/strategies/cyberkongz/index.ts
+++ src/strategies/cyberkongz/index.ts
@@ -1,7 +1,7 @@
 import { formatUnits } from '@ethersproject/units';
 import { multicall } from '../../utils';
 
-export const author = 'owl';
+export const author = 'cesarsld';
 export const version = '0.1.0';
 
 const abi = [
