d4967008960b92ba5d6fce7deefefdcc1ce82a4f
Brennan Fife <brenfife@gmail.com>
2021-10-07 11:56:49 -0600

    Required fixes

--- src/strategies/hashes-voting/index.ts
+++ src/strategies/hashes-voting/index.ts
@@ -1,29 +1,11 @@
 import { formatUnits } from '@ethersproject/units';
 import { multicall } from '../../utils';
 
-export const author = 'Arr00';
+export const author = 'brennanfife';
 export const version = '0.1.0';
 
 const abi = [
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: '_account',
-        type: 'address'
-      }
-    ],
-    name: 'getCurrentVotes',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
-      }
-    ],
-    stateMutability: 'view',
-    type: 'function'
-  }
+  'function getCurrentVotes(address _account) view returns (uint256)'
 ];
 
 export async function strategy(
