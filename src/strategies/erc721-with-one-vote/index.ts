d9369a70295ae8ba497f28bfe6a3482bc6593730
Chaitanya <yourchaitu@gmail.com>
2021-10-10 22:32:15 +0530

    Update src/strategies/erc721-with-one-vote/index.ts

--- src/strategies/erc721-with-one-vote/index.ts
+++ src/strategies/erc721-with-one-vote/index.ts
@@ -1,7 +1,7 @@
 import { formatUnits } from '@ethersproject/units';
 import { multicall } from '../../utils';
 
-export const author = '2fd';
+export const author = 'alyxdow';
 export const version = '0.1.0';
 
 const abi = [
