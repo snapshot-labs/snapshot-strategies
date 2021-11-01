2528c1bda2b025853bde6194e41004fe77bcafed
Onigiri <35141155+onigiri-x@users.noreply.github.com>
2021-11-01 06:29:41 -0300

    [digitalax-mona-quickswap] Digitalax mona quickswap (#136)
    * Digitalax mona lp on quickswap
    
    * Move this to make it easier to merge
    
    * Update src/strategies/digitalax-mona-quickswap/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -170,6 +170,7 @@ import * as singleStakingVaultBalanceOf from './single-staking-vault-balanceof';
 import * as svsStaking from './svs-staking';
 import * as mcbBalanceFromGraph from './mcb-balance-from-graph';
 import * as colonyReputation from './colony-reputation';
+import * as digitalaxMonaQuickswap from './digitalax-mona-quickswap';
 import * as digitalaxGenesisContribution from './digitalax-genesis-contribution';
 import * as digitalaxLPStakers from './digitalax-lp-stakers';
 
@@ -343,6 +344,7 @@ const strategies = {
   'mutant-cats-stakers-and-holders': mutantCatsStakersAndHolders,
   'svs-staking': svsStaking,
   'mcb-balance-from-graph': mcbBalanceFromGraph,
+  'digitalax-mona-quickswap': digitalaxMonaQuickswap,
   'digitalax-genesis-contribution': digitalaxGenesisContribution,
   'digitalax-lp-stakers': digitalaxLPStakers,
   'colony-reputation': colonyReputation
