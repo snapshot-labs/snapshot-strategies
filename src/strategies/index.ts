81ea88ba0c7de9b2653328cd0328f429724934fa
Chaitanya <yourchaitu@gmail.com>
2021-11-02 14:28:27 +0530

    Revert "[galaxy-nft-with-score] add strategy: galaxy-nft-with-score (#138)"
    This reverts commit 117e7834d47851b0295363fc5992c87462a30caf.

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -173,7 +173,6 @@ import * as colonyReputation from './colony-reputation';
 import * as digitalaxMonaQuickswap from './digitalax-mona-quickswap';
 import * as digitalaxGenesisContribution from './digitalax-genesis-contribution';
 import * as digitalaxLPStakers from './digitalax-lp-stakers';
-import * as galaxyNftWithScore from './galaxy-nft-with-score';
 
 const strategies = {
   coordinape,
@@ -348,8 +347,7 @@ const strategies = {
   'digitalax-mona-quickswap': digitalaxMonaQuickswap,
   'digitalax-genesis-contribution': digitalaxGenesisContribution,
   'digitalax-lp-stakers': digitalaxLPStakers,
-  'colony-reputation': colonyReputation,
-  'galaxy-nft-with-score': galaxyNftWithScore
+  'colony-reputation': colonyReputation
 };
 
 Object.keys(strategies).forEach(function (strategyName) {
