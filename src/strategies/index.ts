c380c8ea0f380ee2140015deacb2b3989ac4695f
jinanton <jin-92@list.ru>
2021-11-22 20:52:53 +0600

    [vote-power-and-share] Vote power and share strategy (#172)
    * feat: added `vote-power-and-share` strategy
    
    added new strategy (`vote-power-and-share`) for get Voting power or share of total supply depending on the parameters used.
    
    * chore: change symbol in exemple
    
    * test: change network in exemple
    
    * test: change exemple
    
    included at least 1 address with a positive score
    change address token
    change number of block (snapshot)
    removed unnecessary parameters
    
    * docs: change readme.md
    
    * revert: revert changes from yarn.lock
    
    * style: after `lint` script
    
    Co-authored-by: Anton Pavlov <pavlovap@competentit.ru>

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -186,6 +186,7 @@ import * as vesper from './vesper';
 import * as thales from './thales';
 import * as bscMvb from './bsc-mvb';
 import * as coinswap from './coinswap';
+import * as votePowerAndShare from './vote-power-and-share';
 
 const strategies = {
   'nouns-rfp-power': nounsPower,
@@ -373,7 +374,8 @@ const strategies = {
   thales,
   'tech-quadratic-ranked-choice': techQuadraticRankedChoice,
   'bsc-mvb': bscMvb,
-  coinswap
+  coinswap,
+  'vote-power-and-share': votePowerAndShare
 };
 
 Object.keys(strategies).forEach(function (strategyName) {
