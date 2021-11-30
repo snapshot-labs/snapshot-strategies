736a8c332ceea6c0b8c92480cbfd20997149e736
Vivek205 <ichbinvivek@gmail.com>
2021-11-30 11:10:58 +0530

    strategy: snet-farmers

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -197,6 +197,7 @@ import * as pushVotingPower from './push-voting-power';
 import * as stakedPSPBalance from './staked-psp-balance';
 import * as erc20BalanceOfContractMultiplier from './erc20-balance-of-contract-multiplier';
 import * as agave from './agave';
+import * as snetFarmers from './snet-farmers';
 
 const strategies = {
   'nouns-rfp-power': nounsPower,
@@ -395,7 +396,8 @@ const strategies = {
   'push-voting-power': pushVotingPower,
   'staked-psp-balance': stakedPSPBalance,
   'erc20-balance-of-contract-multiplier': erc20BalanceOfContractMultiplier,
-  agave
+  agave,
+  'snet-farmers': snetFarmers
 };
 
 Object.keys(strategies).forEach(function (strategyName) {
