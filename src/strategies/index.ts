647f98846b1158adc31256d87cd329924044f70c
ug02fast <arthurzhuk@gmail.com>
2021-12-03 14:35:08 -0800

    spacing

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -201,9 +201,8 @@ import * as agave from './agave';
 import * as juicebox from './juicebox';
 import * as snetFarmers from './snet-farmers';
 import * as snetStakers from './snet-stakers';
-import * as snetLiquidityProviders from "./snet-liquidity-providers";
+import * as snetLiquidityProviders from './snet-liquidity-providers';
 import * as saddleFinance from './saddle-finance';
-
 
 const strategies = {
   'nouns-rfp-power': nounsPower,
@@ -407,7 +406,7 @@ const strategies = {
   juicebox,
   'snet-farmers': snetFarmers,
   'snet-stakers': snetStakers,
-  "snet-liquidity-providers": snetLiquidityProviders,
+  'snet-liquidity-providers': snetLiquidityProviders,
   'saddle-finance': saddleFinance
 };
 
