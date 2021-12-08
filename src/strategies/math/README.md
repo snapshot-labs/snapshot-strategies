742bae86775c37e2799fd02b53b8dbf28708ed63
Jonathan LEI <xJonathan@outlook.com>
2021-12-08 06:54:31 +0000

    Add min and max operations to math strategy

--- src/strategies/math/README.md
+++ src/strategies/math/README.md
@@ -6,21 +6,68 @@ Currently supported operations are:
 
 - `square-root`
 - `cube-root`
+- `min`
+- `max`
 
 The following example takes the cube root of a user's DAI token balance as voting score.
 
 ```json
 {
   "symbol": "MATH",
-  "strategy": {
-    "name": "erc20-balance-of",
-    "network": "1",
-    "params": {
-      "address": "0x6b175474e89094c44da98b954eedeac495271d0f",
-      "symbol": "DAI",
-      "decimals": 18
+  "operands": [
+    {
+      "type": "strategy",
+      "strategy": {
+        "name": "erc20-balance-of",
+        "network": "1",
+        "params": {
+          "address": "0x6b175474e89094c44da98b954eedeac495271d0f",
+          "symbol": "DAI",
+          "decimals": 18
+        }
+      }
     }
-  },
+  ],
   "operation": "square-root"
+}
+```
+
+Here's another example that sets a maximum score of 100 for the result above.
+
+```json
+{
+  "symbol": "MATH",
+  "operands": [
+    {
+      "type": "strategy",
+      "strategy": {
+        "name": "math",
+        "network": "1",
+        "symbol": "MATH",
+        "params": {
+          "operands": [
+            {
+              "type": "strategy",
+              "strategy": {
+                "name": "erc20-balance-of",
+                "network": "1",
+                "params": {
+                  "address": "0x6b175474e89094c44da98b954eedeac495271d0f",
+                  "symbol": "DAI",
+                  "decimals": 18
+                }
+              }
+            }
+          ],
+          "operation": "square-root"
+        }
+      }
+    },
+    {
+      "type": "constant",
+      "value": 100
+    }
+  ],
+  "operation": "min"
 }
 ```
--- src/strategies/math/index.ts
+++ src/strategies/math/index.ts
@@ -2,7 +2,47 @@ import { getProvider } from '../../utils';
 import strategies from '..';
 
 export const author = 'xJonathanLEI';
-export const version = '0.1.0';
+export const version = '0.2.0';
+
+interface Options {
+  operands: Operand[] | undefined;
+  operation: Operation | undefined;
+}
+
+interface LegacyFields {
+  strategy: any; // Legacy option used in v0.1.0
+}
+
+type Operand = StrategyOperand | ConstantOperand;
+
+enum OperandType {
+  Strategy = 'strategy',
+  Constant = 'constant'
+}
+
+interface StrategyOperand {
+  type: OperandType.Strategy;
+  strategy: any;
+}
+
+interface ConstantOperand {
+  type: OperandType.Constant;
+  value: number;
+}
+
+enum Operation {
+  SquareRoot = 'square-root',
+  CubeRoot = 'cube-root',
+  Min = 'min',
+  Max = 'max'
+}
+
+const operandCountByOperation: Record<Operation, number> = {
+  [Operation.SquareRoot]: 1,
+  [Operation.CubeRoot]: 1,
+  [Operation.Min]: 2,
+  [Operation.Max]: 2
+};
 
 export async function strategy(
   space,
@@ -12,36 +52,155 @@ export async function strategy(
   options,
   snapshot
 ): Promise<Record<string, number>> {
-  const upstreamResult: Record<string, number> = await strategies[
-    options.strategy.name
-  ].strategy(
-    space,
-    options.strategy.network,
-    getProvider(options.strategy.network),
-    addresses,
-    options.strategy.params,
-    snapshot
+  const strategyOptions: Options = migrateLegacyOptions(options);
+  validateOptions(strategyOptions);
+
+  // Recursively resolve operands
+  const operandPromises: Promise<
+    Record<string, number>
+  >[] = strategyOptions.operands!.map((item) =>
+    resolveOperand(item, addresses, space, snapshot)
+  );
+  const resolvedOperands: Record<string, number>[] = await Promise.all(
+    operandPromises
   );
 
-  let scoreConverter: (score: number) => number;
-  switch (options.operation) {
-    case 'square-root': {
-      scoreConverter = (score) => Math.sqrt(score);
-      break;
+  const finalResult: Record<string, number> = resolveOperation(
+    strategyOptions.operation!,
+    resolvedOperands
+  );
+
+  return finalResult;
+}
+
+function resolveOperation(
+  operation: Operation,
+  resolvedOperands: Record<string, number>[]
+): Record<string, number> {
+  switch (operation) {
+    case Operation.SquareRoot: {
+      return Object.fromEntries(
+        Object.entries(
+          resolvedOperands[0]
+        ).map(([address, score]: [string, number]) => [
+          address,
+          Math.sqrt(score)
+        ])
+      );
     }
-    case 'cube-root': {
-      scoreConverter = (score) => Math.cbrt(score);
-      break;
+    case Operation.CubeRoot: {
+      return Object.fromEntries(
+        Object.entries(
+          resolvedOperands[0]
+        ).map(([address, score]: [string, number]) => [
+          address,
+          Math.cbrt(score)
+        ])
+      );
     }
-    default: {
-      throw new Error(`Unknown math operation: ${options.operation}`);
+    case Operation.Min: {
+      return Object.fromEntries(
+        Object.entries(
+          resolvedOperands[0]
+        ).map(([address, score]: [string, number]) => [
+          address,
+          Math.min(score, resolvedOperands[1][address])
+        ])
+      );
+    }
+    case Operation.Max: {
+      return Object.fromEntries(
+        Object.entries(
+          resolvedOperands[0]
+        ).map(([address, score]: [string, number]) => [
+          address,
+          Math.max(score, resolvedOperands[1][address])
+        ])
+      );
+    }
+  }
+}
+
+async function resolveOperand(
+  operand: Operand,
+  addresses: string[],
+  space: any,
+  snapshot: any
+): Promise<Record<string, number>> {
+  switch (operand.type) {
+    case OperandType.Strategy: {
+      const strategyOperand: StrategyOperand = operand as StrategyOperand;
+
+      const upstreamResult: Record<string, number> = await strategies[
+        strategyOperand.strategy.name
+      ].strategy(
+        space,
+        strategyOperand.strategy.network,
+        getProvider(strategyOperand.strategy.network),
+        addresses,
+        strategyOperand.strategy.params,
+        snapshot
+      );
+
+      return upstreamResult;
+    }
+    case OperandType.Constant: {
+      const constantOperand: ConstantOperand = operand as ConstantOperand;
+
+      return Object.fromEntries(
+        addresses.map((address) => [address, constantOperand.value])
+      );
+    }
+  }
+}
+
+function validateOptions(options: Options) {
+  if (!options.operands) {
+    throw new Error('Field `operands` missing');
+  }
+  if (!options.operation) {
+    throw new Error('Field `operation` missing');
+  }
+  if (
+    options.operation !== Operation.SquareRoot &&
+    options.operation !== Operation.CubeRoot &&
+    options.operation !== Operation.Min &&
+    options.operation !== Operation.Max
+  ) {
+    throw new Error('Invalid `operation`');
+  }
+
+  for (const operand of options.operands) {
+    if (
+      operand.type !== OperandType.Strategy &&
+      operand.type !== OperandType.Constant
+    ) {
+      throw new Error('Invalid operand type');
     }
   }
 
-  return Object.fromEntries(
-    Object.entries(upstreamResult).map(([address, score]) => [
-      address,
-      scoreConverter(score)
-    ])
-  );
+  if (options.operands.length !== operandCountByOperation[options.operation]) {
+    throw new Error('Operand count mismatch');
+  }
+}
+
+function migrateLegacyOptions(options: Options & LegacyFields): Options {
+  if (options.strategy && options.operands) {
+    throw new Error('Only one of `strategy` and `operands` can be used');
+  }
+
+  // `strategy` was used in v0.1.0
+  if (options.strategy) {
+    return {
+      operands: [
+        {
+          type: OperandType.Strategy,
+          strategy: options.strategy
+        }
+      ],
+      operation: options.operation
+    };
+  } else {
+    return options;
+  }
 }
