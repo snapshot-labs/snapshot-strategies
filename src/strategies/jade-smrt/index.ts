82486296ec8b63238110e311f0b527d1a82e276e
drgorillamd <dr_gorilla@protonmail.com>
2021-12-10 22:57:40 +0100

    feat: optimized

--- src/strategies/jade-smrt/index.ts
+++ src/strategies/jade-smrt/index.ts
@@ -6,8 +6,6 @@ import { formatUnits } from '@ethersproject/units';
 export const author = 'drgorillamd';
 export const version = '1.0.0';
 
-const avaxGraph = 'https://api.thegraph.com/subgraphs/name/elkfinance/avax-blocks';
-
 const abi = [
   'function balanceOf(address account) external view returns (uint256)',
   'function totalSupply() external view returns (uint256)'
@@ -22,64 +20,70 @@ export async function strategy(
   snapshot
 ) {
 
+  let jadePrice;
+  let smrtPrice;
+  let smrtRPrice;
+  let resBsc;
+  let resAvax;
+  let promises: any = []
+
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
   const block = await provider.getBlock(blockTag);
   const timestamp = block.timestamp;
+  const avaxBlockTag = await getAvaxBlockTag(timestamp, options);
 
-  const avaxBlockTag = await getAvaxBlockTag(timestamp);
+  // Token prices :
+  promises.push(geckoPrice(options.JADE.address, timestamp, 'binance-smart-chain'));
+  promises.push(geckoPrice(options.SMRT.address, timestamp, 'avalanche'));
+  promises.push(geckoPrice(options.SMRTR.address, timestamp, 'avalanche'));
 
-  const jadePrice = await geckoPrice(options.JADE.address, timestamp, 'binance-smart-chain');
-  const smrtPrice = await geckoPrice(options.SMRT.address, timestamp, 'avalanche');
-  const smrtRPrice = await geckoPrice(options.SMRTR.address, timestamp, 'avalanche');
-
-  //BSC balances:
+  // BSC balances:
   const multiBsc = new Multicaller(network, provider, abi, { blockTag });
-
   addresses.forEach((address) => {
     multiBsc.call(address+"-jade", options.JADE.address, 'balanceOf', [address]);
     multiBsc.call(address+"-sjade", options.SJADE.address, 'balanceOf', [address]);
   });
-
-  const resBsc = await multiBsc.execute();
+  promises.push(multiBsc.execute());
 
   // Avax balances:
   const multiAvax = new Multicaller('43114', getProvider('43114'), abi, { blockTag: avaxBlockTag });
-
   addresses.forEach((address) => {
     multiAvax.call(address+"-smrt", options.SMRT.address, 'balanceOf', [address]);
     multiAvax.call(address+"-smrtR", options.SMRTR.address, 'balanceOf', [address]);
     multiAvax.call(address+"-smrtRLp", options.SMRTRLP.address, 'balanceOf', [address])
   });
 
-  // Avax SMRTR/WAVAX pool SMRTR balance and LP token total supply
-
+  // Avax SMRTR/WAVAX pool: SMRTR balance and LP token total supply
   const LPBalance = await call(getProvider('43114'), abi, [options.SMRTR.address, 'balanceOf', [options.SMRTRLP.address]]);
   const LPSupply =  await call(getProvider('43114'), abi, [options.SMRTRLP.address, 'totalSupply', []]);
+  promises.push(multiAvax.execute());
 
-  const resAvax = await multiAvax.execute();
+  [jadePrice, smrtPrice, smrtRPrice, resBsc, resAvax] = await Promise.all(promises);
 
-  //const smrtRWeight = smrtRPrice / jadePrice;
-  //const smrtWeight = smrtPrice / jadePrice;
 
   return Object.fromEntries(
     addresses.map( (adr) => {
       let bal = parseFloat(formatUnits(resBsc[adr+"-jade"], options.JADE.decimals));
       bal += parseFloat(formatUnits(resBsc[adr+"-sjade"], options.SJADE.decimals));
 
-      // SMRT balance * SMRT price/JADE price
-      bal += parseFloat(formatUnits(
-        resAvax[adr+"-smrt"]*smrtPrice / jadePrice
-        , options.SMRT.decimals));
+      // SMRT balance * SMRT price/JADE price:
+      const parsedSmrt = parseFloat(formatUnits(
+        resAvax[adr+"-smrt"],
+        options.SMRT.decimals));
+      bal += parsedSmrt * smrtPrice / jadePrice;
 
-      // SMRTR balance * SMRTR price/JADE price
-      bal += parseFloat(formatUnits(
-        resAvax[adr+"-smrtR"]*smrtRPrice / jadePrice
-        , options.SMRTR.decimals));
+      // SMRTR balance * SMRTR price/JADE price:
+      const parsedSrmtr = parseFloat(formatUnits(
+        resAvax[adr+"-smrtR"],
+        options.SMRTR.decimals));
+      bal += parsedSrmtr * smrtRPrice / jadePrice;
 
-      // LP token held * smrtr pool balance / LP token total supply
-      bal += parseFloat(formatUnits(
-        resAvax[adr+"-smrtRLp"].mul(LPBalance).div(LPSupply)
-        , options.SMRTR.decimals));
+      // LP token held * smrtr pool balance / LP token total supply:
+      const LPHeld = resAvax[adr+"-smrtRLp"].mul(LPBalance).div(LPSupply);
+      const parsedLP = parseFloat(formatUnits(
+        LPHeld,
+        options.SMRTR.decimals));
+      bal += parsedLP * smrtRPrice / jadePrice;
 
       return [adr, bal];
     })
@@ -88,6 +92,7 @@ export async function strategy(
 
 async function getAvaxBlockTag(
   timestamp: number,
+  options
 ): Promise<number> {
   const query = {
     blocks: {
@@ -103,7 +108,7 @@ async function getAvaxBlockTag(
       timestamp: true
     }
   };
-  const data = await subgraphRequest(avaxGraph, query);
+  const data = await subgraphRequest(options.avaxGraph, query);
   return Number(data.blocks[0].number);
 }
 
