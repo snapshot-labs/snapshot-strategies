c30817735fcea9d09e9d7573a3631a782abe5265
Travis <travis@everipedia.com>
2022-01-03 20:22:05 -0800

    Frax Finance With Delegations

--- src/strategies/frax-finance/index.ts
+++ src/strategies/frax-finance/index.ts
@@ -195,17 +195,17 @@ export async function strategy(
         const farm_uni_frax_fxs = farmUniFraxFxsBalances[i][0];
 
         // Print statements
-        // console.log(`==================${addresses[i]}==================`);
-        // console.log("Free FXS: ", free_fxs.div(BIG18).toString());
-        // console.log("veFXS: ", vefxs.div(BIG18).toString());
-        // console.log("Free Uni FRAX/FXS LP: ", free_uni_frax_fxs.div(BIG18).toString());
-        // console.log("Farmed Uni FRAX/FXS LP [boosted]: ", farm_uni_frax_fxs.div(BIG18).toString());
-        // console.log("------");
-        // console.log("E18");
-        // console.log("FXS per Uni FRAX/FXS LP E18: ", uniLPFraxFxs_fxs_per_LP_E18.toString());
-        // console.log("E0");
-        // console.log("FXS per Uni FRAX/FXS LP E0: ", uniLPFraxFxs_fxs_per_LP_E18.div(BIG18).toString());
-        // console.log(``);
+        // console.debug(`==================${addresses[i]}==================`);
+        // console.debug("Free FXS: ", free_fxs.div(BIG18).toString());
+        // console.debug("veFXS: ", vefxs.div(BIG18).toString());
+        // console.debug("Free Uni FRAX/FXS LP: ", free_uni_frax_fxs.div(BIG18).toString());
+        // console.debug("Farmed Uni FRAX/FXS LP [boosted]: ", farm_uni_frax_fxs.div(BIG18).toString());
+        // console.debug("------");
+        // console.debug("E18");
+        // console.debug("FXS per Uni FRAX/FXS LP E18: ", uniLPFraxFxs_fxs_per_LP_E18.toString());
+        // console.debug("E0");
+        // console.debug("FXS per Uni FRAX/FXS LP E0: ", uniLPFraxFxs_fxs_per_LP_E18.div(BIG18).toString());
+        // console.debug(``);
 
         return [
           addresses[i],
