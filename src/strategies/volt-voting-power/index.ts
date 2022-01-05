ccee029ed8ad0fa5c5e62690b3173c6e5bc98456
philipappiah <pappiahkubi1@gmail.com>
2022-01-05 14:47:40 +0000

    fix for volt-voting-strategy - change totalStake to match totalTokenSupply

--- src/strategies/volt-voting-power/index.ts
+++ src/strategies/volt-voting-power/index.ts
@@ -99,7 +99,7 @@ export async function strategy(
       } else {
         totalVoltComposition = lp.reserve1;
       }
-      totalStake = Number(lp.reserve0) + Number(lp.reserve1);
+      totalStake = lp.totalSupply
     });
   }
 
