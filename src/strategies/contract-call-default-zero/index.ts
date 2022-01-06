455e1b787bf13ac3c0611d1db3ef4c39e4ceb9c8
Chaitanya <yourchaitu@gmail.com>
2022-01-06 23:25:40 +0530

    Update src/strategies/contract-call-default-zero/index.ts

--- src/strategies/contract-call-default-zero/index.ts
+++ src/strategies/contract-call-default-zero/index.ts
@@ -1,7 +1,7 @@
 import { formatUnits } from '@ethersproject/units';
 import { multicall } from '../../utils';
 
-export const author = 'snoopy';
+export const author = 'ChefSnoopy';
 export const version = '0.1.0';
 
 function getArgs(options, address: string) {
