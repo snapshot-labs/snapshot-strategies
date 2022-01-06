7f96be2d436f5a60590e80c8fe1986bc952b763d
candoizo <candoizo@protonmail.com>
2022-01-06 09:05:36 -0500

    [aavegotchi-agip] add realm voting power

--- src/strategies/aavegotchi-agip/index.ts
+++ src/strategies/aavegotchi-agip/index.ts
@@ -3,7 +3,7 @@ import { Multicaller } from '../../utils';
 import { subgraphRequest } from '../../utils';
 
 export const author = 'candoizo';
-export const version = '0.1.0';
+export const version = '0.1.1';
 
 const AAVEGOTCHI_SUBGRAPH_URL = {
   137: 'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-core-matic'
@@ -72,6 +72,9 @@ export async function strategy(
       gotchisOwned: {
         baseRarityScore: true,
         equippedWearables: true
+      },
+      parcelsOwned: {
+        size: true
       }
     }
   };
@@ -85,9 +88,26 @@ export async function strategy(
     if (itemValue > 0) prices[parseInt(itemInfo.svgId)] = itemValue;
   });
 
+  // agip 17: Voting power of 0.5 GHST/pixel
+  const realmSizeVotePower = {
+    0: 32, // humble
+    1: 128, // reasonable
+    2: 1028, // spacious vertical
+    3: 1028, // spacious horizontal
+    4: 2048 // partner
+  };
+
   const walletScores = {};
-  result.users.map((addrInfo) => {
-    const { id, gotchisOwned } = addrInfo;
+  result.users.map(({ id, gotchisOwned, parcelsOwned }) => {
+    let realmVotingPowerValue = 0;
+    if (parcelsOwned.length > 0) {
+      parcelsOwned.map(({ size }) => {
+        let votePower = realmSizeVotePower[size];
+        if (isNaN(votePower)) votePower = 0;
+        realmVotingPowerValue += votePower;
+      });
+    }
+
     let gotchisBrsEquipValue = 0;
     if (gotchisOwned.length > 0)
       gotchisOwned.map((gotchi) => {
@@ -117,7 +137,8 @@ export async function strategy(
     const addr = addresses.find(
       (addrOption: string) => addrOption.toLowerCase() === id
     );
-    walletScores[addr] = ownerItemValue + gotchisBrsEquipValue;
+    walletScores[addr] =
+      realmVotingPowerValue + ownerItemValue + gotchisBrsEquipValue;
   });
   addresses.map((addr) => {
     if (!walletScores[addr]) walletScores[addr] = 0;
