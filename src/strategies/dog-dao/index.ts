eb0a61886e082477205da3eca3cf79e5b3bcb27e
Matthew Bunday <mkbunday@gmail.com>
2022-01-10 16:29:30 -0500

    Add Rari, Snoop support to DogDAO voting

--- src/strategies/dog-dao/index.ts
+++ src/strategies/dog-dao/index.ts
@@ -6,7 +6,10 @@ export const author = 'zencephalon';
 export const version = '0.0.0';
 
 const contract = {
-  dog: '0xBAac2B4491727D78D2b78815144570b9f2Fe8899'
+  dog: '0xBAac2B4491727D78D2b78815144570b9f2Fe8899', // 18 decimals
+  fdog: '0x6e0e0bec375446ddcd196bdf291c41525fb95438', // 18 decimals, 5x mint ratio
+  sSnoop: '0x7597c90c98b611e5d5db0671180d92189b90c6c6', // 9 decimals
+  snoop: '0x8715ca97c5b464c1957cefbd18015b5567e52060' // 9 decimals
 };
 
 const abi = {
@@ -39,14 +42,32 @@ export async function strategy(
   };
 
   const dogMulti = makeMulticaller(abi.erc20, contract.dog);
+  const fdogMulti = makeMulticaller(abi.erc20, contract.fdog);
+  const snoopMulti = makeMulticaller(abi.erc20, contract.snoop);
+  const sSnoopMulti = makeMulticaller(abi.erc20, contract.sSnoop);
 
-  const [dogBalances] = await Promise.all([dogMulti.execute()]);
+  const [
+    dogBalances,
+    fDogBalances,
+    snoopBalances,
+    sSnoopBalances
+  ] = await Promise.all([
+    dogMulti.execute(),
+    fdogMulti.execute(),
+    snoopMulti.execute(),
+    sSnoopMulti.execute()
+  ]);
 
   console.log(dogBalances);
   const votes = {};
 
   for (const a of addresses) {
     votes[a] = dogBalances[a].div(BigNumber.from(10).pow(18)).toNumber() || 0;
+    votes[a] +=
+      fDogBalances[a].div(BigNumber.from(10).pow(18)).div(5).toNumber() || 0;
+    votes[a] += snoopBalances[a].div(BigNumber.from(10).pow(9)).toNumber() || 0;
+    votes[a] +=
+      sSnoopBalances[a].div(BigNumber.from(10).pow(9)).toNumber() || 0;
   }
 
   return votes;
