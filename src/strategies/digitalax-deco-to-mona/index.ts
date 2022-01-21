1f5d089b8036636b7f6a740967d257b8c91cb315
onigiri-x <victor@digitalax.xyz>
2022-01-21 08:56:05 -0400

    Make the pair depend on chain info for reserves as opposed to subgraph

--- src/strategies/digitalax-deco-to-mona/index.ts
+++ src/strategies/digitalax-deco-to-mona/index.ts
@@ -1,11 +1,12 @@
-import { subgraphRequest } from '../../utils';
+import { multicall } from '../../utils';
 import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
 
 export const author = 'onigiri-x';
 export const version = '0.1.0';
 
-const QUICKSWAP_SUBGRAPH =
-  'https://api.thegraph.com/subgraphs/name/sameepsi/quickswap05';
+const abi = [
+  'function getReserves() external view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast)'
+];
 
 export async function strategy(
   space,
@@ -15,25 +16,26 @@ export async function strategy(
   options,
   snapshot
 ): Promise<Record<string, number>> {
-  // Set up the GraphQL parameters and necessary variables
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
-  const holderParams = {
-    pairs: {
-      __args: {
-        where: {
-          id_in: [
-            '0x7ecb3be21714114d912469810aedd34e6fc27736',
-            '0x3203bf44d434452b4605c7657c51bfeaf2a0847c'
-          ]
-        }
-      },
-      token0Price: true,
-      token1Price: true
-    }
-  };
+  const pairAddresses = [
+    '0x7ecb3be21714114d912469810aedd34e6fc27736',
+    '0x3203bf44d434452b4605c7657c51bfeaf2a0847c'
+  ];
+  const priceResponse = await multicall(
+    network,
+    provider,
+    abi,
+    pairAddresses.map((address: any) => [address, 'getReserves', []]),
+    { blockTag }
+  );
 
-  // Query subgraph for the holders and the stakers based on input addresses
-  const reserves = await subgraphRequest(QUICKSWAP_SUBGRAPH, holderParams);
+  const priceDecoToEth =
+    parseFloat(priceResponse[0]._reserve1) /
+    parseFloat(priceResponse[0]._reserve0);
+  const priceEthToMona =
+    parseFloat(priceResponse[1]._reserve0) /
+    parseFloat(priceResponse[1]._reserve1);
 
   const erc20Balances = await erc20BalanceOfStrategy(
     space,
@@ -47,9 +49,7 @@ export async function strategy(
   return Object.fromEntries(
     addresses.map((address) => [
       address,
-      erc20Balances[address] *
-        reserves.pairs[1].token1Price *
-        reserves.pairs[0].token0Price
+      erc20Balances[address] * priceDecoToEth * priceEthToMona
     ])
   );
 }
