9971c7ab448c87f801d8825783c480c063073423
Chaitanya <yourchaitu@gmail.com>
2022-01-24 18:19:27 +0530

    Update index.ts

--- src/strategies/genomesdao/index.ts
+++ src/strategies/genomesdao/index.ts
@@ -9,7 +9,7 @@ const abi = [
   'function balanceOf(address account) external view returns (uint256)',
   'function totalSupply() external view returns (uint256)',
   'function token0() external view returns (address)',
-  'function getReserves() external view returns (uint112, uint112, uint32)'
+  'function getReserves() external view returns (uint112, uint112, uint32)',
 ];
 
 export async function strategy(
@@ -27,7 +27,7 @@ export async function strategy(
     multi.call(address, options.address, 'balanceOf', [address])
   );
   const result: Record<string, BigNumberish> = await multi.execute();
-
+  
   addresses.forEach((address: any) =>
     multi.call(address, options.lpaddress, 'balanceOf', [address])
   );
@@ -38,10 +38,10 @@ export async function strategy(
   );
   const resultLP2: Record<string, BigNumberish> = await multi.execute();
 
-  multi.call('token0', options.lpaddress, 'token0', []);
-  multi.call('getReserves', options.lpaddress, 'getReserves', []);
-  multi.call('totalSupply', options.lpaddress, 'totalSupply', []);
-  const { token0, getReserves, totalSupply } = await multi.execute();
+  multi.call("token0", options.lpaddress, 'token0', []);
+  multi.call("getReserves", options.lpaddress, 'getReserves', []);
+  multi.call("totalSupply", options.lpaddress, 'totalSupply', []);
+  const {token0, getReserves, totalSupply} = await multi.execute();
   let totalGnomeAmount: BigNumberish;
   if (token0.toLowerCase() === options.address.toLowerCase()) {
     totalGnomeAmount = getReserves[0];
@@ -49,13 +49,9 @@ export async function strategy(
     totalGnomeAmount = getReserves[1];
   }
 
-  return Object.fromEntries(
-    Object.entries(resultLP).map(([address, balance]) => {
-      let bal: BigNumber = BigNumber.from(balance)
-        .mul(totalGnomeAmount)
-        .div(totalSupply);
-      bal = bal.add(result[address]);
-      return [address, parseFloat(formatUnits(bal, options.decimals))];
-    })
-  );
+  return Object.fromEntries(Object.entries(resultLP1).map(([address, balance]) => {
+    let bal: BigNumber = BigNumber.from(balance).mul(totalGnomeAmount).div(totalSupply);
+    bal = bal.add(result[address]).add(resultLP2[address]);
+    return [address, parseFloat(formatUnits(bal, options.decimals))];
+  }));
 }
