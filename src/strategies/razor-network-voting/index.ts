f60f113e309f4f2a26cfefaaa3022a518866d2f6
Aditya Dhir <aditya@razor.network>
2022-01-26 01:07:26 +0530

    strategy_change

--- src/strategies/razor-network-voting/index.ts
+++ src/strategies/razor-network-voting/index.ts
@@ -4,7 +4,7 @@ export const author = 'adi44'
 export const version = '0.1.0'
 
 const RAZOR_NETWORK_SUBGRAPH_URL = 
-    'https://api.thegraph.com/subgraphs/name/adi44/subgraphrazor'
+    'https://api.thegraph.com/subgraphs/name/rajkharvar/indexer'
 
 // a method to calculate corresponding razor amount for delegators
 async function sRZR_to_RZR(
@@ -26,32 +26,34 @@ export async function strategy(
     provider: any,
     addresses: any[],
     options: any,
+    //symbol: string,
     snapshot: string
 ) {
     const params = {                                                                                    // delegators (where : {delegatorAddress : address[1]}){
         delegators : {                                                                                  //  staker () {
             __args : {                                                                                  //     totalSupply
                 where : {                                                                               //     newStake
-                    delegatorAddress_in : addresses                                                     //      }
+                    delegatorAddress_in : addresses                                                   //      }
                 },                                                                                      // delegatorAddress
             },                                                                                          // Amount_Delegated
             staker :{                                                                                   //  }
                 totalSupply : true,
-                newStake : true
+                stake : true
             },
             delegatorAddress : true,
-            Amount_Delegated : true
+            sAmount : true
                 
         },
         stakers : {                                                                                     // stakers (where : {stakerAddress : address[0]}){
             __args : {                                                                                  //  newStake
                 where : {                                                                               //  totalSupply
-                    stakerAddress_in : addresses                                                        //  stakerAddress
+                    staker_in : addresses                                                       //  stakerAddress
                 },                                                                                      // }
             },
-            newStake : true,
+            stake : true,
             totalSupply : true,
-            stakerAddress : true
+            staker : true,
+            sAmount : true,
         }
 
     };
@@ -62,22 +64,14 @@ export async function strategy(
     }
 
     const score = {};
-    // for delegators
-    /* 
-        id10x2 Delegator 1    200    
-        id20x1 Delegator 2    100
-        id20x2 Delegator 2    200
-        id20x3 Delegator 1    100
     
-    
-    */
 
     // subgraph request 1 : it fetches all the details of the stakers and delegators.
     const result = await subgraphRequest(RAZOR_NETWORK_SUBGRAPH_URL, params);
-
+    console.log(result)
     if(result && result.delegators){
-            result.delegators.forEach(async (delegator: { Amount_Delegated: string; staker: { newStake: string; totalSupply: string; }; delegatorAddress: string | number; }) => {
-                let razor_amount = await sRZR_to_RZR(BigNumber.from(delegator.Amount_Delegated), BigNumber.from(delegator.staker.totalSupply), BigNumber.from(delegator.staker.newStake))
+            result.delegators.forEach(async (delegator: { sAmount: string; staker: { stake: string; totalSupply: string; }; delegatorAddress: string | number; }) => {
+                let razor_amount = await sRZR_to_RZR(BigNumber.from(delegator.sAmount), BigNumber.from(delegator.staker.totalSupply), BigNumber.from(delegator.staker.stake))
                 //if delegator has delegated to more than one staker, we need to add that amount also to calculate score.
                 if(!score[delegator.delegatorAddress])
                 { 
@@ -94,16 +88,17 @@ export async function strategy(
     
     // for stakers
     if(result && result.stakers){
-        result.stakers.forEach((staker: { stakerAddress: string | number; newStake: string; }) => {
+        result.stakers.forEach(async (Staker: { staker: string | number; stake: string; sAmount : string|Number; totalSupply : string}) => {
+            let razor_amount = await sRZR_to_RZR(BigNumber.from(Staker.sAmount), BigNumber.from(Staker.totalSupply), BigNumber.from(Staker.stake))
             //score will be based on the current stake in the block Number
-            if(!score[staker.stakerAddress])
+            if(!score[Staker.staker])
                 { 
                     //if score[delegator] has no score setup already we will put it as intial amount
-                    score[staker.stakerAddress] = wei_to_ether(parseInt(staker.newStake))
+                    score[Staker.staker] = wei_to_ether(Number(razor_amount))
                 }
                 else{
                     // update the score of delegator by adding new Stoken -> razor Value
-                    score[staker.stakerAddress] += wei_to_ether(parseInt(staker.newStake))
+                    score[Staker.staker] += wei_to_ether(Number(razor_amount))
                 }
             
         });
