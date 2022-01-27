5fe89eadcab9f4b0c7097e05c415c9e5295ec7c0
sandmanfinance <info@sandman.finance>
2022-01-27 02:30:26 +0000

    Change name of strategy

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -242,6 +242,7 @@ import * as stakedBalancer from './staked-balancer';
 import * as stakedUniswapModifiable from './staked-uniswap-modifiable';
 import * as erc1155BalanceOfIds from './erc1155-balance-of-ids';
 import * as erc1155BalanceOfIdsWeighted from './erc1155-balance-of-ids-weighted';
+import * as sandmanDao from './sandman-dao';
 
 const strategies = {
   'nouns-rfp-power': nounsPower,
@@ -484,7 +485,8 @@ const strategies = {
   'staked-balancer': stakedBalancer,
   'staked-uniswap-modifiable': stakedUniswapModifiable,
   'erc1155-balance-of-ids': erc1155BalanceOfIds,
-  'erc1155-balance-of-ids-weighted': erc1155BalanceOfIdsWeighted
+  'erc1155-balance-of-ids-weighted': erc1155BalanceOfIdsWeighted,
+  'sandman-dao': sandmanDao
 };
 
 Object.keys(strategies).forEach(function (strategyName) {
