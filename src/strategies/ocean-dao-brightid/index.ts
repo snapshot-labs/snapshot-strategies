77132f2e4f3294d5d3c1e2a7d8435c61769a1c72
trizin <25263018+trizin@users.noreply.github.com>
2022-02-08 12:41:43 +0300

    Use blockfinder && add delegations

--- src/strategies/ocean-dao-brightid/index.ts
+++ src/strategies/ocean-dao-brightid/index.ts
@@ -4,6 +4,7 @@ import {
   multicall,
   subgraphRequest
 } from '../../utils';
+import { getDelegations } from '../../utils/delegation';
 
 export const author = 'trizin';
 export const version = '0.1.0';
@@ -14,98 +15,27 @@ const abi = [
 
 /* Code from multichain strategy */
 
-const defaultGraphs = {
-  '56': 'https://api.thegraph.com/subgraphs/name/apyvision/block-info',
-  '137': 'https://api.thegraph.com/subgraphs/name/sameepsi/maticblocks',
-  '42161':
-    'https://api.thegraph.com/subgraphs/name/ianlapham/arbitrum-one-blocks'
-};
-
-async function getChainBlockNumber(
-  timestamp: number,
-  graphURL: string
-): Promise<number> {
+async function getBlocks(snapshot, provider, options, network) {
+  const blocks = {};
+  Object.keys(options.strategies).forEach((s) => (blocks[s] = 'latest'));
+  const block = await provider.getBlock(snapshot);
   const query = {
     blocks: {
       __args: {
-        first: 1,
-        orderBy: 'number',
-        orderDirection: 'desc',
         where: {
-          timestamp_lte: timestamp
+          ts: block.timestamp,
+          network_in: Object.keys(blocks)
         }
       },
-      number: true,
-      timestamp: true
+      network: true,
+      number: true
     }
   };
-  const data = await subgraphRequest(graphURL, query);
-  return Number(data.blocks[0].number);
-}
-
-async function getChainBlocksFromSubGraph(
-  snapshot,
-  provider,
-  options,
-  network
-): Promise<any> {
-  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
-  const block = await provider.getBlock(blockTag);
-  const chainBlocks = {};
-
-  for (const strategy of Object.keys(options.chains)) {
-    if (chainBlocks[strategy]) {
-      continue;
-    }
-    if (blockTag === 'latest' || strategy === network) {
-      chainBlocks[strategy] = blockTag;
-    } else {
-      const graph = options.graphs?.[strategy] || defaultGraphs[strategy];
-      chainBlocks[strategy] = await getChainBlockNumber(block.timestamp, graph);
-    }
-  }
-
-  return chainBlocks;
-}
-
-async function getChainBlocksFromApi(
-  snapshot,
-  provider,
-  options
-): Promise<any> {
-  const block = await provider.getBlock(snapshot);
-  const timestamp = block.timestamp;
-
-  //Should receive the timestamp as query param and return the block height by chain
-  const apiUrl = `${options.blockApi}?timestamp=${timestamp}`;
-  const resp = await fetch(apiUrl, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
-  }).then((r) => r.json());
-
-  //Response should contain blocks object with chainIds as keys and block numbers as values
-  return resp.blocks;
-}
-
-async function getChainBlocks(
-  snapshot,
-  provider,
-  options,
-  network
-): Promise<any> {
-  if (options.blockApi) {
-    return await getChainBlocksFromApi(snapshot, provider, options);
-  } else {
-    return await getChainBlocksFromSubGraph(
-      snapshot,
-      provider,
-      options,
-      network
-    );
-  }
+  const url = 'https://blockfinder.snapshot.org/graphql';
+  const data = await subgraphRequest(url, query);
+  data.blocks.forEach((block) => (blocks[block.network] = block.number));
+  data.blocks[network] = snapshot;
+  return blocks;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -118,12 +48,9 @@ export async function strategy(
   options,
   snapshot
 ) {
-  const chainBlocks = await getChainBlocks(
-    snapshot,
-    provider,
-    options,
-    network
-  );
+  const chainBlocks = await getBlocks(snapshot, provider, options, network);
+  const delegations = await getDelegations(space, network, addresses, snapshot);
+  console.debug(delegations);
 
   const brightIdNetwork = options.brightIdNetwork || network;
   const response = await multicall(
@@ -140,18 +67,18 @@ export async function strategy(
 
   const totalScores = {};
 
-  for (const chain of Object.keys(options.chains)) {
+  for (const chain of Object.keys(options.strategies)) {
     let scores = await getScoresDirect(
       space,
-      options.chains[chain],
+      options.strategies[chain],
       chain,
       getProvider(chain),
       addresses,
       chainBlocks[chain]
     );
+
     // [{ address: '0x...', score: 0.5 },{ address: '0x...', score: 0.5 }]
     // sum scores for each address and return
-
     scores = scores.reduce((finalScores: any, score: any) => {
       for (const [address, value] of Object.entries(score)) {
         if (!finalScores[address]) {
@@ -162,6 +89,16 @@ export async function strategy(
       return finalScores;
     }, {});
     // { address: '0x...55', score: 1.0 }
+
+    // sum delegations
+    addresses.forEach((address) => {
+      if (delegations[address]) {
+        delegations[address].forEach((delegator) => {
+          scores[address] += scores[delegator];
+          scores[delegator] = 0;
+        });
+      }
+    });
 
     for (const key of Object.keys(scores)) {
       totalScores[key] = totalScores[key]
