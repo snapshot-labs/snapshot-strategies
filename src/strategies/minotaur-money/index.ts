3cb0214b547dc2ed75f24fea4de8e9d62f744f6e
pepperstepper <86356812+pepperstepper@users.noreply.github.com>
2022-03-05 13:47:47 +0000

    [minotaur-money] Makes the voting units absolute MINO held instead of % of MINO supply. (#434)
    * Add minotaur-money strategy
    
    Counts up users MINO, sMINO and wsMINO,
    in their wallets, as well as in the MMF staking pool.
    
    https://minotaur.money
    
    * Add new MMF Staking pool + remove wsMino and MINO EOA balance counting
    
    * re add in brace
    
    * remove unused import comment
    
    * Update src/strategies/minotaur-money/index.ts
    
    bump version
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    * change object type to any type
    
    * bump version number
    
    * Make voting units absolute MINO
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/strategies/minotaur-money/index.ts
+++ src/strategies/minotaur-money/index.ts
@@ -2,7 +2,7 @@ import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
 import { Multicaller, call } from '../../utils';
 
 export const author = 'pepperstepper';
-export const version = '0.0.2';
+export const version = '0.0.3';
 
 const minoContractAddress = '0x3A1138075bd97a33F23A87824b811146FA44288E';
 const sMinoContractAddress = '0xB46fe6791A30d51970EA3B840C9fa5F1F107b86F';
@@ -12,12 +12,10 @@ const mmfPoolAddressNew = '0x849f97c5452cc4bad1069b8efe2b3561b06694c3';
 
 const erc20ContractAbi = [
   'function balanceOf(address account) external view returns (uint256)',
-  'function totalSupply() external view returns (uint256)'
+  'function totalSupply() external view returns (uint256)',
 ];
 const sMinoContractAbi = ['function index() external view returns (uint256)'];
-const mmfPoolAbi = [
-  'function userInfo(address) view returns (uint256 amount, uint256 rewardDebt)'
-];
+const mmfPoolAbi = ['function userInfo(address) view returns (uint256 amount, uint256 rewardDebt)']
 
 type MultiCallResult = Record<string, BigNumberish>;
 type MultiCallObjectResult = Record<string, any>;
@@ -40,14 +38,6 @@ export async function strategy(
     ]);
   };
 
-  const callMinoTotalSupply = () => {
-    return call(provider, erc20ContractAbi, [
-      minoContractAddress,
-      'totalSupply',
-      []
-    ]);
-  };
-
   const makeMulticaller = (abi, contractAddress, functionSignature) => {
     const multiCaller = new Multicaller(network, provider, abi, {
       blockTag
@@ -57,6 +47,12 @@ export async function strategy(
     );
     return multiCaller;
   };
+
+  const minoMulti = makeMulticaller(
+    erc20ContractAbi,
+    minoContractAddress,
+    'balanceOf'
+  );
 
   const sMinoMulti = makeMulticaller(
     erc20ContractAbi,
@@ -76,22 +72,16 @@ export async function strategy(
     'userInfo'
   );
 
-  const [
-    minoTotalSupply,
-    index,
-    sMinoBalances,
-    mmfUserInfosOld,
-    mmfUserInfoNew
-  ]: [
-    BigNumber,
+  const [index, minoBalances, sMinoBalances, mmfUserInfosOld, mmfUserInfoNew]: [
     BigNumber,
     MultiCallResult,
+    MultiCallResult,
     MultiCallObjectResult,
-    MultiCallObjectResult
+    MultiCallObjectResult,
   ] = await Promise.all([
-    callMinoTotalSupply(),
     callIndex(),
     sMinoMulti.execute(),
+    minoMulti.execute(),
     wsMinoInMMFMultiOld.execute(),
     wsMinoInMMFMultiNew.execute()
   ]);
@@ -99,31 +89,24 @@ export async function strategy(
   const scores: Record<string, BigNumber> = {};
 
   for (const address of addresses) {
-    const wsMinoScore = BigNumber.from(
-      mmfUserInfosOld[address]
-        ? BigNumber.from(mmfUserInfosOld[address]['amount'])
-        : 0
-    )
-      .add(
-        mmfUserInfoNew[address]
-          ? BigNumber.from(mmfUserInfoNew[address]['amount'])
-          : 0
-      )
-      .mul(index)
-      .div(BigNumber.from(10).pow(18));
+    const wsMinoScore = BigNumber.from(mmfUserInfosOld[address] ? BigNumber.from(mmfUserInfosOld[address]['amount']) : 0)
+    .add(mmfUserInfoNew[address] ? BigNumber.from(mmfUserInfoNew[address]['amount']) : 0)
+    .mul(index)
+    .div(BigNumber.from(10).pow(18))
 
-    const minoScore = wsMinoScore.add(sMinoBalances[address] || 0);
+    const minoScore = wsMinoScore
+      .add(sMinoBalances[address] || 0)
+      .add(minoBalances[address] || 0)
 
     scores[address] = minoScore;
   }
 
-  const scoresNumber = Object.fromEntries(
+  const scoresNumber =  Object.fromEntries(
     Object.entries(scores).map(([address, balance]) => [
       address,
-      balance.mul(BigNumber.from(10).pow(9)).div(minoTotalSupply).toNumber() /
-        1000000000
+      balance.toNumber() / 1000000000
     ])
   );
 
-  return scoresNumber;
+  return scoresNumber
 }
