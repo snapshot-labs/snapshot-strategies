5052e6987246e11fec8d753fc148668b48cde2b3
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-03-23 20:39:51 +0800

    Update on chain voting

--- src/strategies/cake/index.ts
+++ src/strategies/cake/index.ts
@@ -1,10 +1,36 @@
 import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
+import { formatUnits } from '@ethersproject/units';
+import { multicall } from '../../utils';
 
 export const author = 'pancake-swap';
 export const version = '0.0.1';
 
-const MINIUM_VOTING_POWER = 0.01;
 const CAKE_ADDRESS = '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82';
+
+const onChainVPBlockNumber = 16300686;
+const onChainVPAddress = '0xc0FeBE244cE1ea66d27D23012B3D616432433F42';
+
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '_user',
+        type: 'address'
+      }
+    ],
+    name: 'getVotingPowerWithoutPool',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    stateMutability: 'view',
+    type: 'function'
+  }
+];
 
 export async function strategy(
   space,
@@ -14,7 +40,31 @@ export async function strategy(
   options,
   snapshot
 ) {
-  const score = await erc20BalanceOfStrategy(
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+  if (
+    blockTag === 'latest' ||
+    (typeof blockTag === 'number' && blockTag >= onChainVPBlockNumber)
+  ) {
+    const response = await multicall(
+      network,
+      provider,
+      abi,
+      addresses.map((address: any) => [
+        onChainVPAddress,
+        'getVotingPowerWithoutPool',
+        [address.toLowerCase()]
+      ]),
+      { blockTag }
+    );
+    return Object.fromEntries(
+      response.map((value, i) => [
+        addresses[i],
+        parseFloat(formatUnits(value.toString(), options.decimals))
+      ])
+    );
+  }
+
+  return erc20BalanceOfStrategy(
     space,
     network,
     provider,
@@ -25,12 +75,5 @@ export async function strategy(
       decimals: 18
     },
     snapshot
-  );
-
-  return Object.fromEntries(
-    Object.entries(score).map((address: any) => [
-      address[0],
-      address[1] > MINIUM_VOTING_POWER ? address[1] : 0
-    ])
   );
 }
