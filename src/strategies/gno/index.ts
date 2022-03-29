ce5f9ac062ac3ecdd5618dd52cf32923584ab519
Auryn Macmillan <hi@auryn.xyz>
2022-03-28 22:23:14 -0400

    fix user id case

--- src/strategies/gno/index.ts
+++ src/strategies/gno/index.ts
@@ -1,7 +1,8 @@
 import { subgraphRequest } from '../../utils';
+const { getAddress } = require('@ethersproject/address');
 
 export const author = 'nginnever';
-export const version = '0.1.0';
+export const version = '0.1.1';
 
 export async function strategy(
   _space,
@@ -11,10 +12,14 @@ export async function strategy(
   options,
   snapshot
 ) {
+  const adds = addresses.map((element) => {
+    return element.toLowerCase();
+  });
+
   const params = {
     users: {
       __args: {
-        where: { id_in: addresses },
+        where: { id_in: adds },
         first: 1000
       },
       id: true,
@@ -28,7 +33,7 @@ export async function strategy(
   const result = await subgraphRequest(options.SUBGRAPH_URL, params);
   const score = {};
   result.users.map((user) => {
-    score[user.id] = Number(user.voteWeight);
+    score[getAddress(user.id)] = Number(user.voteWeight);
   });
   return score || {};
 }
