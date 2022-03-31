0860e6ba61eb295e40d7a6e6c4486cfddcf93ec5
Charles L <prometheus.charles916@gmail.com>
2022-03-31 13:43:00 -0400

    fix: update balanceOf to erc20-balance-of

--- src/strategies/genomesdao/index.ts
+++ src/strategies/genomesdao/index.ts
@@ -1,12 +1,12 @@
 import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
-import { formatUnits } from '@ethersproject/units';
+import { formatUnits, parseUnits } from '@ethersproject/units';
+import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
 import { Multicaller } from '../../utils';
 
 export const author = 'Charles-repo';
 export const version = '0.1.2';
 
 const abi = [
-  'function balanceOf(address account) external view returns (uint256)',
   'function totalSupply() external view returns (uint256)',
   'function token0() external view returns (address)',
   'function getReserves() external view returns (uint112, uint112, uint32)',
@@ -25,35 +25,64 @@ export async function strategy(
   const votingAddress = "0x48D1CcB09f771788F59c8aAAB613936eDfA267b7"; // voting contract
 
   const multi = new Multicaller(network, provider, abi, { blockTag });
-  addresses.forEach((address: any) =>
-    multi.call(address, options.address, 'balanceOf', [address])
-  );
-  const result: Record<string, BigNumberish> = await multi.execute();
 
-  addresses.forEach((address: any) =>
-    multi.call(address, options.veaddress, 'balanceOf', [address])
+  const result = await erc20BalanceOfStrategy(
+    space,
+    network,
+    provider,
+    addresses,
+    options,
+    snapshot
   );
-  const veresult: Record<string, BigNumberish> = await multi.execute();
+
+  let options_ve = {...options};
+  options_ve.address = options.veaddress;
+  const veresult = await erc20BalanceOfStrategy(
+    space,
+    network,
+    provider,
+    addresses,
+    options_ve,
+    snapshot
+  );
 
   addresses.forEach((address: any) =>
     multi.call(address, votingAddress, 'lockEntries', [address])
   );
   const lockresult: Record<string, BigNumberish> = await multi.execute();
 
-  addresses.forEach((address: any) =>
-    multi.call(address, votingAddress, 'balanceOf', [address])
+  let options_voting = {...options};
+  options_voting.address = votingAddress;
+  const lockedamountresult = await erc20BalanceOfStrategy(
+    space,
+    network,
+    provider,
+    addresses,
+    options_voting,
+    snapshot
   );
-  const lockedamountresult: Record<string, BigNumberish> = await multi.execute();
 
-  addresses.forEach((address: any) =>
-    multi.call(address, options.lpaddress, 'balanceOf', [address])
+  let options_lp = {...options};
+  options_lp.address = options.lpaddress;
+  const resultLP1 = await erc20BalanceOfStrategy(
+    space,
+    network,
+    provider,
+    addresses,
+    options_lp,
+    snapshot
   );
-  const resultLP1: Record<string, BigNumberish> = await multi.execute();
 
-  addresses.forEach((address: any) =>
-    multi.call(address, options.harvaddress, 'balanceOf', [address])
+  let options_lp2 = {...options};
+  options_lp2.address = options.harvaddress;
+  const resultLP2 = await erc20BalanceOfStrategy(
+    space,
+    network,
+    provider,
+    addresses,
+    options_lp2,
+    snapshot
   );
-  const resultLP2: Record<string, BigNumberish> = await multi.execute();
 
   multi.call('token0', options.lpaddress, 'token0', []);
   multi.call('getReserves', options.lpaddress, 'getReserves', []);
@@ -68,15 +97,15 @@ export async function strategy(
 
   return Object.fromEntries(
     Object.entries(resultLP1).map(([address, balance]) => {
-      let bal: BigNumber = BigNumber.from(balance)
-        .add(resultLP2[address])
+      let bal: BigNumber = BigNumber.from(parseUnits(balance.toFixed(options.decimals), options.decimals))
+        .add(parseUnits(resultLP2[address].toFixed(options.decimals), options.decimals))
         .mul(totalGnomeAmount)
         .div(totalSupply);
-      bal = bal.add(result[address]);
+      bal = bal.add(parseUnits(result[address].toFixed(options.decimals), options.decimals));
       if (BigNumber.from(lockresult[address][1]).gte(365))
-        bal = bal.add(BigNumber.from(veresult[address]).mul(5));
+        bal = bal.add(BigNumber.from(parseUnits(veresult[address].toFixed(options.decimals), options.decimals)).mul(5));
       if (BigNumber.from(lockresult[address][1]).gte(1)) {
-        bal = bal.add(BigNumber.from(lockedamountresult[address]).mul(110).div(100));
+        bal = bal.add(BigNumber.from(parseUnits(lockedamountresult[address].toFixed(options.decimals), options.decimals)).mul(110).div(100));
       }
       return [address, parseFloat(formatUnits(bal, options.decimals))];
     })
