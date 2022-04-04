65429ba12fb7b25e6215df9aa3cdf78e7fb3ed72
andreymasyuk <andriy0694@mail.ru>
2022-04-04 21:35:03 +0300

    added new strategy with staking

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -25,6 +25,7 @@ import * as erc20WithBalance from './erc20-with-balance';
 import * as erc20BalanceOfDelegation from './erc20-balance-of-delegation';
 import * as erc20BalanceOfQuadraticDelegation from './erc20-balance-of-quadratic-delegation';
 import * as erc20BalanceOfWeighted from './erc20-balance-of-weighted';
+import * as erc20BalanceOfStaked from './erc20-balance-of-staked';
 import * as erc20BalanceOfIndexed from './erc20-balance-of-indexed';
 import * as revest from './revest';
 import * as erc20Price from './erc20-price';
@@ -324,6 +325,7 @@ const strategies = {
   'erc20-balance-of-delegation': erc20BalanceOfDelegation,
   'erc20-balance-of-quadratic-delegation': erc20BalanceOfQuadraticDelegation,
   'erc20-balance-of-weighted': erc20BalanceOfWeighted,
+  'erc20-balance-of-staked': erc20BalanceOfStaked,
   'erc20-balance-of-indexed': erc20BalanceOfIndexed,
   'erc20-price': erc20Price,
   'balance-of-with-min': balanceOfWithMin,
