051481c273b6fcdb65486b88d8a8218e18c324d0
Thijmen Maus <thijmen@maus.nu>
2022-04-23 01:40:14 +0200

    Update calculation strategy - Updated the calculation strategy to work better with more addresses - Updated BatchUtils contract - Added a treshold

--- src/strategies/samurailegends-generals-balance/index.ts
+++ src/strategies/samurailegends-generals-balance/index.ts
@@ -1,10 +1,10 @@
 import { Multicaller } from '../../utils';
 
 export const author = 'Samurai-Legends';
-export const version = '0.1.2';
+export const version = '0.2.0';
 
 const abi = [
-  'function erc721GetAllTokensOfOwner(address nftAddress, address user) external view returns (uint[] memory)'
+  'function erc721BatchOwnerOf(address nftAddress, uint idMin, uint idMax) external view returns (address[] memory)'
 ];
 
 export async function strategy(
@@ -18,20 +18,26 @@ export async function strategy(
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
   const multi = new Multicaller(network, provider, abi, { blockTag });
-  addresses.forEach((address) =>
-    multi.call(address, options.address, 'erc721GetAllTokensOfOwner', [
-      options.nftAddress,
-      address
-    ])
+
+  multi.call('batch.owners', options.batchAddress, 'erc721BatchOwnerOf', [
+    options.nftAddress,
+    '0',
+    options.treshold.toString()
+  ]);
+  const result = await multi.execute();
+
+  const balances = result.batch.owners.reduce(
+    (prev: Record<string, number>, curr: string) => {
+      if (curr in prev) prev[curr] += 1;
+      else prev[curr] = 1;
+      return prev;
+    },
+    {}
   );
-  const result: Record<string, number[]> = await multi.execute();
 
   return Object.fromEntries(
-    Object.entries(result).map(([address, nftBalance]) => {
-      const balance = nftBalance.reduce((prev, curr) => {
-        if (curr >= 0 && curr < 5000) return prev + 1;
-        return prev;
-      }, 0);
+    addresses.map((address: string) => {
+      const balance = balances[address] || 0;
       return [address, balance * options.multiplier];
     })
   );
