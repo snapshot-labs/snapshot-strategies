a79c202fddc06c271cb7d48cdced80b19cbec6e9
spicysquid168 <spicysquid168@gmail.com>
2022-04-26 11:49:32 +0700

    fix: chunking ve-balance-of-at

--- src/strategies/ve-balance-of-at/index.ts
+++ src/strategies/ve-balance-of-at/index.ts
@@ -21,10 +21,16 @@ export async function strategy(
     typeof snapshot === 'number' ? snapshot : await provider.getBlockNumber();
 
   const multi = new Multicaller(network, provider, abi);
-  addresses.forEach((address) =>
-    multi.call(address, options.address, 'balanceOfAt', [address, blockTag])
-  );
-  const result: Record<string, BigNumberish> = await multi.execute();
+  let result: Record<string, BigNumberish> = {};
+
+  const chunkSize = 200;
+  for (let i = 0; i < addresses.length; i += chunkSize) {
+    const chunk = addresses.slice(i, i + chunkSize);
+    chunk.forEach((address) =>
+      multi.call(address, options.address, 'balanceOfAt', [address, blockTag])
+    );
+    result = await multi.execute();
+  }
 
   return Object.fromEntries(
     Object.entries(result).map(([address, balance]) => [
