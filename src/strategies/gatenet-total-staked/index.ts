dd9f114b5a05ab810f0dd104d5180ec20da7e2e5
Sagar Upadhyay <usagar80@gmail.com>
2022-04-29 17:21:18 +0530

    Update strategy logic and examples

--- src/strategies/gatenet-total-staked/index.ts
+++ src/strategies/gatenet-total-staked/index.ts
@@ -1,6 +1,7 @@
 import { Multicaller, subgraphRequest } from '../../utils';
 import abis from './abis/Compound';
-import { BigNumber } from '@ethersproject/bignumber';
+import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
+import { formatUnits } from '@ethersproject/units';
 
 export const author = 'usagar80';
 export const version = '0.0.1';
@@ -44,54 +45,54 @@ export async function strategy(
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
   const multi = new Multicaller(network, provider, abi, { blockTag });
-  addresses.forEach((address) => {
-    multi.call('lockPeriod', options.address, 'LOCK_PERIOD');
-  });
+  multi.call('lockPeriod', options.address, 'LOCK_PERIOD');
   const multiResult = await multi.execute();
   const lockPeriod = Number(multiResult.lockPeriod);
 
-  const result = {};
-  for (const address of addresses) {
-    console.log(address);
-    const args: {
-      where: { sender: string };
-    } = {
-      where: {
-        sender: address
-      }
-    };
+  const result: Record<string, BigNumberish> = {};
+  const args: {
+    where: { sender_in: any };
+  } = {
+    where: {
+      sender_in: addresses
+    }
+  };
 
-    const query = {
-      compoundDeposits: {
-        __args: args,
-        id: true,
-        sender: true,
-        amount: true,
-        shares: true,
-        time: true
-      },
-      compoundWithdraws: {
-        __args: args,
-        id: true,
-        sender: true,
-        amount: true,
-        shares: true,
-        time: true
-      },
-      stakings: {
-        id: true,
-        txName: true,
-        amount: true,
-        user: true,
-        time: true,
-        unStakeIndex: true
-      }
-    };
-    const transactionsList = await subgraphRequest(
-      'https://api.studio.thegraph.com/query/17252/gatenet-cvm/v0.6.1',
-      query
-    );
-    console.log(transactionsList);
+  const query = {
+    compoundDeposits: {
+      __args: args,
+      __typename: true,
+      id: true,
+      sender: true,
+      amount: true,
+      shares: true,
+      time: true
+    },
+    compoundWithdraws: {
+      __args: args,
+      __typename: true,
+      id: true,
+      sender: true,
+      amount: true,
+      shares: true,
+      time: true
+    },
+    stakings: {
+      __typename: true,
+      id: true,
+      txName: true,
+      amount: true,
+      user: true,
+      time: true,
+      unStakeIndex: true
+    }
+  };
+  const transactionsList = await subgraphRequest(
+    'https://api.studio.thegraph.com/query/17252/gatenet-cvm/v0.6.1',
+    query
+  );
+
+  for (const address of addresses) {
     let feePerShare = BigNumber.from(0);
     let rewardRate = BigNumber.from(0);
     let waitingFees = BigNumber.from(0);
@@ -99,137 +100,146 @@ export async function strategy(
     let currentShares = BigNumber.from(0);
     let transactionTypeTemp = '';
     let sign = '~';
+    const ether = BigNumber.from(10).pow(18);
 
-    const rawTransactions = transactionsList.compoundDeposits
-      .concat(transactionsList.compoundWithdraws)
-      .concat(transactionsList.stakings)
-      .sort(function (a, b) {
-        return a.time - b.time;
-      });
-    const transactions = rawTransactions
-      .map((transaction) => {
-        const type = getTransactionType(transaction);
-        if (type !== OTHERS) {
-          let amount;
-          switch (type) {
-            case STAKE: {
-              currentShares = BigNumber.from(transaction.shares).add(
-                currentShares
-              );
-              amount = BigNumber.from(transaction.amount);
-              sign = '+';
-              break;
-            }
-
-            case FEE_DISTRIBUTION: {
-              const totalShares = BigNumber.from(transaction.unStakeIndex);
-              const transactionAmount = BigNumber.from(transaction.amount);
-              const previousFeePerShare = feePerShare;
-              /// 0
-              feePerShare = previousFeePerShare.add(
-                transactionAmount.mul('1e18').div(totalShares)
-              );
-              // 0
-              amount = currentShares.mul(
-                feePerShare.sub(previousFeePerShare).div('1e18')
-              );
-              waitingFees = waitingFees.add(amount);
-              sign = '+';
-              break;
-            }
-
-            case REWARD_ADDED: {
-              const rewardRateIncrease = BigNumber.from(
-                transaction.unStakeIndex
-              );
-              const previousRewardRate = rewardRate;
-              rewardRate = rewardRate
-                .add(rewardRateIncrease)
-                .add(previousRewardRate);
-
-              amount = currentShares.mul(rewardRate.sub(previousRewardRate));
-              waitingRewards = waitingRewards.add(amount);
-              sign = '+';
-              break;
-            }
-            case UNSTAKE: {
-              currentShares = currentShares.sub(
-                BigNumber.from(transaction.shares || 0)
-              );
-              amount = BigNumber.from(transaction.amount);
-              sign = '-';
-              break;
-            }
-            case 'Claim': {
-              if (transaction.user.toUpperCase() === address.toUpperCase()) {
-                if (transactionTypeTemp !== UNSTAKE) {
-                  amount = BigNumber.from(transaction.amount);
-                  sign = '-';
-                }
-              }
-              break;
-            }
-          }
-          transactionTypeTemp = type;
-          //          amount = amount.toNumber();
-          return amount
-            ? {
-                name: type,
-                timeStamp: transaction.time,
-                time: new Date(transaction.time * 1000).toLocaleTimeString(),
-                amount,
-                date: new Date(transaction.time * 1000).toLocaleString(
-                  'en-GB',
-                  {
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric'
-                  }
-                ),
-                sign
-              }
-            : null;
-        }
-      })
-      .filter((transaction) => transaction)
-      .reverse();
-    // Copied Content Start
-    const firstUnstakeIndex = transactions.findIndex(
-      (x) => x.name === 'Unstake'
+    const compoundDeposits = transactionsList.compoundDeposits.filter(
+      (s) => s.sender.toLowerCase() === address.toLowerCase()
     );
-    let filtered = transactions;
-    if (firstUnstakeIndex >= 0)
-      filtered = transactions.slice(0, firstUnstakeIndex);
-    const stake = filtered
-      .filter((t) => t.name === 'Stake')
-      .reduce((acc, transaction) => {
-        return acc + transaction.amount;
-      }, 0);
-    const firstUnstaked = transactions[firstUnstakeIndex];
-    let lockedTransaction = transactions
-      .slice(firstUnstakeIndex)
-      .filter(
-        (t) =>
-          firstUnstaked &&
-          firstUnstaked.timeStamp - t.timeStamp <= lockPeriod &&
-          t.name === 'Stake'
-      )
-      .reduce((acc, t) => (acc += Number(t.amount)), 0);
-    if (!lockedTransaction) lockedTransaction = 0;
+    const compoundWithdraws = transactionsList.compoundWithdraws.filter(
+      (s) => s.sender.toLowerCase() === address.toLowerCase()
+    );
 
-    console.log(stake);
-    console.log(lockedTransaction);
-    result[address] = stake + lockedTransaction;
-    // Copied Content End
-    //gatenet-total-staked
+    if (compoundWithdraws.length > 0 && compoundDeposits.length > 0) {
+      const rawTransactions = compoundDeposits
+        .concat(compoundWithdraws)
+        .concat(transactionsList.stakings)
+        .sort(function (a, b) {
+          return a.time - b.time;
+        });
+      const transactions = rawTransactions
+        .map((transaction) => {
+          const type = getTransactionType(transaction);
+          if (type !== OTHERS) {
+            let amount;
+            switch (type) {
+              case STAKE: {
+                currentShares = BigNumber.from(transaction.shares).add(
+                  currentShares
+                );
+                amount = BigNumber.from(transaction.amount);
+                sign = '+';
+                break;
+              }
+
+              case FEE_DISTRIBUTION: {
+                const totalShares = BigNumber.from(transaction.unStakeIndex);
+                const transactionAmount = BigNumber.from(transaction.amount);
+                const previousFeePerShare = feePerShare;
+                /// 0
+                feePerShare = previousFeePerShare.add(
+                  transactionAmount.mul(ether).div(totalShares)
+                );
+                // 0
+                amount = currentShares.mul(
+                  feePerShare.sub(previousFeePerShare).div(ether)
+                );
+                waitingFees = waitingFees.add(amount);
+                sign = '+';
+                break;
+              }
+
+              case REWARD_ADDED: {
+                const rewardRateIncrease = BigNumber.from(
+                  transaction.unStakeIndex
+                );
+                const previousRewardRate = rewardRate;
+                rewardRate = rewardRate
+                  .add(rewardRateIncrease)
+                  .add(previousRewardRate);
+
+                amount = currentShares.mul(rewardRate.sub(previousRewardRate));
+                waitingRewards = waitingRewards.add(amount);
+                sign = '+';
+                break;
+              }
+              case UNSTAKE: {
+                currentShares = currentShares.sub(
+                  BigNumber.from(transaction.shares || 0)
+                );
+                amount = BigNumber.from(transaction.amount);
+                sign = '-';
+                break;
+              }
+              case 'Claim': {
+                if (transaction.user.toUpperCase() === address.toUpperCase()) {
+                  if (transactionTypeTemp !== UNSTAKE) {
+                    amount = BigNumber.from(transaction.amount);
+                    sign = '-';
+                  }
+                }
+                break;
+              }
+            }
+            transactionTypeTemp = type;
+            //          amount = amount.toNumber();
+            return amount
+              ? {
+                  name: type,
+                  timeStamp: transaction.time,
+                  time: new Date(transaction.time * 1000).toLocaleTimeString(),
+                  amount,
+                  date: new Date(transaction.time * 1000).toLocaleString(
+                    'en-GB',
+                    {
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric'
+                    }
+                  ),
+                  sign
+                }
+              : null;
+          }
+        })
+        .filter((transaction) => transaction)
+        .reverse();
+      // Copied Content Start
+      const firstUnstakeIndex = transactions.findIndex(
+        (x) => x.name === UNSTAKE
+      );
+      let filtered = transactions;
+      if (firstUnstakeIndex >= 0)
+        filtered = transactions.slice(0, firstUnstakeIndex);
+      const stake: BigNumberish = filtered
+        .filter((t) => t.name === STAKE)
+        .reduce((acc: BigNumberish, transaction) => {
+          return BigNumber.from(transaction.amount).add(acc);
+        }, BigNumber.from(0));
+      const firstUnstaked = transactions[firstUnstakeIndex];
+      let lockedTransaction: BigNumberish = transactions
+        .slice(firstUnstakeIndex)
+        .filter(
+          (t) =>
+            firstUnstaked &&
+            firstUnstaked.timeStamp - t.timeStamp <= lockPeriod &&
+            t.name === STAKE
+        )
+        .reduce((acc: BigNumberish, t) => {
+          return BigNumber.from(t.amount).add(acc);
+        }, BigNumber.from(0));
+      if (!lockedTransaction) lockedTransaction = 0;
+
+      result[address] = BigNumber.from(0).add(stake).add(lockedTransaction);
+    } else {
+      result[address] = BigNumber.from(0);
+    }
   }
-
-  console.log(result);
-
-  return Object.fromEntries(
+  const retObj = Object.fromEntries(
     Object.entries(result).map(([address, balance]) => [
       address,
-      Number(balance)
+      parseFloat(formatUnits(balance, options.decimals))
     ])
   );
+
+  return retObj;
 }
