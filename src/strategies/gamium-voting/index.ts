f2d2017eb93b1d2df73dc7c50f4d6d5aea0d7c19
GamiumWorld <103736043+GamiumWorld@users.noreply.github.com>
2022-05-12 21:34:39 +0200

    Update src/strategies/gamium-voting/index.ts
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/strategies/gamium-voting/index.ts
+++ src/strategies/gamium-voting/index.ts
@@ -5,9 +5,7 @@ import { Multicaller } from '../../utils';
 export const author = 'gamiumworld';
 export const version = '0.1.0';
 
-const tokenAbi = [
-  { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "balance", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }
-];
+const tokenAbi = [ 'function balanceOf(address _owner) view returns (uint256 balance)' ];
 const stakingAbi = [
   { "inputs": [ { "internalType": "address", "name": "user", "type": "address" } ], "name": "totalStakeTokenDeposited", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function", "constant": true }
 ];
