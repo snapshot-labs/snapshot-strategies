fec3f807f7c5337ff270716854a3147b1d1fcec7
ChaituVR <ChaituVR@users.noreply.github.com>
2022-05-22 10:16:28 +0000

    Automated lint

--- src/strategies/gamium-voting/index.ts
+++ src/strategies/gamium-voting/index.ts
@@ -5,7 +5,9 @@ import { Multicaller } from '../../utils';
 export const author = 'gamiumworld';
 export const version = '0.1.0';
 
-const tokenAbi = [ 'function balanceOf(address _owner) view returns (uint256 balance)' ];
+const tokenAbi = [
+  'function balanceOf(address _owner) view returns (uint256 balance)'
+];
 const stakingAbi = [
   'function totalStakeTokenDeposited(address user) view returns (uint256)'
 ];
@@ -24,29 +26,68 @@ export async function strategy(
 ) {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
-  options.token = options.token || "0x5B6bf0c7f989dE824677cFBD507D9635965e9cD3";
-  options.lp_token = options.lp_token || "0xEdeec0ED10Abee9b5616bE220540CAb40C9d991E";
-  options.staking_token = options.staking_token || "0x8a3FB54dE0df64915FD66B55e1594141C1A880AB";
-  options.staking_pair = options.staking_pair || "0xaD0916e7Ba7100629EAe9143e035F98ab5EA4ABd";
-  options.symbol = options.symbol || "GMM";
-  options.decimals = options.decimals || 18;  
+  options.token = options.token || '0x5B6bf0c7f989dE824677cFBD507D9635965e9cD3';
+  options.lp_token =
+    options.lp_token || '0xEdeec0ED10Abee9b5616bE220540CAb40C9d991E';
+  options.staking_token =
+    options.staking_token || '0x8a3FB54dE0df64915FD66B55e1594141C1A880AB';
+  options.staking_pair =
+    options.staking_pair || '0xaD0916e7Ba7100629EAe9143e035F98ab5EA4ABd';
+  options.symbol = options.symbol || 'GMM';
+  options.decimals = options.decimals || 18;
 
-  const liquidityPoolMulticaller = new Multicaller(network, provider, liquidityPoolAbi, { blockTag });
+  const liquidityPoolMulticaller = new Multicaller(
+    network,
+    provider,
+    liquidityPoolAbi,
+    { blockTag }
+  );
 
-  liquidityPoolMulticaller.call('lpTotalSupply', options.lp_token, 'totalSupply');
+  liquidityPoolMulticaller.call(
+    'lpTotalSupply',
+    options.lp_token,
+    'totalSupply'
+  );
   liquidityPoolMulticaller.call('lpReserves', options.lp_token, 'getReserves');
 
-  const { lpTotalSupply, lpReserves } = await liquidityPoolMulticaller.execute();
+  const {
+    lpTotalSupply,
+    lpReserves
+  } = await liquidityPoolMulticaller.execute();
 
-  const liquidityPoolTokenRatio = parseFloat(formatUnits(lpReserves[0], options.decimals)) / parseFloat(formatUnits(lpTotalSupply, options.decimals));
+  const liquidityPoolTokenRatio =
+    parseFloat(formatUnits(lpReserves[0], options.decimals)) /
+    parseFloat(formatUnits(lpTotalSupply, options.decimals));
 
-  const tokenMulticaller = new Multicaller(network, provider, tokenAbi, { blockTag });
-  const stakingTokenMulticaller = new Multicaller(network, provider, stakingAbi, { blockTag });
-  const stakingPairMulticaller = new Multicaller(network, provider, stakingAbi, { blockTag });
+  const tokenMulticaller = new Multicaller(network, provider, tokenAbi, {
+    blockTag
+  });
+  const stakingTokenMulticaller = new Multicaller(
+    network,
+    provider,
+    stakingAbi,
+    { blockTag }
+  );
+  const stakingPairMulticaller = new Multicaller(
+    network,
+    provider,
+    stakingAbi,
+    { blockTag }
+  );
 
   addresses.forEach((address) => {
-    stakingPairMulticaller.call(address, options.staking_pair, 'totalStakeTokenDeposited', [address]);
-    stakingTokenMulticaller.call(address, options.staking_token, 'totalStakeTokenDeposited', [address]);
+    stakingPairMulticaller.call(
+      address,
+      options.staking_pair,
+      'totalStakeTokenDeposited',
+      [address]
+    );
+    stakingTokenMulticaller.call(
+      address,
+      options.staking_token,
+      'totalStakeTokenDeposited',
+      [address]
+    );
     tokenMulticaller.call(address, options.token, 'balanceOf', [address]);
   });
 
@@ -54,13 +95,25 @@ export async function strategy(
     Record<string, BigNumberish>,
     Record<string, BigNumberish>,
     Record<string, BigNumberish>
-  ] = await Promise.all([stakingPairMulticaller.execute(), stakingTokenMulticaller.execute(), tokenMulticaller.execute()]);
+  ] = await Promise.all([
+    stakingPairMulticaller.execute(),
+    stakingTokenMulticaller.execute(),
+    tokenMulticaller.execute()
+  ]);
 
   return Object.fromEntries(
     addresses.map((address) => {
-      const tokenBalance = parseFloat(formatUnits(tokenResponse[address], options.decimals));
-      const stakingTokenBalance = parseFloat(formatUnits(stakingTokenResponse[address], options.decimals));
-      const stakingPairBalance = parseFloat(formatUnits(stakingPairResponse[address], options.decimals)) * (1 + liquidityPoolTokenRatio);
+      const tokenBalance = parseFloat(
+        formatUnits(tokenResponse[address], options.decimals)
+      );
+      const stakingTokenBalance = parseFloat(
+        formatUnits(stakingTokenResponse[address], options.decimals)
+      );
+      const stakingPairBalance =
+        parseFloat(
+          formatUnits(stakingPairResponse[address], options.decimals)
+        ) *
+        (1 + liquidityPoolTokenRatio);
       return [address, tokenBalance + stakingTokenBalance + stakingPairBalance];
     })
   );
