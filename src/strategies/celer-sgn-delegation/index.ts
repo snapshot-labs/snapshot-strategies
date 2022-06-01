32192f286bf87e38fd43dc905e6e87dd71951fd2
Michael Zhou <zhoumotongxue008@gmail.com>
2022-06-02 07:03:12 +0800

    Add SGN V2 delegations to celer-sgn-delegation

--- src/strategies/celer-sgn-delegation/index.ts
+++ src/strategies/celer-sgn-delegation/index.ts
@@ -1,11 +1,13 @@
 import { BigNumber } from '@ethersproject/bignumber';
+import { Provider } from '@ethersproject/providers';
 import { formatUnits } from '@ethersproject/units';
+
 import { multicall } from '../../utils';
 
 export const author = 'dominator008';
-export const version = '0.1.0';
+export const version = '0.2.0';
 
-const abi = [
+const v1StakingABI = [
   {
     constant: true,
     inputs: [],
@@ -86,35 +88,63 @@ const abi = [
   }
 ];
 
+const v2StakingABI = [
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '_delAddr',
+        type: 'address'
+      }
+    ],
+    name: 'getDelegatorTokens',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256'
+      },
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    stateMutability: 'view',
+    type: 'function'
+  }
+];
+
 export async function strategy(
-  space,
-  network,
-  provider,
+  space: string,
+  network: string,
+  provider: Provider,
   addresses: string[],
-  options,
-  snapshot
+  options: { v1StakingAddress: string; v2StakingViewerAddress: string },
+  snapshot: string | number
 ) {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
-  // 1. Get the number of validators
+  // Staking V1
+  // 1.1. Get the number of validators
   const validatorNum: BigNumber = (
     await multicall(
       network,
       provider,
-      abi,
-      [[options.dposAddress, 'getValidatorNum', []]],
+      v1StakingABI,
+      [[options.v1StakingAddress, 'getValidatorNum', []]],
       { blockTag }
     )
   )[0][0];
 
-  // 2. Get the addresses of the validators
+  // 1.2. Get the addresses of the validators
   const validatorAddresses: string[] = (
     await multicall(
       network,
       provider,
-      abi,
+      v1StakingABI,
       Array.from(Array(validatorNum.toNumber()).keys()).map((index: number) => [
-        options.dposAddress,
+        options.v1StakingAddress,
         'validatorSet',
         [index]
       ]),
@@ -122,13 +152,17 @@ export async function strategy(
     )
   ).map((value: string[]) => value[0]);
 
-  // 2. Get the delegation of all addresses to all validators
+  // 1.3. Get the delegation of all addresses to all validators
   const callInfos = validatorAddresses.reduce<any[]>(
     (infos, validatorAddress) =>
       infos.concat(
         addresses.map((address: string) => [
           address,
-          [options.dposAddress, 'getDelegatorInfo', [validatorAddress, address]]
+          [
+            options.v1StakingAddress,
+            'getDelegatorInfo',
+            [validatorAddress, address]
+          ]
         ])
       ),
     []
@@ -144,14 +178,14 @@ export async function strategy(
       await multicall(
         network,
         provider,
-        abi,
+        v1StakingABI,
         batches[i].map((info) => info[1]),
         { blockTag }
       )
     );
   }
 
-  // 3. For each address, aggregate the delegations to each validator
+  // 1.4. For each address, aggregate the delegations to each validator
   const delegations = delegatorInfoResponse.map((info, i) => [
     callInfos[i][0],
     info.delegatedStake
@@ -167,11 +201,38 @@ export async function strategy(
     }
     return aggregates;
   }, {});
+
+  // Staking V2
+  // 2.1. Get delegator tokens for all addresses
+  const v2DelegatorTokens: string[] = (
+    await multicall(
+      network,
+      provider,
+      v2StakingABI,
+      addresses.map((address: string) => [
+        options.v2StakingViewerAddress,
+        'getDelegatorTokens',
+        [address]
+      ]),
+      { blockTag }
+    )
+  ).map((value: string[]) => value[0]);
+  const v2DelegatorTokensMap = addresses.reduce((map, address, i, _) => {
+    map[address] = v2DelegatorTokens[i];
+    return map;
+  }, {});
+
+  // 3. Sum up V1 and V2 delegations
   return Object.entries<BigNumber>(aggregatedDelegations).reduce(
     (transformed, [delegatorAddress, delegatedStake]) => {
       transformed[delegatorAddress] = parseFloat(
         formatUnits(delegatedStake.toString(), 18)
       );
+      if (v2DelegatorTokensMap[delegatorAddress]) {
+        transformed[delegatorAddress] += parseFloat(
+          formatUnits(v2DelegatorTokensMap[delegatorAddress].toString(), 18)
+        );
+      }
       return transformed;
     },
     {}
