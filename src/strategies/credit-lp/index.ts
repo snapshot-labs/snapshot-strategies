05c13ae4e265a58cc521c92e234864e3f1663976
Chaitanya <yourchaitu@gmail.com>
2022-06-07 23:54:39 +0530

    Update src/strategies/credit-lp/index.ts

--- src/strategies/credit-lp/index.ts
+++ src/strategies/credit-lp/index.ts
@@ -7,40 +7,10 @@ export const version = '0.1.0';
 
 
 const abi = [
-  {
-    inputs: [
-      { internalType: 'uint256', name: '_pid', type: 'uint256' },
-      { internalType: 'address', name: '_user', type: 'address' }
-    ],
-    name: 'getUserInfo',
-    outputs: [
-      {
-        components: [
-          { internalType: 'uint256', name: 'amount', type: 'uint256' },
-          { internalType: 'uint256[]', name: 'RewardDebt', type: 'uint256[]' },
-          {
-            internalType: 'uint256[]',
-            name: 'RemainingRewards',
-            type: 'uint256[]'
-          }
-        ],
-        internalType: 'struct SteakHouseV2.UserInfo',
-        name: '',
-        type: 'tuple'
-      }
-    ],
-    stateMutability: 'view',
-    type: 'function'
-  },
-  {
-    inputs: [],
-    name: 'getPricePerFullShare',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    stateMutability: 'view',
-    type: 'function'
-  },
-  'function balanceOf(address account) external view returns (uint256)',
-  'function totalSupply() external view returns (uint256)'
+  'function getUserInfo(uint256 _pid, address _user) view returns (tuple(uint256 amount, uint256[] RewardDebt, uint256[] RemainingRewards))',
+  'function getPricePerFullShare() view returns (uint256)',
+  'function balanceOf(address account) view returns (uint256)',
+  'function totalSupply() view returns (uint256)'
 ];
 
 export async function strategy(
