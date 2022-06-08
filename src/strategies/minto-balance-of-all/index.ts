2e2bf047d5d83ff3862e9c698f94b68de410452b
Arsenii <backend@cryptouniverse.io>
2022-06-08 19:42:05 +0300

    remake strategy

--- src/strategies/minto-balance-of-all/index.ts
+++ src/strategies/minto-balance-of-all/index.ts
@@ -1,20 +1,7 @@
-import { formatUnits } from '@ethersproject/units';
-import { Multicaller } from '../../utils';
+import fetch from 'cross-fetch';
 
 export const author = 'btcmt-minto';
 export const version = '0.1.0';
-
-const abi = [
-  'function balanceOfSum(address account) external view returns (uint256)'
-];
-
-const stakingAbi = [
-  'function userStakes(address account) external view returns (uint256, uint256, uint256, uint256, uint256, uint256, uint256)'
-];
-
-const autoStakingAbi = [
-  'function userStake(address account) external view returns (uint256, uint256, uint256)'
-];
 
 export async function strategy(
   space,
@@ -24,40 +11,20 @@ export async function strategy(
   options,
   snapshot
 ): Promise<Record<string, number>> {
-  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+  const MINTO_VOTE_API_URL = options.mintoVoteUrl;
 
-  const multi = new Multicaller(network, provider, abi, { blockTag });
-
-  const multiStaking = new Multicaller(network, provider, stakingAbi, {
-    blockTag
+  const response = await fetch(MINTO_VOTE_API_URL, {
+    method: 'POST',
+    headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      addresses: addresses
+    })
   });
-  const multiAutoStaking = new Multicaller(network, provider, autoStakingAbi, {
-    blockTag
-  });
-
-  addresses.forEach((address) => {
-    multi.call(address, options.address, 'balanceOfSum', [address]);
-    multiStaking.call(address, options.stakingAddress, 'userStakes', [address]);
-    multiAutoStaking.call(address, options.autoStakingAddress, 'userStake', [
-      address
-    ]);
-  });
-
-  const [result, resultStaking, resultAutoStaking] = await Promise.all([
-    multi.execute(),
-    multiStaking.execute(),
-    multiAutoStaking.execute()
-  ]);
+  const data = await response.json();
 
   return Object.fromEntries(
     addresses.map((address) => {
-      const sum =
-        parseFloat(formatUnits(result[address], options.decimals)) +
-        parseFloat(formatUnits(resultStaking[address][1], options.decimals)) +
-        parseFloat(
-          formatUnits(resultAutoStaking[address][0], options.decimals)
-        );
-      return [address, sum];
+      return [address, data[address]];
     })
   );
 }
