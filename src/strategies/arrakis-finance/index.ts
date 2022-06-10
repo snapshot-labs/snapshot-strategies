67d81c72e16fdc7d248248dfcfadfd4cbbe49e67
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-06-10 19:48:45 -0400

    [WIP] Begin implementing arrakis-finance strategy

--- src/strategies/arrakis-finance/index.ts
+++ src/strategies/arrakis-finance/index.ts
@@ -2,11 +2,15 @@ import { BigNumberish } from '@ethersproject/bignumber';
 import { formatUnits } from '@ethersproject/units';
 import { Multicaller } from '../../utils';
 
-export const author = 'bonustrack';
-export const version = '0.1.1';
+export const author = 'MantisClone';
+export const version = '0.1.0';
 
 const abi = [
-  'function balanceOf(address account) external view returns (uint256)'
+  'function token0() external view returns (address)',
+  'function token1() external view returns (address)',
+  'function balanceOf(address account) public view returns (uint256)',
+  'function totalSupply() public view returns (uint256)',
+  'function getUnderlyingBalances() external view returns (uint256 amount0Current, uint256 amount1Current)'
 ];
 
 export async function strategy(
@@ -20,13 +24,37 @@ export async function strategy(
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
   const multi = new Multicaller(network, provider, abi, { blockTag });
+  multi.call('token0', options.poolAddress, 'token0', []);
+  multi.call('token1', options.poolAddress, 'token1', []);
+  multi.call('underlyingBalances', options.poolAddress, 'getUnderlyingBalances', []);
+  multi.call('lpTokenTotalSupply', options.poolAddress, 'totalSupply', []);
   addresses.forEach((address) =>
-    multi.call(address, options.address, 'balanceOf', [address])
+    multi.call(`lpTokenBalances.${address}`, options.poolAddress, 'balanceOf', [address])
   );
-  const result: Record<string, BigNumberish> = await multi.execute();
+  const result = await multi.execute();
+
+  const token0 = result.token0;
+  const token1 = result.token1;
+  const underlyingBalances = result.underlyingBalances;
+  const lpTokenTotalSupply = result.lpTokenTotalSupply;
+  const lpTokenBalances = result.lpTokenBalances;
+
+  // if options.tokenAddress == token0:
+  //   index = 0
+  // elif options.tokenAddress == token1:
+  //   index = 1
+  // else:
+    //Emit error
+  let index = 0;
+
+  console.log(token0)
+  console.log(token1)
+  console.log(underlyingBalances)
+  console.log(lpTokenTotalSupply)
+  console.log(lpTokenBalances)
 
   return Object.fromEntries(
-    Object.entries(result).map(([address, balance]) => [
+    Object.entries(lpTokenBalances).map(([address, balance]) => [
       address,
       parseFloat(formatUnits(balance, options.decimals))
     ])
