3a3faa91127a8d3af3bbeadc32ba122c985c73bf
andreibadea20 <andrei.badea@softbinator.com>
2022-06-28 17:08:14 +0300

    strategy finished

--- src/strategies/dps-nft/index.ts
+++ src/strategies/dps-nft/index.ts
@@ -1,12 +1,44 @@
 import { getAddress } from '@ethersproject/address';
 import { subgraphRequest } from '../../utils';
 
-export const author = 'andreibadea20';
-export const version = '0.1.0';
-
+// add some information required
+export const author = 'andreibadea20'; 
+export const version = '0.1.0';   
 const DPS_SUBGRAPH_URL = {
   '80001': 'https://api.thegraph.com/subgraphs/name/andreibadea20/subgraph-dps'
 };
+
+const PAGE_SIZE = 1000;
+
+// first params to fetch informations about users
+const params = {
+  users: {
+    __args: {
+      first: PAGE_SIZE,
+      skip: 0
+    },
+    id: true,
+    numberOfTokens: true
+    }
+};
+
+// second params to fetch informations about nfts
+const paramsForNFTs = {
+  nftlockeds: {
+    tokenId: true,
+    owner: {
+      id: true
+    },
+    transferedAtTimestamp: true
+  },
+  nftafterClaimeds: {
+    tokenId: true,
+    owner: {
+      id: true
+    },
+    transferedAtTimestamp: true
+  }
+}
 
 export async function strategy(
   space,
@@ -17,67 +49,50 @@ export async function strategy(
   snapshot
 ) {
 
-  const params = {
-    users: {
-      __args: {
-        where: {
-          id_in: addresses.map((address) => address.toLowerCase())
-        },
-        first: 1000
-      },
-      id: true,
-      numberOfTokens: true
-      }
-  };
+  // fetch informations using sugraphRequest
+  const resultNFTs = await subgraphRequest(DPS_SUBGRAPH_URL[network], paramsForNFTs);
+
+  const score = {};
+
+  let page = 0;
+  while(page !== -1) {
+    params.users.__args.skip = page * PAGE_SIZE;
+
+    const result = await subgraphRequest(DPS_SUBGRAPH_URL[network], params);
+   
+    if (result && result.users && resultNFTs && resultNFTs.nftlockeds && resultNFTs.nftafterClaimeds) {
+      result.users.forEach((u) => {
+        let userScore = parseInt(u.numberOfTokens);
+        let lockedNFTs = 0;
+        let claimedNFTs = 0;
+        const userAddress = getAddress(u.id);
+
+        resultNFTs.nftlockeds.forEach((n) => {
+          if(n.owner.id === u.id) {
+            lockedNFTs = lockedNFTs + 1;
+          }
+        });
+
+        resultNFTs.nftafterClaimeds.forEach((nft) => {
+          if(nft.owner.id === u.id) {
+            claimedNFTs = claimedNFTs + 1;
+          }
+        });
+        
+        userScore = userScore + lockedNFTs - claimedNFTs;
       
-  const paramsForNFTs = {
-    nftlockeds: {
-      tokenId: true,
-      owner: {
-        id: true
-      },
-      transferedAtTimestamp: true
-    },
-    nftafterClaimeds: {
-      tokenId: true,
-      owner: {
-        id: true
-      },
-      transferedAtTimestamp: true
+        if (!score[userAddress]) 
+          score[userAddress] = 0;
+
+        score[userAddress] = userScore;
+      });
+
+        // we use pagination
+        page = result.users.length < PAGE_SIZE ? -1 : page + 1;
+    } else {
+      page = -1;
     }
   }
 
-  const result = await subgraphRequest(DPS_SUBGRAPH_URL[network], params);
-  const score = {};
-  const resultNFTs = await subgraphRequest(DPS_SUBGRAPH_URL[network], paramsForNFTs);
-
-  if (result && result.users && resultNFTs && resultNFTs.nftlockeds && resultNFTs.nftafterClaimeds) {
-    result.users.forEach((u) => {
-      let userScore = parseInt(u.numberOfTokens);
-      let lockedNFTs = 0;
-      let claimedNFTs = 0;
-      const userAddress = getAddress(u.id);
-
-      resultNFTs.nftlockeds.forEach((n) => {
-        if(n.owner.id === u.id) {
-          lockedNFTs = lockedNFTs + 1;
-        }
-      });
-
-      resultNFTs.nftafterClaimeds.forEach((nft) => {
-        if(nft.owner.id === u.id) {
-          claimedNFTs = claimedNFTs + 1;
-        }
-      });
-      
-      userScore = userScore + lockedNFTs - claimedNFTs;
-     
-      if (!score[userAddress]) 
-        score[userAddress] = 0;
-
-      score[userAddress] = userScore;
-    });
-  }
-
-  return score;
+  return score || {};
 }
