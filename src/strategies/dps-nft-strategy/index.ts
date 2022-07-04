0ee37fab852ab12ea1d6a6ea6dac1c102678da43
andreibadea20 <andrei.badea@softbinator.com>
2022-07-04 15:06:25 +0300

    changed an issue with request

--- src/strategies/dps-nft-strategy/index.ts
+++ src/strategies/dps-nft-strategy/index.ts
@@ -14,34 +14,18 @@ const PAGE_SIZE = 1000;
 const params = {
     users: {
         __args: {
+            block: {number: 22572401},
             first: PAGE_SIZE,
             skip: 0
         },
         id: true,
-        numberOfTokens: true
-    }
-};
-
-const paramsForNFTs = {
-    nftlockeds: {
-        __args: {
-            block: {number: Number(22572400)}
+        numberOfTokens: true,
+        listOfNFTsLocked: {
+            tokenId: true
         },
-        tokenId: true,
-        owner: {
-            id: true
-        },
-        transferedAtTimestamp: true
-    },
-    nftafterClaimeds: {
-        __args: {
-            block: {number: Number(22572400)}
-        },
-        tokenId: true,
-        owner: {
-            id: true
-        },
-        transferedAtTimestamp: true
+        listOfNFTsReturned: {
+            tokenId: true
+        }   
     }
 };
 
@@ -49,34 +33,28 @@ export async function strategy(space, network, provider, addresses, options, sna
     
     if (snapshot !== 'latest') {
         // @ts-ignore
-        paramsForNFTs.nftlockeds.__args.block = { number: snapshot};
-        paramsForNFTs.nftafterClaimeds.__args.block = { number: snapshot};
+        params.users.__args.block = {number: snapshot};
     }
 
-    const resultNFTs = await subgraphRequest(DPS_SUBGRAPH_URL[network], paramsForNFTs);
     const score = {};
     let page = 0;
 
     while (page !== -1) {
         params.users.__args.skip = page * PAGE_SIZE;
         const result = await subgraphRequest(DPS_SUBGRAPH_URL[network], params);
-        if (result && result.users && resultNFTs && resultNFTs.nftlockeds && resultNFTs.nftafterClaimeds) {
+        if (result && result.users) {
             result.users.forEach((u) => {
+
                 const userAddress = getAddress(u.id);
+
                 let userScore = parseInt(u.numberOfTokens);
-                let lockedNFTs = 0;
-                let claimedNFTs = 0;
-                resultNFTs.nftlockeds.forEach((n) => {
-                    if (n.owner.id === u.id) {
-                        lockedNFTs = lockedNFTs + 1;
-                    }
-                });
-                resultNFTs.nftafterClaimeds.forEach((nft) => {
-                    if(nft.owner.id === u.id) {
-                        claimedNFTs = claimedNFTs + 1;
-                    }
-                });
+
+                let lockedNFTs = u.listOfNFTsLocked.length;
+                let claimedNFTs = u.listOfNFTsReturned.length;
+
+
                 userScore = userScore + lockedNFTs - claimedNFTs;
+
                 if (!score[userAddress])
                     score[userAddress] = 0;
                 score[userAddress] = userScore;
