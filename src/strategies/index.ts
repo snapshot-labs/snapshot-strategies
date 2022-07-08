08ec763830a9e0218ef2e8f0d1f36666ebb5c9c2
cryptoinhero <33366277+cryptoinhero@users.noreply.github.com>
2022-07-08 12:20:39 +0800

    fix: build issue

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -664,7 +664,7 @@ const strategies = {
   'solv-voucher-claimable': solvVoucherClaimable,
   'balance-of-with-linear-vesting-power': balanceOfWithLinearVestingPower,
   apeswap,
-  orbitalswap
+  orbitalswap,
   h2o
 };
 
