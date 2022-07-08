5deb4cd75561b8ff1faf63eaf0741cb2db0bd5f4
Prof Wobble <profwobble@protonmail.com>
2022-07-08 14:32:04 +0700

    added merkle proof

--- src/strategies/jelly/index.ts
+++ src/strategies/jelly/index.ts
@@ -17,6 +17,30 @@ export async function strategy(
 ) {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
+  const merkleURI =  'https://jelly-merkle-proofs.s3.amazonaws.com/proof-f5c0285a019ef4bc9f2170e29280f996e58d5335cb3f45e00136d756df3d4784.json';
+  const remappedMerkleDataRes = await fetch(
+    merkleURI ,
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+  );
+  const remappedMerkleRaw = await remappedMerkleDataRes.json();
+  const remappedMerkleData = await remappedMerkleRaw.claims;
+
+  const retroAddrs = Object.keys(remappedMerkleData);
+
+  const retroUserBalances = {};
+  retroAddrs.forEach((addr, i) => {
+    retroUserBalances[addr.toLowerCase()] = parseFloat(
+      formatUnits(remappedMerkleData[addr].amount, 18)
+    );
+
+  });
+
   const response = await multicall(
     network,
     provider,
@@ -29,10 +53,33 @@ export async function strategy(
     { blockTag }
   );
 
-  return Object.fromEntries(
-    response.map((value, i) => [
-      addresses[i],
-      parseFloat(formatUnits(value.toString(), 18))
-    ])
+  const userWalletBalances = response.map((amount, i) => {
+    return [
+      addresses[i].toLowerCase(),
+      parseFloat(formatUnits(amount.toString(), 18))
+    ];
+  });
+
+  const userTotal = {};
+  // loop through user, investor/advisor/team-member, and airdrop wallets to calculate total.
+
+  userWalletBalances.forEach(([address, amount]) => {
+    const addr = address.toLowerCase();
+    if (userTotal[addr]) userTotal[addr] += amount;
+    else userTotal[addr] = amount;
+  });
+
+  for (const [address, amount] of Object.entries(retroUserBalances)) {
+    const addr = address.toLowerCase();
+    if (userTotal[addr]) userTotal[addr] += amount;
+    else userTotal[addr] = amount;
+  }
+
+  const finalUserBalances = Object.fromEntries(
+    addresses.map((addr) => [addr, userTotal[addr.toLowerCase()]])
   );
+
+  return finalUserBalances;
+
+
 }
