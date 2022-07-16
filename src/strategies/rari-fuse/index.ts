8f94ff43c8bff881f14bc826ec586a97d388edea
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-07-16 17:13:27 -0400

    Try using getAllBorrowers and getPoolAssetWithData
      - Hardcode return values to 1
      - Comment out unused code

--- src/strategies/rari-fuse/index.ts
+++ src/strategies/rari-fuse/index.ts
@@ -1,17 +1,19 @@
-import { BigNumberish } from '@ethersproject/bignumber';
-import { formatUnits } from '@ethersproject/units';
-import { MaxUint256 } from '@ethersproject/constants';
+// import { BigNumberish } from '@ethersproject/bignumber';
+// import { formatUnits } from '@ethersproject/units';
+// import { MaxUint256 } from '@ethersproject/constants';
 import { Multicaller } from '../../utils';
 
 export const author = 'MantisClone';
 export const version = '0.1.0';
 
-const fusePoolLens = '0x6Dc585Ad66A10214Ef0502492B0CC02F0e836eec'
+// const fusePoolLens = '0x6Dc585Ad66A10214Ef0502492B0CC02F0e836eec'
 
-const absMaxHealth = MaxUint256 // show all users
+// const absMaxHealth = MaxUint256 // show all users
 
 const abi = [
-  'function getPoolUsersWithData(address comptroller,uint256 maxHealth) returns (FusePoolUser[], uint256, uint256)'
+  // 'function getPoolUsersWithData(address comptroller,uint256 maxHealth) returns (FusePoolUser[], uint256, uint256)',
+  'function getAllBorrowers() external view returns (address[] memory)',
+  'function getPoolAssetsWithData(address comptroller) returns (FusePoolAsset[])'
 ];
 
 export async function strategy(
@@ -25,19 +27,23 @@ export async function strategy(
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
   const multi = new Multicaller(network, provider, abi, { blockTag });
-  addresses.forEach((voterAddress) =>
-    multi.call('poolUsersWithData', fusePoolLens, 'getPoolUsersWithData', [options.poolAddress, absMaxHealth])
-  );
-  const result: Record<string, BigNumberish> = await multi.execute();
+  multi.call('poolUsers', options.poolAddress, 'getAllBorrowers', [])
+  // addresses.forEach((voterAddress) =>
+  //   multi.call('poolUsersWithData', fusePoolLens, 'getPoolUsersWithData', [options.poolAddress, absMaxHealth])
+  // );
+  const result = await multi.execute();
 
-  const poolUsersWithData = result.poolUsersWithData;
+  const poolUsers = result.poolUsers;
+  // const poolUsersWithData = result.poolUsersWithData;
 
-  console.log(`poolUsersWithData = ${poolUsersWithData}`);
+  console.log(`poolUsers = ${poolUsers}`)
+  // console.log(`poolUsersWithData = ${poolUsersWithData}`);
 
   return Object.fromEntries(
-    Object.entries(result).map(([address, balance]) => [
-      address,
-      parseFloat(formatUnits(balance, options.decimals))
-    ])
+    result.users.map((u) => [u.id, 1])
+  //   Object.entries(result).map(([address, balance]) => [
+  //     address,
+  //     parseFloat(formatUnits(balance, options.decimals))
+  //   ])
   );
 }
