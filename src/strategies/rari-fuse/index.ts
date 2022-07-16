76df2a17b1a753afd2e7500fe1175f8803aecd65
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-07-16 18:44:31 -0400

    Try using cToken.balanceOfUnderlying(address owner)

--- src/strategies/rari-fuse/index.ts
+++ src/strategies/rari-fuse/index.ts
@@ -1,14 +1,12 @@
-// import { BigNumberish } from '@ethersproject/bignumber';
-// import { formatUnits } from '@ethersproject/units';
+import { BigNumberish } from '@ethersproject/bignumber';
+import { formatUnits } from '@ethersproject/units';
 import { Multicaller } from '../../utils';
 
 export const author = 'MantisClone';
 export const version = '0.1.0';
 
-const fusePoolLens = '0x6Dc585Ad66A10214Ef0502492B0CC02F0e836eec'
-
 const abi = [
-  'function getAllBorrowers() external view returns (address[] memory)',
+  'function balanceOfUnderlying(address owner) external returns (uint256);',
 ];
 
 export async function strategy(
@@ -22,21 +20,15 @@ export async function strategy(
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
   const multi = new Multicaller(network, provider, abi, { blockTag });
-  multi.call('poolUsers', options.poolAddress, 'getAllBorrowers', [])
   addresses.forEach((voterAddress) =>
-    multi.call('poolAssetsWithData', fusePoolLens, 'getPoolAssetsWithData', [options.poolAddress])
+    multi.call('underlyingBalance', options.cTokenAddress, 'balanceOfUnderlying', [voterAddress])
   );
-  const result = await multi.execute();
-
-  const poolUsers = result.poolUsers;
-
-  console.log(`poolUsers = ${poolUsers}`)
+  const result: Record<string, BigNumberish> = await multi.execute();
 
   return Object.fromEntries(
-    result.users.map((u) => [u.id, 1])
-  //   Object.entries(result).map(([address, balance]) => [
-  //     address,
-  //     parseFloat(formatUnits(balance, options.decimals))
-  //   ])
+    Object.entries(result).map(([address, balance]) => [
+      address,
+      parseFloat(formatUnits(balance, options.decimals))
+    ])
   );
 }
