bda81be1fc8b7c2429bdd47700a1c914a82df111
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-07-17 07:30:58 -0400

    Use balanceOf, remove unneded parameters

--- src/strategies/rari-fuse/index.ts
+++ src/strategies/rari-fuse/index.ts
@@ -6,7 +6,7 @@ export const author = 'MantisClone';
 export const version = '0.1.0';
 
 const abi = [
-  'function balanceOfUnderlying(address owner) external returns (uint256);',
+  'function balanceOf(address owner) external returns (uint256)'
 ];
 
 export async function strategy(
@@ -20,15 +20,15 @@ export async function strategy(
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
   const multi = new Multicaller(network, provider, abi, { blockTag });
-  addresses.forEach((voterAddress) =>
-    multi.call('underlyingBalance', options.cTokenAddress, 'balanceOfUnderlying', [voterAddress])
+  addresses.forEach((address) =>
+    multi.call(address, options.cTokenAddress, 'balanceOf', [address])
   );
   const result: Record<string, BigNumberish> = await multi.execute();
 
   return Object.fromEntries(
     Object.entries(result).map(([address, balance]) => [
       address,
-      parseFloat(formatUnits(balance, options.decimals))
+      parseFloat(formatUnits(balance, options.underlyingTokenDecimals))
     ])
   );
 }
