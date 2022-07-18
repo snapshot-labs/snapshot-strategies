21130df97c07dffe37126043211a02340fa8d0f8
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-07-18 18:24:11 -0400

    Add functions for calculating underlying token balance, console logging

--- src/strategies/rari-fuse/index.ts
+++ src/strategies/rari-fuse/index.ts
@@ -1,11 +1,17 @@
-import { BigNumberish } from '@ethersproject/bignumber';
-import { formatUnits } from '@ethersproject/units';
+import { BigNumber } from '@ethersproject/bignumber';
+// import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
+// import { formatUnits } from '@ethersproject/units';
 import { Multicaller } from '../../utils';
 
 export const author = 'MantisClone';
 export const version = '0.1.0';
 
-const abi = ['function balanceOf(address owner) external returns (uint256)'];
+const abi = [
+  'function underlying() public view returns (address)',
+  'function decimals() public view returns (uint8)',
+  'function exchangeRateStored() public view returns (uint256)',
+  'function balanceOf(address owner) external returns (uint256)',
+];
 
 export async function strategy(
   space,
@@ -18,15 +24,35 @@ export async function strategy(
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
   const multi = new Multicaller(network, provider, abi, { blockTag });
+  multi.call('underlying', options.fToken, 'underlying', []);
+  multi.call('tokenDecimals', options.token, 'decimals', []);
+  multi.call('fTokenDecimals', options.fToken, 'decimals', []);
+  multi.call('exchangeRate', options.fTokenAddress, 'exchangeRateStored', []);
   addresses.forEach((address) =>
-    multi.call(address, options.fTokenAddress, 'balanceOf', [address])
+    multi.call(`fTokenBalances.${address}`, options.fTokenAddress, 'balanceOf', [address])
   );
-  const result: Record<string, BigNumberish> = await multi.execute();
+  const result = await multi.execute();
+
+  const underlying: string = result.underlying;
+  const tokenDecimals: BigNumber = result.tokenDecimals;
+  const fTokenDecimals: BigNumber = result.fTokenDecimals;
+  const exchangeRate: BigNumber = result.exchangeRate;
+  const fTokenBalances: Record<string, BigNumber> = result.fTokenBalances;
+
+  console.log(`underlying = ${underlying}`);
+  console.log(`tokenDecimals = ${tokenDecimals}`);
+  console.log(`fTokenDecimals = ${fTokenDecimals}`);
+  console.log(`exchangeRate = ${exchangeRate}`);
+  console.log(`fTokenBalances = ${fTokenBalances}`);
 
   return Object.fromEntries(
     Object.entries(result).map(([address, balance]) => [
       address,
-      parseFloat(formatUnits(balance, options.decimals))
+      1
     ])
+    // Object.entries(result).map(([address, balance]) => [
+    //   address,
+    //   parseFloat(formatUnits(balance, options.decimals))
+    // ])
   );
 }
