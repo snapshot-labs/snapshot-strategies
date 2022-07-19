e06697e0ff6a2ed17231a5df196e6258b610e59a
charq <73696209+buchaoqun@users.noreply.github.com>
2022-07-19 16:08:38 +0800

    Update src/strategies/erc3525-vesting-voucher/index.ts
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/strategies/erc3525-vesting-voucher/index.ts
+++ src/strategies/erc3525-vesting-voucher/index.ts
@@ -6,65 +6,7 @@ export const author = 'charq';
 export const version = '0.1.0';
 
 const abi = [
-    {
-        inputs: [
-            {
-                internalType: "uint256",
-                name: "tokenId_",
-                type: "uint256"
-            }
-        ],
-        name: "getSnapshot",
-        outputs: [
-            {
-                internalType: "uint8",
-                name: "claimType_",
-                type: "uint8"
-            },
-            {
-                internalType: "uint64",
-                name: "term_",
-                type: "uint64"
-            },
-            {
-                internalType: "uint256",
-                name: "vestingAmount_",
-                type: "uint256"
-            },
-            {
-                internalType: "uint256",
-                name: "principal_",
-                type: "uint256"
-            },
-            {
-                internalType: "uint64[]",
-                name: "maturities_",
-                type: "uint64[]"
-            },
-            {
-                internalType: "uint32[]",
-                name: "percentages_",
-                type: "uint32[]"
-            },
-            {
-                internalType: "uint256",
-                name: "availableWithdrawAmount_",
-                type: "uint256"
-            },
-            {
-                internalType: "string",
-                name: "originalInvestor_",
-                type: "string"
-            },
-            {
-                internalType: "bool",
-                name: "isValid_",
-                type: "bool"
-            }
-        ],
-        stateMutability: "view",
-        type: "function"
-    }
+  'function getSnapshot(uint256 tokenId_) view returns (uint8 claimType_, uint64 term_, uint256 vestingAmount_, uint256 principal_, uint64[] maturities_, uint32[] percentages_, uint256 availableWithdrawAmount_, string originalInvestor_, bool isValid_)'
 ];
 
 export async function strategy(
