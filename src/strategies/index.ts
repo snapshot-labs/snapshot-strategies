a243f39b694911743479dfc80f3c11d060431b24
Cr3k <99047920+Cr3k@users.noreply.github.com>
2022-07-19 19:34:20 +0200

    Add self strategy

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -335,6 +335,7 @@ import * as h2o from './h2o';
 import * as dopamine from './dopamine';
 import * as lrcL2SubgraphBalanceOf from './lrc-l2-subgraph-balance-of';
 import * as rariFuse from './rari-fuse';
+import * as self from './self';
 
 const strategies = {
   'forta-shares': fortaShares,
@@ -673,7 +674,8 @@ const strategies = {
   h2o,
   dopamine,
   'lrc-l2-subgraph-balance-of': lrcL2SubgraphBalanceOf,
-  'rari-fuse': rariFuse
+  'rari-fuse': rariFuse,
+  self
 };
 
 Object.keys(strategies).forEach(function (strategyName) {
