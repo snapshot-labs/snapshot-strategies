75e20a7afa89e468056d9fd9aaff90a3c27642c5
Arpit Karnatak <60638961+arpitkarnatak@users.noreply.github.com>
2022-07-29 23:13:41 +0530

    Update src/strategies/erc721-pair-weights/index.ts
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/strategies/erc721-pair-weights/index.ts
+++ src/strategies/erc721-pair-weights/index.ts
@@ -1,7 +1,7 @@
 import { formatUnits } from '@ethersproject/units';
 import { multicall } from '../../utils';
 
-export const author = 'huuep';
+export const author = 'arpitkarnatak';
 export const version = '0.1.0';
 
 const abi = [
