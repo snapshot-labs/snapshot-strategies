949e3dee8a3e4c6f7d339650b1392acf38682177
DungNT <dung.nguyen@adaptis.io>
2022-08-04 18:12:57 +0700

    update author

--- src/strategies/xsgt-voting-power/index.ts
+++ src/strategies/xsgt-voting-power/index.ts
@@ -2,7 +2,7 @@ import { BigNumberish } from '@ethersproject/bignumber';
 import { formatUnits } from '@ethersproject/units';
 import { Multicaller } from '../../utils';
 
-export const author = 'bluebelt';
+export const author = 'acinmavi';
 export const version = '1.0.0';
 
 const abi = [
