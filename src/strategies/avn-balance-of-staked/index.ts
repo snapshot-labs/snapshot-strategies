3aeb60aed9ba22b44863d6afa9fdbd17f45ee363
Chinazaekpere Ngubo <emmanuel.ngubo@aventus.io>
2022-08-12 08:00:32 +0100

    Update index.ts

--- src/strategies/avn-balance-of-staked/index.ts
+++ src/strategies/avn-balance-of-staked/index.ts
@@ -3,7 +3,7 @@ import { formatUnits } from '@ethersproject/units';
 import { multicall } from '../../utils';
 
 export const author = 'andrew-frank';
-export const version = '0.1.1';
+export const version = '0.1.2';
 
 const AVT_ABI = [
   {
@@ -28,41 +28,6 @@ const AVT_ABI = [
     type: 'function'
   }
 ];
-
-const NUM_NODES = 10;
-// [0, 1, ... , 9] for convinience
-const NODES_INDICES = Array.from(Array(NUM_NODES).keys());
-
-class EthCall {
-  constructor(
-    public readonly contract: string,
-    public readonly method: string,
-    public readonly args: Array<string | number>
-  ) {}
-  get ethCall(): any[] {
-    return [this.contract, this.method, this.args];
-  }
-}
-
-/** splits array into chunks */
-function chunkArray<T>(arr: T[], length: number): T[][] {
-  const chunks: T[][] = [];
-  let i = 0;
-  const n = arr.length;
-
-  while (i < n) {
-    chunks.push(arr.slice(i, (i += length)));
-  }
-
-  return chunks;
-}
-
-/** sums big numbers in array */
-function sumNumbers(arr: BigNumber[]): BigNumber {
-  return arr.reduce((previus, current) => {
-    return previus.add(current[0]);
-  }, BigNumber.from(0));
-}
 
 export async function strategy(
   _space,
