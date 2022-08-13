5d70a139741db0b6fca2040108fa4be803f38bea
bonustrack <fabien@bonustrack.co>
2022-08-13 18:13:41 +0700

    Add validations strategies

---
+++ src/validations/index.ts
@@ -0,0 +1,11 @@
+import basic from './basic';
+import aave from './aave';
+import nouns from './nouns';
+import timeperiod from './timeperiod';
+
+export default {
+  basic,
+  aave,
+  nouns,
+  timeperiod
+};
