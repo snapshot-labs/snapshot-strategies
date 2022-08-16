5b95fc0e6d4d97e8c59ded6671d2ba89cb479494
Justin Huang <justingotemail@gmail.com>
2022-08-16 11:56:00 -0700

    Call erc-20

--- src/strategies/proxyprotocol-erc20-balance-of/index.ts
+++ src/strategies/proxyprotocol-erc20-balance-of/index.ts
@@ -1,14 +1,8 @@
 import fetch from 'cross-fetch';
-import { BigNumberish } from '@ethersproject/bignumber';
-import { formatUnits } from '@ethersproject/units';
-import { Multicaller } from '../../utils';
+import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
 
 export const author = 'rawrjustin';
-export const version = '0.1';
-
-const abi = [
-  'function balanceOf(address account) external view returns (uint256)'
-];
+export const version = '0.1.0';
 
 const calculateVotingPower = (inputAddresses, addressScores, walletMap) => {
   let userVotingPower = {};
@@ -52,19 +46,14 @@ export async function strategy(
   var flattenedWalletAddresses = [].concat.apply([], arrayOfProxyWallets);
 
   // Query for token holdings
-  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
-  const multi = new Multicaller(network, provider, abi, { blockTag });
-  flattenedWalletAddresses.forEach((address) =>
-    multi.call(address, options.address, 'balanceOf', [address])
-  );
-  const result: Record<string, BigNumberish> = await multi.execute();
-
-  const addressScores = Object.fromEntries(
-    Object.entries(result).map(([address, balance]) => [
-      address,
-      parseFloat(formatUnits(balance, options.decimals))
-    ])
-  );
+  const addressScores = await erc20BalanceOfStrategy(
+    space,
+    network,
+    provider,
+    flattenedWalletAddresses,
+    options,
+    snapshot
+  )
 
   // Calculate the voting power across all wallets and map it back to original Proxy wallets.
   return calculateVotingPower(
