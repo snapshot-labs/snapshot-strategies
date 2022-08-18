6baceac626e5112ff4d01391888e37a40fbd4446
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-08-18 12:38:36 -0400

    Start hacking on fight-club index.ts

--- src/strategies/fight-club/index.ts
+++ src/strategies/fight-club/index.ts
@@ -1,12 +1,12 @@
-import { BigNumberish } from '@ethersproject/bignumber';
-import { formatUnits } from '@ethersproject/units';
+// import { formatUnits } from '@ethersproject/units';
 import { Multicaller } from '../../utils';
 
 export const author = 'MantisClone';
 export const version = '0.1.0';
 
 const abi = [
-  'function balanceOf(address account) external view returns (uint256)'
+  'function balanceOf(address account) external view returns (uint256)',
+  'function balanceOf(address account, uint256 id) external view returns (uint256);'
 ];
 
 export async function strategy(
@@ -20,15 +20,30 @@ export async function strategy(
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
   const multi = new Multicaller(network, provider, abi, { blockTag });
-  addresses.forEach((address) =>
-    multi.call(address, options.address, 'balanceOf', [address])
-  );
-  const result: Record<string, BigNumberish> = await multi.execute();
+  options.gloves.forEach((gloveAddress: string) => {
+    addresses.forEach((address: string) => {
+      multi.call(`${address}.gloves.${gloveAddress}`, gloveAddress, 'balanceOf', [address]);
+    })
+  });
+  // options.weightClassTokenIds.forEach((id: number) => {
+  //   addresses.forEach((address: string) => {
+  //     multi.call(`${address}.weightClasses.${id}`, options.weightClassAddress, 'balanceOf', [address, id]);
+  //   })
+  // });
+
+  const result = await multi.execute();
+
+  // result.forEach((address: string) => {
+  //   result[address].gloves.forEach([gloveAddress, num]) => {
+
+  //   })
+  // })
 
   return Object.fromEntries(
-    Object.entries(result).map(([address, balance]) => [
-      address,
-      parseFloat(formatUnits(balance, options.decimals))
-    ])
+    result.map((u) => [u, 1])
+    // Object.entries(result).map(([address, balance]) => [
+    //   address,
+    //   parseFloat(formatUnits(balance, options.decimals))
+    // ])
   );
 }
