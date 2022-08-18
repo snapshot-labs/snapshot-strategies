ddc09b38cca28618fd91bc9e3adc734df1b9dd06
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-08-18 13:46:33 -0400

    Add calls to weightClass erc-1155 balanceOf
      - Fails with error:
        unexpected tokens (argument="value", value="balanceOf (address account, uint256 id) external view returns (uint256) ;", code=INVALID_ARGUMENT, version=abi/5.6.4)
      - Maybe can't specify ERC721 balanceOf in same ABI array as ERC1155
        balanceOf??

--- src/strategies/fight-club/index.ts
+++ src/strategies/fight-club/index.ts
@@ -5,8 +5,8 @@ export const author = 'MantisClone';
 export const version = '0.1.0';
 
 const abi = [
-  'function balanceOf(address account) external view returns (uint256)',
-  // 'function balanceOf(address account, uint256 id) external view returns (uint256);'
+  'function balanceOf(address account, uint256 id) external view returns (uint256);',
+  'function balanceOf(address account) external view returns (uint256)'
 ];
 
 export async function strategy(
@@ -25,18 +25,21 @@ export async function strategy(
       multi.call(`${address}.gloves.${gloveAddress}`, gloveAddress, 'balanceOf', [address]);
     })
   });
-  // options.weightClassTokenIds.forEach((id: number) => {
-  //   addresses.forEach((address: string) => {
-  //     multi.call(`${address}.weightClasses.${id}`, options.weightClassAddress, 'balanceOf', [address, id]);
-  //   })
-  // });
+  Object.entries(options.weightClassTokenIds).forEach(([weightClassId]) => {
+    addresses.forEach((address: string) => {
+      multi.call(`${address}.weightClasses.${weightClassId}`, options.weightClassAddress, 'balanceOf', [address, weightClassId]);
+    })
+  });
 
   const result = await multi.execute();
 
   Object.entries(result).forEach(([address]) => {
-    Object.entries(result[address].gloves).forEach(([gloveAddress, num]) => {
-      console.log('address',address,'gloveAddress',gloveAddress,'num',num);
-    })
+    Object.entries(result[address].gloves).forEach(([gloveAddress, numGloves]) => {
+      console.log('address',address,'gloveAddress',gloveAddress,'numGloves',numGloves);
+    });
+    Object.entries(result[address].weightClasses).forEach(([weightClassId, numKudos]) => {
+      console.log('address',address,'weightClassId',weightClassId,'numKudos',numKudos);
+    });
   })
 
   return Object.fromEntries(
