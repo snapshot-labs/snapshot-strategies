5d063729b9724914fca74b06c0e33f60034c4d72
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-08-18 17:05:46 -0400

    Make weightedResult look more like result

--- src/strategies/fight-club/index.ts
+++ src/strategies/fight-club/index.ts
@@ -31,38 +31,25 @@ export async function strategy(
       multi.call(`${address}.weightClasses.${weightClassId}`, options.weightClassAddress, 'balanceOf(address, uint256)', [address, weightClassId]);
     })
   });
-
   const result: Record<string, Record<string, Record<string, BigNumber>>> = await multi.execute();
+  console.dir(result, { depth: null });
 
   const weightedResult: Record<string, Record<string, Record<string, BigNumber>>> = Object.fromEntries(
     addresses.map((address: string) => [
       address,
-      Object.fromEntries([
-        ['gloves', Object.entries(result[address].gloves).map(([gloveAddress, numGloves]) => [
+      {
+        'gloves': Object.fromEntries(Object.entries(result[address].gloves).map(([gloveAddress, numGloves]) => [
           gloveAddress,
           numGloves.mul(options.gloves[gloveAddress])
-        ])],
-        ['weightedClasses', Object.entries(result[address].weightClasses).map(([weightClassId, numKudos]) => [
+        ])),
+        'weightedClasses': Object.fromEntries(Object.entries(result[address].weightClasses).map(([weightClassId, numKudos]) => [
           weightClassId,
           numKudos.mul(options.weightClassTokenIds[weightClassId])
-        ])]
-      ])
+        ]))
+      }
     ])
   );
-
-  const address = '0x1EC1CcEF3e1735bdA3F4BA698e8a524AA7c93274';
-  console.log("weightedResult",weightedResult);
-  console.log("weightedResult.gloves", weightedResult[address].gloves);
-  console.log("weightedResult.gloves", weightedResult[address].weightClasses);
-
-  addresses.forEach((address: string) => {
-    Object.entries(weightedResult[address].gloves).forEach(([gloveAddress, numGloves]) => {
-      console.log('address',address,'gloveAddress',gloveAddress,'numGloves',numGloves);
-    });
-    Object.entries(weightedResult[address].weightClasses).forEach(([weightClassId, numKudos]) => {
-      console.log('address',address,'weightClassId',weightClassId,'numKudos',numKudos);
-    });
-  });
+  console.dir(weightedResult, { depth: null });
 
   return Object.fromEntries(
     addresses.map((address: string) => {
