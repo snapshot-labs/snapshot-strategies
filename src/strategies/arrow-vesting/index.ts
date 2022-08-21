701db266e056da8f0888fe91ccbac8d2120790d6
ChaituVR <ChaituVR@users.noreply.github.com>
2022-08-21 10:15:45 +0000

    Automated lint

--- src/strategies/arrow-vesting/index.ts
+++ src/strategies/arrow-vesting/index.ts
@@ -7,79 +7,106 @@ export const author = 'BrassLion';
 export const version = '0.1.0';
 
 const vestingFactoryAbi = [
-  "function escrows_length() public view returns (uint256)",
-  "function escrows(uint256 index) public view returns (address)",
+  'function escrows_length() public view returns (uint256)',
+  'function escrows(uint256 index) public view returns (address)'
 ];
 
 const vestingContractAbi = [
-  "function recipient() public view returns (address)",
-]
+  'function recipient() public view returns (address)'
+];
 
 const tokenAbi = [
-  "function balanceOf(address account) external view returns (uint256)",
+  'function balanceOf(address account) external view returns (uint256)'
 ];
 
 export async function strategy(
-    space,
+  space,
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+): Promise<Record<string, number>> {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+
+  // Get all vesting contract addresses.
+  const vestingFactory = new Contract(
+    options.vestingFactory,
+    vestingFactoryAbi,
+    provider
+  );
+
+  const vestingContractCount = await vestingFactory.escrows_length();
+
+  const vestingFactoryMulti = new Multicaller(
     network,
     provider,
-    addresses,
-    options,
-    snapshot
-  ): Promise<Record<string, number>> {
-    const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+    vestingFactoryAbi,
+    { blockTag }
+  );
 
-    // Get all vesting contract addresses.
-    const vestingFactory = new Contract(options.vestingFactory, vestingFactoryAbi, provider);
+  [...Array(vestingContractCount.toNumber()).keys()].forEach((contractIdx) => {
+    vestingFactoryMulti.call(contractIdx, options.vestingFactory, 'escrows', [
+      contractIdx
+    ]);
+  });
 
-    const vestingContractCount = await vestingFactory.escrows_length();
+  const vestingContracts: Record<number, string> =
+    await vestingFactoryMulti.execute();
 
-    const vestingFactoryMulti = new Multicaller(network, provider, vestingFactoryAbi, { blockTag });
+  // Get all beneficiaries of vesting contracts.
+  const vestingContractMulti = new Multicaller(
+    network,
+    provider,
+    vestingContractAbi,
+    { blockTag }
+  );
 
-    [...Array(vestingContractCount.toNumber()).keys()].forEach((contractIdx) => {
-      vestingFactoryMulti.call(contractIdx, options.vestingFactory, "escrows", [contractIdx])
-    });
-
-    const vestingContracts: Record<number, string> = await vestingFactoryMulti.execute();
-
-    // Get all beneficiaries of vesting contracts.
-    const vestingContractMulti = new Multicaller(network, provider, vestingContractAbi, { blockTag });
-
-    Object.values(vestingContracts).forEach((vestingContractAddress) => {
-      vestingContractMulti.call(vestingContractAddress, vestingContractAddress, "recipient", [])
-    });
-
-    const vestingContractRecipients: Record<string, string> = await vestingContractMulti.execute();
-
-    // Get all balances of vesting contracts.
-    const tokenMulti = new Multicaller(network, provider, tokenAbi, { blockTag });
-
-    Object.values(vestingContracts).forEach((vestingContractAddress) => {
-      tokenMulti.call(vestingContractAddress, options.address, "balanceOf", [vestingContractAddress])
-    });
-
-    const vestingContractBalances: Record<string, BigNumberish> = await tokenMulti.execute();
-
-    // Sum all vesting contract balances by recipient over requested addresses.
-    let addressBalances: Record<string, BigNumber> = {};
-
-    addresses.forEach(address => {
-      addressBalances[address] = BigNumber.from(0);
-    });
-
-    Object.values(vestingContracts).forEach((vestingContractAddress) => {
-      const recipient = vestingContractRecipients[vestingContractAddress];
-      
-      if (recipient in addressBalances) {
-        addressBalances[recipient] = addressBalances[recipient].add(vestingContractBalances[vestingContractAddress]);
-      }
-    });
-  
-    return Object.fromEntries(
-      Object.entries(addressBalances).map(([address, balance]) => [
-        address,
-        parseFloat(formatUnits(balance, options.decimals))
-      ])
+  Object.values(vestingContracts).forEach((vestingContractAddress) => {
+    vestingContractMulti.call(
+      vestingContractAddress,
+      vestingContractAddress,
+      'recipient',
+      []
     );
-  }
-  
+  });
+
+  const vestingContractRecipients: Record<string, string> =
+    await vestingContractMulti.execute();
+
+  // Get all balances of vesting contracts.
+  const tokenMulti = new Multicaller(network, provider, tokenAbi, { blockTag });
+
+  Object.values(vestingContracts).forEach((vestingContractAddress) => {
+    tokenMulti.call(vestingContractAddress, options.address, 'balanceOf', [
+      vestingContractAddress
+    ]);
+  });
+
+  const vestingContractBalances: Record<string, BigNumberish> =
+    await tokenMulti.execute();
+
+  // Sum all vesting contract balances by recipient over requested addresses.
+  const addressBalances: Record<string, BigNumber> = {};
+
+  addresses.forEach((address) => {
+    addressBalances[address] = BigNumber.from(0);
+  });
+
+  Object.values(vestingContracts).forEach((vestingContractAddress) => {
+    const recipient = vestingContractRecipients[vestingContractAddress];
+
+    if (recipient in addressBalances) {
+      addressBalances[recipient] = addressBalances[recipient].add(
+        vestingContractBalances[vestingContractAddress]
+      );
+    }
+  });
+
+  return Object.fromEntries(
+    Object.entries(addressBalances).map(([address, balance]) => [
+      address,
+      parseFloat(formatUnits(balance, options.decimals))
+    ])
+  );
+}
--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -355,7 +355,7 @@ import * as orcaPod from './orca-pod';
 import * as proxyProtocolErc20BalanceOf from './proxyprotocol-erc20-balance-of';
 import * as proxyProtocolErc721BalanceOf from './proxyprotocol-erc721-balance-of';
 import * as proxyProtocolErc1155BalanceOf from './proxyprotocol-erc1155-balance-of';
-import * as arrowVesting from "./arrow-vesting";
+import * as arrowVesting from './arrow-vesting';
 
 const strategies = {
   'forta-shares': fortaShares,
@@ -715,7 +715,7 @@ const strategies = {
   'proxyprotocol-erc20-balance-of': proxyProtocolErc20BalanceOf,
   'proxyprotocol-erc721-balance-of': proxyProtocolErc721BalanceOf,
   'proxyprotocol-erc1155-balance-of': proxyProtocolErc1155BalanceOf,
-  "arrow-vesting": arrowVesting
+  'arrow-vesting': arrowVesting
 };
 
 Object.keys(strategies).forEach(function (strategyName) {
