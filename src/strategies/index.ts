00469bc462cbea3db02ee12486a7fbd90d3a182e
Mitesh Mutha <mtshmtha@gmail.com>
2022-08-25 19:49:29 +0200

    feat: Add gysr-pending-rewards strategy (#799)
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -290,6 +290,7 @@ import * as landDaoTiers from './landdao-token-tiers';
 import * as defiplaza from './defiplaza';
 import * as stakingClaimedUnclaimed from './staking-claimed-unclaimed';
 import * as gysrStakingBalance from './gysr-staking-balance';
+import * as gysrPendingRewards from './gysr-pending-rewards';
 import * as wanakafarmStaking from './wanakafarm-staking';
 import * as starsharks from './starsharks';
 import * as printerFinancial from './printer-financial';
@@ -654,6 +655,7 @@ const strategies = {
   revest: revest,
   'staking-claimed-unclaimed': stakingClaimedUnclaimed,
   'gysr-staking-balance': gysrStakingBalance,
+  'gysr-pending-rewards': gysrPendingRewards,
   'wanakafarm-staking': wanakafarmStaking,
   starsharks,
   'printer-financial': printerFinancial,
