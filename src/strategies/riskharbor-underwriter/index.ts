0f5fff3dfda43a4cfe8ee8621fcce29af85c24f4
Drew <89287674+dewpe@users.noreply.github.com>
2022-09-04 20:37:33 -0500

    returned checksum address

--- src/strategies/riskharbor-underwriter/index.ts
+++ src/strategies/riskharbor-underwriter/index.ts
@@ -1,3 +1,4 @@
+import { getAddress } from '@ethersproject/address';
 import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
 import { formatUnits } from '@ethersproject/units';
 import { subgraphRequest } from '../../utils';
@@ -70,7 +71,7 @@ export async function strategy(
 
   return Object.fromEntries(
     Object.entries(agUserBals).map(([address, balance]) => [
-      address,
+      getAddress(address),
       // Divide each bal by 1eDecimals
       parseFloat(formatUnits(balance, decimals))
     ])
