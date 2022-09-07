c1854705700a07176f383139b8018ed70bc3e9a7
karamorf <karamorf.eth>
2022-09-07 10:01:06 +0000

    fixing parsing of NFT ownership and doing it by slot.

--- src/strategies/lrc-l2-nft-balance-of/index.ts
+++ src/strategies/lrc-l2-nft-balance-of/index.ts
@@ -1,32 +1,36 @@
 import { subgraphRequest } from '../../utils';
 
 export const author = 'karamorf';
-export const version = '0.1.1';
+export const version = '0.1.0';
 
-const LIMIT = 500;
+const LIMIT = 1000;
 
-function makeQuery(minter, skip, blacklisted_accounts) {
+function makeQuery(snapshot, minter, lastUpdatedAt, blacklisted_accounts) {
   const query: any = {
-    nonFungibleTokens: {
+    accountNFTSlots: {
       __args: {
         where: {
-          minter: minter,
-          slots_: {
-            account_not_in: blacklisted_accounts
-          }
+          nft_: { minter: minter },
+          account_not_in: blacklisted_accounts,
+          lastUpdatedAt_gt: lastUpdatedAt
         },
-        first: LIMIT,
-        skip: skip
+        first: LIMIT
       },
-      token: true,
-      slots: {
-        account: {
-          address: true
-        },
-        balance: true
-      }
+      account: { address: true },
+      balance: true,
+      lastUpdatedAt: true
     }
+  };
+
+  if (snapshot !== 'latest') {
+    query.accountNFTSlots.__args.where = {
+      ...query.accountNFTSlots.__args.where,
+      _change_block: {
+        number_gte: snapshot
+      }
+    };
   }
+
   return query;
 }
 
@@ -38,30 +42,37 @@ export async function strategy(
   options,
   snapshot
 ): Promise<Record<string, number>> {
-  console.log("hello");
+  let blacklisted_ids = options.blacklisted_account_ids;
   let balances = {};
-  let skip = 0;
-  let result_size = 0;
+  let lastUpdatedAt = 0;
+  let response_size = 0;
+  let requests: any[] = [];
+
+  if(! blacklisted_ids || blacklisted_ids.length === 0) {
+    blacklisted_ids = [""];
+  }
+
 
   do {
-    const query = makeQuery(options.minter, skip, options.blacklisted_accounts);
-    const response = await subgraphRequest(
-      options.graph, query
+    const request = subgraphRequest(
+      options.graph,
+      makeQuery(snapshot, options.minter_account_id, lastUpdatedAt, blacklisted_ids)
     );
-    const tokens = response.map((data) => data.nonFungibleTokens).flat();
+    requests.push(request);
+    const response = await request;
 
-    tokens.forEach((token) => {
-      token.slots.forEach((slot) => {
-        if(! balances.hasOwnProperty(slot.account.address)) {
-          balances[slot.account.address] = 0;
-        }
-        balances[slot.account.address] += parseInt(slot.balance);
-      })
+    response.accountNFTSlots.forEach((slot) => {
+      if(! balances.hasOwnProperty(slot.account.address)) {
+        balances[slot.account.address] = 0;
+      }
+      balances[slot.account.address] += parseInt(slot.balance);
+      lastUpdatedAt = slot.lastUpdatedAt;
     });
+    response_size = response.accountNFTSlots.length;
 
-    result_size = tokens.length
-    skip += result_size;
-  } while(result_size == LIMIT);
+  } while(response_size == LIMIT);
+
+  await Promise.all(requests);
 
   return balances;
 }
