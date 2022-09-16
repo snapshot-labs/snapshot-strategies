3f452842e427356f6f18537a48bec8e69f64e24a
Rai <ratan.r.sur@gmail.com>
2022-09-16 19:15:44 -0400

    switch away from quadratic balances

--- src/strategies/amkt/index.ts
+++ src/strategies/amkt/index.ts
@@ -1,4 +1,5 @@
 import { strategy as erc1155AllBalancesOf } from '../erc1155-all-balances-of';
+import { strategy as erc20BalanceOf } from '../erc20-balance-of';
 import { strategy as math } from '../math';
 import { OperandType, Operation } from '../math/options';
 
@@ -7,7 +8,7 @@ export const version = '0.1.0';
 
 const UID = '0xba0439088dc1e75F58e0A7C107627942C15cbb41';
 // currently set to USDC, change once mainnet contract deployed
-const AMKT = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+const AMKT = '0xBf2d6955Bf8849691F635a29cFF19525FABc683E';
 
 export async function strategy(
   space,
@@ -29,25 +30,14 @@ export async function strategy(
     snapshot
   );
 
-  const sqrtBalances = await math(
+  const balances = await erc20BalanceOf(
     space,
     network,
     provider,
     addresses,
     {
-      operands: [
-        {
-          type: OperandType.Strategy,
-          strategy: {
-            name: 'erc20-balance-of',
-            params: {
-              address: AMKT,
-              decimals: 18
-            }
-          }
-        }
-      ],
-      operation: Operation.SquareRoot
+      address: AMKT,
+      decimals: 18
     },
     snapshot
   );
@@ -55,7 +45,7 @@ export async function strategy(
   return Object.fromEntries(
     addresses.map((address) => [
       address,
-      uidBalances[address] * sqrtBalances[address]
+      uidBalances[address] * balances[address]
     ])
   );
 }
