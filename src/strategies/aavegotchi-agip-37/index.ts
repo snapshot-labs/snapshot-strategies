5882635bdf14e366c59a11fb861fadf715184a85
Jarrod Lilkendey <programmablewealth@gmail.com>
2022-09-23 12:52:50 +1000

    code cleanup

--- src/strategies/aavegotchi-agip-37/index.ts
+++ src/strategies/aavegotchi-agip-37/index.ts
@@ -8,8 +8,6 @@ const tokenAbi = [
   'function totalSupply() view returns (uint256)',
   'function allUserInfo(address _user) view returns (tuple(address lpToken, uint256 allocPoint, uint256 pending, uint256 userBalance, uint256 poolBalance)[] _info)',
   'function convertToAssets(uint256 shares) view returns (uint)',
-  //   'function stakedInCurrentEpoch(address _account) view returns (tuple(address poolAddress, string poolName, string poolUrl, uint256 rate, uint256 amount)[] _staked)',
-  // 'function staked(address _account) view returns (uint256 ghst_, uint256 poolTokens_, uint256 ghstUsdcPoolToken_)'
 ];
 
 export async function strategy(
@@ -41,15 +39,12 @@ export async function strategy(
   options.ghstKekAddress = options.ghstKekAddress || '0xbfad162775ebfb9988db3f24ef28ca6bc2fb92f0';
   options.ghstKekPoolId = options.ghstKekPoolId || 4;
   
-  options.ghstUsdcAddress =
-    options.ghstUsdcAddress || '0x096c5ccb33cfc5732bcd1f3195c13dbefc4c82f4';
+  options.ghstUsdcAddress = options.ghstUsdcAddress || '0x096c5ccb33cfc5732bcd1f3195c13dbefc4c82f4';
   options.ghstUsdcPoolId = options.ghstUsdcPoolId || 5;
 
-  options.ghstWmaticAddress =
-    options.ghstWmaticAddress || '0xf69e93771F11AECd8E554aA165C3Fe7fd811530c';
+  options.ghstWmaticAddress = options.ghstWmaticAddress || '0xf69e93771F11AECd8E554aA165C3Fe7fd811530c';
   options.ghstWmaticPoolId = options.ghstWmaticPoolId || 6;
 
-    
   options.ghstGltrAddress = options.ghstGltrAddress || '0xb0E35478a389dD20050D66a67FB761678af99678';
   options.ghstGltrPoolId = options.ghstGltrPoolId || 7;
 
@@ -72,18 +67,11 @@ export async function strategy(
     stakedQueries.push([options.gltrStakingAddress, 'allUserInfo', [address]]);
   }
 
-  // const stakedQuery = addresses.map((address: string) => [
-  //   options.gltrStakingAddress,
-  //   'allUserInfo',
-  //   [address]
-  // ]);
-
   const res = await multicall(
     network,
     provider,
     tokenAbi,
     [
-      // ...stakedQuery,
       ...stakedQueries,
       ...unstakedQueries,
       [options.ghstFudAddress, 'totalSupply', []],
@@ -104,9 +92,7 @@ export async function strategy(
     { blockTag }
   );
 
-  console.log('multicall done', res);
-
-  // unstaked WAP GHST
+  // unstaked wapGHST - use convertToAssets which is required for calculating the voting power for wapGHST
   let unstakedConvertedWapGHST:any = [];
   for (let i = 0; i < addresses.length; i++) {
     let addressIndex = (i * 7) + 1;
@@ -114,7 +100,7 @@ export async function strategy(
     unstakedConvertedWapGHST.push([options.wapGhstAddress, 'convertToAssets', [unstakedWapGHSTBalance]]);
   }
 
-  // staked WAP GHST
+  // staked wapGHST - use convertToAssets which is required for calculating the voting power for wapGHST
   let stakedConvertedWapGHST:any = [];
   for (let i = 0; i < addresses.length; i++) {
     const stakedWapGHSTBalance = res[i]._info[options.wapGhstPoolId].userBalance;
@@ -133,20 +119,6 @@ export async function strategy(
     { blockTag }
   );
 
-  // console.log('addresses', addresses);
-  // res.map((r, i) => {
-  //   if (i > addresses.length - 1) {
-  //     console.log('res', i, Number(r.toString()) / 1e18)
-  //   } else {
-  //     console.log('res', i, r._info)
-  //   }
-  // });
-
-  wapGHST_res.map((r, i) => {
-    console.log('wapGHST_res', i, Number(r.toString()) / 1e18)
-    // console.log(i, r)
-  });
-
   const tokensPerUni = (balanceInUni: number, totalSupply: number) => {
     return balanceInUni / 1e18 / (totalSupply / 1e18);
   };
@@ -162,7 +134,7 @@ export async function strategy(
     ghstWmaticLp: tokensPerUni(res[lpTokensStartIndex+13], res[lpTokensStartIndex+12]),
   };
 
-  let entries:Array<object> = [];
+  let entries = {};
   for (let addressIndex = 0; addressIndex < addresses.length; addressIndex++) {
     let i = (addressIndex * 7) + 1;
 
@@ -230,279 +202,9 @@ export async function strategy(
     loggedString += "\TOTAL VOTING POWER\n" + totalVotingPower;
     console.log(loggedString);
 
-    entries.push({ 
-      [address]: totalVotingPower
-    });
+    entries[address] = totalVotingPower;
   }
 
   console.log('entries', entries);
   return entries;
-
-  // const blockAfterStakingUpgrade = 22007789;
-  // const afterStakingUpgrade =
-  //   blockTag === 'latest' || blockAfterStakingUpgrade < blockTag;
-  // const stakeFunctionName = afterStakingUpgrade
-  //   ? 'stakedInCurrentEpoch'
-  //   : 'staked';
-  // const stakeQuery = addresses.map((address: string) => [
-  //   options.stakingAddress,
-  //   stakeFunctionName,
-  //   [address]
-  // ]);
-
-  // const res = await multicall(
-  //   network,
-  //   provider,
-  //   tokenAbi,
-  //   [
-  //     [options.ghstQuickAddress, 'totalSupply', []],
-  //     [options.tokenAddress, 'balanceOf', [options.ghstQuickAddress]],
-  //     [options.ghstUsdcAddress, 'totalSupply', []],
-  //     [options.tokenAddress, 'balanceOf', [options.ghstUsdcAddress]],
-  //     [options.ghstWethAddress, 'totalSupply', []],
-  //     [options.tokenAddress, 'balanceOf', [options.ghstWethAddress]],
-  //     [options.ghstWmaticAddress, 'totalSupply', []],
-  //     [options.tokenAddress, 'balanceOf', [options.ghstWmaticAddress]],
-  //     ...stakeQuery
-  //   ],
-  //   { blockTag }
-  // );
-
-  // const tokensPerUni = (balanceInUni: number, totalSupply: number) => {
-  //   return balanceInUni / 1e18 / (totalSupply / 1e18);
-  // };
-
-  // const ghstQuickTotalSupply = res[0];
-  // const ghstQuickTokenBalanceInUni = res[1];
-  // const ghstQuickTokensPerUni = tokensPerUni(
-  //   ghstQuickTokenBalanceInUni,
-  //   ghstQuickTotalSupply
-  // );
-
-  // const ghstUsdcTotalSupply = res[2];
-  // const ghstUsdcTokenBalanceInUni = res[3];
-  // const ghstUsdcTokensPerUni = tokensPerUni(
-  //   ghstUsdcTokenBalanceInUni,
-  //   ghstUsdcTotalSupply
-  // );
-
-  // const response = res.slice(8);
-  // let entries;
-  // if (afterStakingUpgrade) {
-  //   const ghstWethTotalSupply = res[4];
-  //   const ghstWethTokenBalanceInUni = res[5];
-  //   const ghstWethTokensPerUni = tokensPerUni(
-  //     ghstWethTokenBalanceInUni,
-  //     ghstWethTotalSupply
-  //   );
-
-  //   const ghstWmaticTotalSupply = res[6];
-  //   const ghstWmaticTokenBalanceInUni = res[7];
-  //   const ghstWmaticTokensPerUni = tokensPerUni(
-  //     ghstWmaticTokenBalanceInUni,
-  //     ghstWmaticTotalSupply
-  //   );
-
-  //   entries = response.map((userStakeInfo, i: number) => {
-  //     const votePowerAmounts = userStakeInfo._staked.map((info) => {
-  //       if (
-  //         info.poolAddress.toLowerCase() === options.tokenAddress.toLowerCase()
-  //       ) {
-  //         return Number(info.amount.toString()) / 1e18;
-  //       }
-  //       if (
-  //         info.poolAddress.toLowerCase() ===
-  //         options.ghstQuickAddress.toLowerCase()
-  //       ) {
-  //         return (
-  //           (Number(info.amount.toString()) / 1e18) * ghstQuickTokensPerUni
-  //         );
-  //       }
-  //       if (
-  //         info.poolAddress.toLowerCase() ===
-  //         options.ghstUsdcAddress.toLowerCase()
-  //       ) {
-  //         return (Number(info.amount.toString()) / 1e18) * ghstUsdcTokensPerUni;
-  //       }
-  //       if (
-  //         info.poolAddress.toLowerCase() ===
-  //         options.ghstWethAddress.toLowerCase()
-  //       ) {
-  //         return (Number(info.amount.toString()) / 1e18) * ghstWethTokensPerUni;
-  //       }
-  //       if (
-  //         info.poolAddress.toLowerCase() ===
-  //         options.ghstWmaticAddress.toLowerCase()
-  //       ) {
-  //         return (
-  //           (Number(info.amount.toString()) / 1e18) * ghstWmaticTokensPerUni
-  //         );
-  //       }
-  //       return 0;
-  //     });
-
-  //     return [addresses[i], votePowerAmounts.reduce((a, b) => a + b, 0)];
-  //   });
-  // } else {
-  //   // before staking upgrade old response
-  //   entries = response.map((values, i) => [
-  //     addresses[i],
-  //     values[0] / 1e18 + //ghst_
-  //       (values[1] / 10 ** options.decimals) * ghstQuickTokensPerUni + //poolTokens_
-  //       (values[2] / 10 ** options.decimals) * ghstUsdcTokensPerUni //ghstUsdcPoolToken_
-  //   ]);
-  // }
-
-  // console.log('entries', entries);
-  // console.log('Object.fromEntries entries', Object.fromEntries(entries));
-  
-  // return entries
 }
-
-
-// const tokenAbi = [
-//   'function balanceOf(address account) view returns (uint256)',
-//   'function totalSupply() view returns (uint256)',
-//   'function stakedInCurrentEpoch(address _account) view returns (tuple(address poolAddress, string poolName, string poolUrl, uint256 rate, uint256 amount)[] _staked)',
-//   'function staked(address _account) view returns (uint256 ghst_, uint256 poolTokens_, uint256 ghstUsdcPoolToken_)'
-// ];
-
-// export async function strategy(
-//   _space,
-//   network,
-//   provider,
-//   addresses,
-//   options,
-//   snapshot
-// ) {
-//   options.ghstQuickAddress =
-//     options.ghstQuickAddress || '0x8b1fd78ad67c7da09b682c5392b65ca7caa101b9';
-
-//   options.ghstUsdcAddress =
-//     options.ghstUsdcAddress || '0x096c5ccb33cfc5732bcd1f3195c13dbefc4c82f4';
-
-//   options.ghstWethAddress =
-//     options.ghstWethAddress || '0xccb9d2100037f1253e6c1682adf7dc9944498aff';
-
-//   options.ghstWmaticAddress =
-//     options.ghstWmaticAddress || '0xf69e93771F11AECd8E554aA165C3Fe7fd811530c';
-
-//   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
-
-//   const blockAfterStakingUpgrade = 22007789;
-//   const afterStakingUpgrade =
-//     blockTag === 'latest' || blockAfterStakingUpgrade < blockTag;
-//   const stakeFunctionName = afterStakingUpgrade
-//     ? 'stakedInCurrentEpoch'
-//     : 'staked';
-//   const stakeQuery = addresses.map((address: string) => [
-//     options.stakingAddress,
-//     stakeFunctionName,
-//     [address]
-//   ]);
-
-//   const res = await multicall(
-//     network,
-//     provider,
-//     tokenAbi,
-//     [
-//       [options.ghstQuickAddress, 'totalSupply', []],
-//       [options.tokenAddress, 'balanceOf', [options.ghstQuickAddress]],
-//       [options.ghstUsdcAddress, 'totalSupply', []],
-//       [options.tokenAddress, 'balanceOf', [options.ghstUsdcAddress]],
-//       [options.ghstWethAddress, 'totalSupply', []],
-//       [options.tokenAddress, 'balanceOf', [options.ghstWethAddress]],
-//       [options.ghstWmaticAddress, 'totalSupply', []],
-//       [options.tokenAddress, 'balanceOf', [options.ghstWmaticAddress]],
-//       ...stakeQuery
-//     ],
-//     { blockTag }
-//   );
-
-//   const tokensPerUni = (balanceInUni: number, totalSupply: number) => {
-//     return balanceInUni / 1e18 / (totalSupply / 1e18);
-//   };
-
-//   const ghstQuickTotalSupply = res[0];
-//   const ghstQuickTokenBalanceInUni = res[1];
-//   const ghstQuickTokensPerUni = tokensPerUni(
-//     ghstQuickTokenBalanceInUni,
-//     ghstQuickTotalSupply
-//   );
-
-//   const ghstUsdcTotalSupply = res[2];
-//   const ghstUsdcTokenBalanceInUni = res[3];
-//   const ghstUsdcTokensPerUni = tokensPerUni(
-//     ghstUsdcTokenBalanceInUni,
-//     ghstUsdcTotalSupply
-//   );
-
-//   const response = res.slice(8);
-//   let entries;
-//   if (afterStakingUpgrade) {
-//     const ghstWethTotalSupply = res[4];
-//     const ghstWethTokenBalanceInUni = res[5];
-//     const ghstWethTokensPerUni = tokensPerUni(
-//       ghstWethTokenBalanceInUni,
-//       ghstWethTotalSupply
-//     );
-
-//     const ghstWmaticTotalSupply = res[6];
-//     const ghstWmaticTokenBalanceInUni = res[7];
-//     const ghstWmaticTokensPerUni = tokensPerUni(
-//       ghstWmaticTokenBalanceInUni,
-//       ghstWmaticTotalSupply
-//     );
-
-//     entries = response.map((userStakeInfo, i: number) => {
-//       const votePowerAmounts = userStakeInfo._staked.map((info) => {
-//         if (
-//           info.poolAddress.toLowerCase() === options.tokenAddress.toLowerCase()
-//         ) {
-//           return Number(info.amount.toString()) / 1e18;
-//         }
-//         if (
-//           info.poolAddress.toLowerCase() ===
-//           options.ghstQuickAddress.toLowerCase()
-//         ) {
-//           return (
-//             (Number(info.amount.toString()) / 1e18) * ghstQuickTokensPerUni
-//           );
-//         }
-//         if (
-//           info.poolAddress.toLowerCase() ===
-//           options.ghstUsdcAddress.toLowerCase()
-//         ) {
-//           return (Number(info.amount.toString()) / 1e18) * ghstUsdcTokensPerUni;
-//         }
-//         if (
-//           info.poolAddress.toLowerCase() ===
-//           options.ghstWethAddress.toLowerCase()
-//         ) {
-//           return (Number(info.amount.toString()) / 1e18) * ghstWethTokensPerUni;
-//         }
-//         if (
-//           info.poolAddress.toLowerCase() ===
-//           options.ghstWmaticAddress.toLowerCase()
-//         ) {
-//           return (
-//             (Number(info.amount.toString()) / 1e18) * ghstWmaticTokensPerUni
-//           );
-//         }
-//         return 0;
-//       });
-
-//       return [addresses[i], votePowerAmounts.reduce((a, b) => a + b, 0)];
-//     });
-//   } else {
-//     // before staking upgrade old response
-//     entries = response.map((values, i) => [
-//       addresses[i],
-//       values[0] / 1e18 + //ghst_
-//         (values[1] / 10 ** options.decimals) * ghstQuickTokensPerUni + //poolTokens_
-//         (values[2] / 10 ** options.decimals) * ghstUsdcTokensPerUni //ghstUsdcPoolToken_
-//     ]);
-//   }
-
-//   return Object.fromEntries(entries);
-// }
