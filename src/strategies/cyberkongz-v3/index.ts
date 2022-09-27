5b93c54fbdc71b4ccba89230075124eec64feecf
Maxbrand99 <maxbrand99@gmail.com>
2022-09-27 01:42:28 -0400

    Update index.ts

--- src/strategies/cyberkongz-v3/index.ts
+++ src/strategies/cyberkongz-v3/index.ts
@@ -1,30 +1,10 @@
 import { formatUnits } from '@ethersproject/units';
 import {getProvider, getSnapshots, multicall} from '../../utils';
 
-export const author = 'maxbrand99-eth';
+export const author = 'maxbrand99';
 export const version = '1.0.0';
 
 const bananaContract = '0xe2311ae37502105b442bbef831e9b53c5d2e9b3b';
-
-// addresses are blacklisted across all registries instead of just one, but that should be ok
-const blacklistedAddresses = [
-  '0xb14b87790643d2dab44b06692d37dd95b4b30e56', // WGK
-  '0x9d59eba4deaee09466ba9d4073bf912bc72982b0', // NFT20 Gen
-  '0x0f4676178b5c53ae0a655f1b19a96387e4b8b5f2', // NFT20 VX
-  '0xcfa9a297a406a48d1137172c18de04c944b47ba9', // Banana Sushi
-  '0x820f92c1b3ad8e962e6c6d9d7caf2a550aec46fb', // Banana Tip.cc
-  '0x9ffad2ff3a59d8579e3b0edc6c8f2f591c94dfab', // Banana cyberkongz.eth
-  '0xe058d87fc1185e38ab68893136834715b30961e1', // Banana Rewarder
-  '0xe2311ae37502105b442bbef831e9b53c5d2e9b3b', // CyberKongZ: BANANA Token
-  '0x7a08865A3E7c291f3b794210Bc51D559B49DFd15', // The Klaw - mainchain
-  '0xe6f45376f64e1f568bd1404c155e5ffd2f80f7ad', // VX Polygon Bridge - mainchain
-  '0x40ec5b33f54e0e8a33a975908c5ba1c14e5bbbdf', // Banana Polygon Bridge - mainchain
-  '0x0000000000000000000000000000000000000000', // VX Polygon -> mainchain Brige (Polygon)
-  '0xD6a92755Ac5384867083Abd79aD007DE389b955e', // Cyberkong Gnosis (Polygon)
-  '0x000000000000000000000000000000000000dead', // Matic Burn Address (Polygon)
-  '0x70C575588B98C1F46B1382c706AdAf398A874e3E', // Kong Proxy (Polygon)
-  '0xab8eee3493a55a7bd8126865fd662b7097928088', // Marketplace LP (Polygon)
-];
 
 const abi = [
   'function balanceOf(address account) external view returns (uint256)',
@@ -67,7 +47,7 @@ export async function strategy(space, network, provider, addresses, options, sna
 
   response.forEach((value: any, i: number) => {
     const address = allCalls[i][2][0];
-    if (allAddresses[allCalls[i][0]] == "BANANA" && blacklistedAddresses.find((add) => add === address)) {
+    if (allAddresses[allCalls[i][0]] == "BANANA" && options.skipList.find((add) => add === address)) {
       nanaSupply -= value;
     }
   });
@@ -75,7 +55,7 @@ export async function strategy(space, network, provider, addresses, options, sna
   const merged = {};
   response.forEach((value: any, i: number) => {
     const address = allCalls[i][2][0];
-    if (blacklistedAddresses.find((add) => add === address)) {
+    if (options.skipList.find((add) => add === address)) {
       return;
     }
     merged[address] = (merged[address] || 0) as number;
