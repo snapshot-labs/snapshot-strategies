805ad32227d09289008934b7506602f043accb98
BrassLion <BrassLion@users.noreply.github.com>
2022-09-30 01:02:29 +0100

    Change Arrow vesting to return voting power based on potential vested tokens

--- src/strategies/arrow-vesting/index.ts
+++ src/strategies/arrow-vesting/index.ts
@@ -1,22 +1,20 @@
-import { BigNumberish, BigNumber } from '@ethersproject/bignumber';
 import { Contract } from '@ethersproject/contracts';
 import { formatUnits } from '@ethersproject/units';
 import { Multicaller } from '../../utils';
 
 export const author = 'BrassLion';
-export const version = '0.1.0';
+export const version = '0.1.1';
 
 const vestingFactoryAbi = [
   'function escrows_length() public view returns (uint256)',
-  'function escrows(uint256 index) public view returns (address)'
+  'function escrows(uint256 index) public view returns (address)',
 ];
 
 const vestingContractAbi = [
-  'function recipient() public view returns (address)'
-];
-
-const tokenAbi = [
-  'function balanceOf(address account) external view returns (uint256)'
+  'function recipient() public view returns (address)',
+  'function total_locked() public view returns (uint256)',
+  'function start_time() public view returns (uint256)',
+  'function end_time() public view returns (uint256)',
 ];
 
 export async function strategy(
@@ -33,10 +31,10 @@ export async function strategy(
   const vestingFactory = new Contract(
     options.vestingFactory,
     vestingFactoryAbi,
-    provider
+    provider,
   );
 
-  const vestingContractCount = await vestingFactory.escrows_length();
+  const vestingContractCount = await vestingFactory.escrows_length({blockTag: blockTag});
 
   const vestingFactoryMulti = new Multicaller(
     network,
@@ -46,15 +44,13 @@ export async function strategy(
   );
 
   [...Array(vestingContractCount.toNumber()).keys()].forEach((contractIdx) => {
-    vestingFactoryMulti.call(contractIdx, options.vestingFactory, 'escrows', [
-      contractIdx
-    ]);
+    vestingFactoryMulti.call(contractIdx, options.vestingFactory, 'escrows', [contractIdx]);
   });
 
   const vestingContracts: Record<number, string> =
     await vestingFactoryMulti.execute();
 
-  // Get all beneficiaries of vesting contracts.
+  // Get all vesting contract parameters.
   const vestingContractMulti = new Multicaller(
     network,
     provider,
@@ -63,50 +59,35 @@ export async function strategy(
   );
 
   Object.values(vestingContracts).forEach((vestingContractAddress) => {
-    vestingContractMulti.call(
-      vestingContractAddress,
-      vestingContractAddress,
-      'recipient',
-      []
-    );
+    vestingContractMulti.call(`${vestingContractAddress}.recipient`, vestingContractAddress, 'recipient', []);
+    vestingContractMulti.call(`${vestingContractAddress}.total_locked`, vestingContractAddress, 'total_locked', []);
+    vestingContractMulti.call(`${vestingContractAddress}.start_time`, vestingContractAddress, 'start_time', []);
+    vestingContractMulti.call(`${vestingContractAddress}.end_time`, vestingContractAddress, 'end_time', []);
   });
 
-  const vestingContractRecipients: Record<string, string> =
-    await vestingContractMulti.execute();
-
-  // Get all balances of vesting contracts.
-  const tokenMulti = new Multicaller(network, provider, tokenAbi, { blockTag });
-
-  Object.values(vestingContracts).forEach((vestingContractAddress) => {
-    tokenMulti.call(vestingContractAddress, options.address, 'balanceOf', [
-      vestingContractAddress
-    ]);
-  });
-
-  const vestingContractBalances: Record<string, BigNumberish> =
-    await tokenMulti.execute();
+  const vestingContractParameters: Record<string, object> = await vestingContractMulti.execute();
 
   // Sum all vesting contract balances by recipient over requested addresses.
-  const addressBalances: Record<string, BigNumber> = {};
+  const block = await provider.getBlock(blockTag)
+  const time = block.timestamp
+  const addressBalances: Record<string, number> = {};
 
   addresses.forEach((address) => {
-    addressBalances[address] = BigNumber.from(0);
+    addressBalances[address] = 0;
   });
 
-  Object.values(vestingContracts).forEach((vestingContractAddress) => {
-    const recipient = vestingContractRecipients[vestingContractAddress];
+  Object.entries(vestingContractParameters).forEach(([contractAddress, params]) => {
+    const recipient = params["recipient"];
+    const start = params["start_time"]
 
-    if (recipient in addressBalances) {
-      addressBalances[recipient] = addressBalances[recipient].add(
-        vestingContractBalances[vestingContractAddress]
-      );
+    if (recipient in addressBalances && time > start) {
+
+      const locked = parseFloat(formatUnits(params["total_locked"], options.decimals))
+      const end = params["end_time"]
+
+      addressBalances[recipient] += Math.min(locked * (time - start) / (end - start), locked);
     }
   });
 
-  return Object.fromEntries(
-    Object.entries(addressBalances).map(([address, balance]) => [
-      address,
-      parseFloat(formatUnits(balance, options.decimals))
-    ])
-  );
+  return addressBalances
 }
