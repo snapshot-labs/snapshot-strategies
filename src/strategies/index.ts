25071b6431038a18b6037c6a82ea95d6e070dedb
Chaitanya <yourchaitu@gmail.com>
2022-10-05 23:40:09 +0530

    Update index.ts

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -182,6 +182,7 @@ import * as unipoolSameToken from './unipool-same-token';
 import * as unipoolUniv2Lp from './unipool-univ2-lp';
 import * as unipoolXSushi from './unipool-xsushi';
 import * as poapWithWeight from './poap-with-weight';
+import * as poapWithWeightV2 from './poap-with-weight-v2';
 import * as uniswapV3 from './uniswap-v3';
 import * as uniswapV3Staking from './uniswap-v3-staking';
 import * as l2Deversifi from './l2-deversifi';
@@ -567,6 +568,7 @@ const strategies = {
   'unipool-univ2-lp': unipoolUniv2Lp,
   'unipool-xsushi': unipoolXSushi,
   'poap-with-weight': poapWithWeight,
+  'poap-with-weight-v2': poapWithWeightV2,
   'uniswap-v3': uniswapV3,
   'uniswap-v3-staking': uniswapV3Staking,
   'l2-deversifi': l2Deversifi,
