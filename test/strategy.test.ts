a62d482cae1b5aaf0858a329cfd7b4d44484a08e
Chaitanya <yourchaitu@gmail.com>
2022-10-10 23:59:34 +0530

    Strategies depend on other addresses

--- test/strategy.test.ts
+++ test/strategy.test.ts
@@ -54,9 +54,16 @@ describe.each(examples)(
       expect(example.strategy.name).toBe(strategy);
     });
 
-    it('Addresses in example should be minimum 5 and maximum 20', () => {
-      expect(example.addresses.length).toBeGreaterThanOrEqual(5);
+    it('Addresses in example should be minimum 3 and maximum 20', () => {
+      expect(example.addresses.length).toBeGreaterThanOrEqual(3);
       expect(example.addresses.length).toBeLessThanOrEqual(20);
+    });
+
+    it('Must use a snapshot block number in the past', async () => {
+      expect(typeof example.snapshot).toBe('number');
+      const provider = snapshot.utils.getProvider(example.network);
+      const blockNumber = await snapshot.utils.getBlockNumber(provider);
+      expect(example.snapshot).toBeLessThanOrEqual(blockNumber);
     });
 
     it('Strategy should run without any errors', async () => {
@@ -115,6 +122,36 @@ describe.each(examples)(
         )
       ).toBe(true);
     });
+
+    (snapshot.strategies[strategy].dependOnOtherAddress ? it.skip : it)(
+      'Voting power should not depend on other addresses',
+      async () => {
+        // limit addresses to have only 10 addresses
+        const testAddresses = example.addresses.slice(0, 10);
+        const scoresOneByOne = await Promise.all(
+          testAddresses.map((address) =>
+            callGetScores({
+              ...example,
+              addresses: [address]
+            })
+          )
+        );
+
+        const oldScores = {};
+        const newScores = {};
+
+        scoresOneByOne.forEach((score) => {
+          const address = Object.keys(score[0])[0];
+          const value = Object.values(score[0])[0];
+          if (value) newScores[address] = value;
+          const oldScore = scores[0][address];
+          if (oldScore) oldScores[address] = oldScore;
+        });
+
+        expect(newScores).not.toEqual({});
+        expect(newScores).toEqual(oldScores);
+      }
+    );
   }
 );
 
