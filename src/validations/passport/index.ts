4fc26f41cbd93e7c9f5e11ef33ff17d57145e070
Chaitanya <yourchaitu@gmail.com>
2022-10-21 04:10:21 +0530

    Passport validation (#895)
    * Passport validation
    
    * Fix build issues
    
    * Clean
    
    Co-authored-by: bonustrack <fabien@bonustrack.co>

---
+++ src/validations/passport/index.ts
@@ -0,0 +1,29 @@
+import Validation from '../validation';
+import { getPassport, getVerifiedStamps } from './helper';
+
+export default class extends Validation {
+  public id = 'passport';
+  public github = 'snapshot-labs';
+  public version = '0.1.0';
+
+  async validate(): Promise<boolean> {
+    const passport: any = await getPassport(this.author);
+    if (!passport) return false;
+    if (!passport.stamps || !this.params.stamps) return false;
+
+    const verifiedStamps: false | any[] = await getVerifiedStamps(
+      passport,
+      this.author,
+      this.params.stamps
+    );
+    if (!verifiedStamps) return false;
+
+    let weight = 0;
+    this.params.stamps.forEach((stamp: any) => {
+      const found = verifiedStamps.find((s: any) => s.provider === stamp.id);
+      if (found?.verified) weight += stamp.weight;
+    });
+
+    return weight >= this.params.min_weight;
+  }
+}
