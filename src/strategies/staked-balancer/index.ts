1c4f37bc4a487e77c85349cb8fa575e6f7d3e759
lukaszh2o <111342592+lukaszh2o@users.noreply.github.com>
2022-10-25 23:27:28 +0200

    Staked-Balancer: remove unnecessary accumulation of score

--- src/strategies/staked-balancer/index.ts
+++ src/strategies/staked-balancer/index.ts
@@ -107,9 +107,7 @@ export async function strategy(
 
       response.forEach((value, i) => {
         const userAddress = getAddress(addresses[i]);
-        if (!score[userAddress]) score[userAddress] = 0;
-        score[userAddress] =
-          score[userAddress] + (value / stkTotalSupply) * tokensPerStkLP;
+        score[userAddress] = (value / stkTotalSupply) * tokensPerStkLP;
       });
     }
   }
