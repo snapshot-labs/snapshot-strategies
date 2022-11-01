f8dae058d40e03b0138925a67d73eb580b76f5fd
Chaitanya <yourchaitu@gmail.com>
2022-11-01 17:04:05 +0530

    Revert "Export examples and schema for validations (#926)" (#927)
    This reverts commit 4c4e2f1f5246bf9c5a7a561b6d308da4e42a2cb6.

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -3,52 +3,11 @@ import aave from './aave';
 import nouns from './nouns';
 import timeperiod from './timeperiod';
 import passport from './passport';
-import { readFileSync } from 'fs';
-import path from 'path';
 
-const validations = {
+export default {
   basic,
   aave,
   nouns,
   timeperiod,
   passport
 };
-
-Object.keys(validations).forEach(function (validationName) {
-  let examples = null;
-  let schema = null;
-  let about = '';
-
-  try {
-    examples = JSON.parse(
-      readFileSync(
-        path.join(__dirname, validationName, 'examples.json'),
-        'utf8'
-      )
-    );
-  } catch (error) {
-    examples = null;
-  }
-
-  try {
-    schema = JSON.parse(
-      readFileSync(path.join(__dirname, validationName, 'schema.json'), 'utf8')
-    );
-  } catch (error) {
-    schema = null;
-  }
-
-  try {
-    about = readFileSync(
-      path.join(__dirname, validationName, 'README.md'),
-      'utf8'
-    );
-  } catch (error) {
-    about = '';
-  }
-  validations[validationName].examples = examples;
-  validations[validationName].schema = schema;
-  validations[validationName].about = about;
-});
-
-export default validations;
