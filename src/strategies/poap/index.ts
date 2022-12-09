0e7019e212f2e5ccca7f35faf113838100e1ab3b
Alexander Greene <greenealexander036@gmail.com>
2022-12-08 22:02:28 -0600

    fix: use erc721 strategy

--- src/strategies/poap/index.ts
+++ src/strategies/poap/index.ts
@@ -1,15 +1,8 @@
 import { getAddress } from '@ethersproject/address';
-import { subgraphRequest } from '../../utils';
+import { strategy as erc721Strategy } from '../erc721';
 
 export const author = 'greenealexander';
 export const version = '1.0.0';
-
-const POAP_API_ENDPOINT_URL = {
-  '1': 'https://api.thegraph.com/subgraphs/name/poap-xyz/poap',
-  '100': 'https://api.thegraph.com/subgraphs/name/poap-xyz/poap-xdai'
-};
-// subgraph query in filter has max length of 500
-const MAX_ACCOUNTS_IN_QUERY = 500;
 
 export async function strategy(
   space,
@@ -17,54 +10,19 @@ export async function strategy(
   provider,
   addresses,
   options,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   snapshot
 ) {
-  const addressesMap = addresses.reduce((map, address) => {
-    map[getAddress(address)] = 0;
+  const lowercaseAddresses = addresses.map((a) => a.toLowerCase());
+  const results = await erc721Strategy(
+    space,
+    network,
+    provider,
+    lowercaseAddresses,
+    options,
+    snapshot
+  );
+  return lowercaseAddresses.reduce((map, address) => {
+    map[getAddress(address)] = results[address] ?? 0;
     return map;
   }, {});
-  const lowercaseAddresses = Object.keys(addressesMap).map((address) =>
-    address.toLowerCase()
-  );
-
-  // batch addresses to query into slices of MAX_ACCOUNTS_IN_QUERY size
-  const lowercaseAddressBatches: string[][] = [];
-  for (let i = 0; i < lowercaseAddresses.length; i += MAX_ACCOUNTS_IN_QUERY) {
-    const slice = lowercaseAddresses.slice(i, i + MAX_ACCOUNTS_IN_QUERY);
-    lowercaseAddressBatches.push(slice);
-  }
-
-  const results = await Promise.allSettled<{
-    accounts: { id: string; tokensOwned: string }[];
-  }>(
-    lowercaseAddressBatches.map((addresses) => {
-      const query = {
-        accounts: {
-          __args: {
-            where: {
-              id_in: addresses
-            }
-          },
-          id: true,
-          tokensOwned: true
-        }
-      };
-      return subgraphRequest(POAP_API_ENDPOINT_URL[network], query);
-    })
-  );
-
-  for (const supplyResponse of results) {
-    if (supplyResponse.status === 'rejected') continue;
-
-    for (const account of supplyResponse.value.accounts) {
-      const accountId = getAddress(account.id);
-
-      if (addressesMap[accountId] === undefined) continue;
-
-      addressesMap[accountId] = parseInt(account.tokensOwned);
-    }
-  }
-
-  return addressesMap;
 }
