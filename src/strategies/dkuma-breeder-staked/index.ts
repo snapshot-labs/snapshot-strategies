5b08a1332a445f8cb0de8240d188661cc0735d63
TheK <thekdev9@gmail.com>
2023-01-01 20:38:28 +0100

    dkuma strategy finished

--- src/strategies/dkuma-breeder-staked/index.ts
+++ src/strategies/dkuma-breeder-staked/index.ts
@@ -5,6 +5,7 @@ import { Multicaller } from '../../utils';
 
 export const author = 'TheKdev9';
 export const version = '0.1.0';
+
 interface walletInfoInt {
   amount: BigNumberish;
   enteredAt: BigNumberish;
@@ -12,8 +13,6 @@ interface walletInfoInt {
   rewardTakenActual: BigNumberish;
   bag: BigNumberish;
 }
-
-const DECIMALS = 18;
 
 export async function strategy(
   space,
@@ -25,20 +24,18 @@ export async function strategy(
 ): Promise<Record<string, number>> {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
-  const multi = new Multicaller(network, provider, [options.dbreederAbi], {
+  const multi = new Multicaller(network, provider, [options.dbreeder.abi], {
     blockTag
   });
   addresses.forEach((address) =>
-    multi.call(address, options.dbreederAddress, options.dbreederAbi.name, [
-      address
-    ])
+    multi.call(address, options.dbreeder.address, 'stakeInfo', [address])
   );
   const result: Record<string, walletInfoInt> = await multi.execute();
 
   return Object.fromEntries(
     Object.entries(result).map(([address, walletInfo]) => [
       address,
-      parseFloat(formatUnits(walletInfo.amount, DECIMALS))
+      parseFloat(formatUnits(walletInfo.amount, options.decimals))
     ])
   );
 }
@@ -57,14 +54,14 @@ export async function strategy(
 //     network,
 //     provider,
 //     [abi],
-//     addresses.map((address: any) => [dbreederAddress, options.dbreederAbi.name, [address]]),
+//     addresses.map((address: any) => [options.dbreeder.address, abi.name, [address]]),
 //     { blockTag }
 //   );
 
 //   return Object.fromEntries(
 //     response.map((value, i) => [
 //       addresses[i],
-//       parseFloat(formatUnits(value.amount, DECIMALS))
+//       parseFloat(formatUnits(value.amount, options.decimals))
 //     ])
 //   );
 // }
