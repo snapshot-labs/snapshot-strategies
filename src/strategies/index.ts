3039a782bebe14115c6320bd9f2beff239796fd9
nexonfidev <98641861+nexonfidev@users.noreply.github.com>
2023-01-27 01:13:39 +0530

    Add Nexon Army NFT strategy. [nexon-army-nft] (#1022)
    * added nexon army nft strategy
    
    * updated strategy name
    
    * updated folder name
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -416,6 +416,7 @@ import * as jpegdLockedJpegOf from './jpegd-locked-jpeg-of';
 import * as litDaoGovernance from './lit-dao-governance';
 import * as babywealthyclub from './babywealthyclub';
 import * as battleflyVGFLYAndStakedGFLY from './battlefly-vgfly-and-staked-gfly';
+import * as nexonArmyNFT from './nexon-army-nft';
 
 const strategies = {
   'forta-shares': fortaShares,
@@ -837,7 +838,8 @@ const strategies = {
   'vsta-pool-staking': vstaPoolStaking,
   'jpegd-locked-jpeg-of': jpegdLockedJpegOf,
   babywealthyclub,
-  'battlefly-vgfly-and-staked-gfly': battleflyVGFLYAndStakedGFLY
+  'battlefly-vgfly-and-staked-gfly': battleflyVGFLYAndStakedGFLY,
+  'nexon-army-nft': nexonArmyNFT,
 };
 
 Object.keys(strategies).forEach(function (strategyName) {
