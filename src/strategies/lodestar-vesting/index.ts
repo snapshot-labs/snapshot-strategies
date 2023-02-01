17be4ca4a7a3517d673276dc6e60c723d8e311cc
Appo <103305611+0xAppo@users.noreply.github.com>
2023-02-01 01:15:14 -0500

    Revert "fixing error reporting"
    This reverts commit 2da623944215b38eab51a8eee9a4f1cc21830696.

--- src/strategies/lodestar-vesting/index.ts
+++ src/strategies/lodestar-vesting/index.ts
@@ -26,7 +26,7 @@ export async function strategy(
       options.beneficiaryAddresses.length >= 25 ||
       options.contractAddresses.length >= 25
     ) {
-      throw new Error('Too many beneficiary/contract addresses provided.');
+      console.error('ERROR: Too many stake pool addresses provided.');
       return;
     }
     if (options.beneficiaryAddresses.includes(address)) {
