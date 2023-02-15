9f5716bae6eb8276b7e00619a0fd223d53f372c4
0xHessian <99917971+0xHessian@users.noreply.github.com>
2023-02-15 10:13:42 +0000

    Add vesting snapshot strategy

--- src/strategies/stfx-vesting-contract/README.md
+++ src/strategies/stfx-vesting-contract/README.md
@@ -1,1 +1,9 @@
 # stfx-vesting-contract
+
+Queries the STFX Vesting contracts to calculate the balance of any vesting contracts whose recipient is the given address.
+
+## Parameters
+
+|    Parameter     |                        Desc                        |
+| ---------------- | -------------------------------------------------- |
+| address          | The address to calculate the voting power of       |
--- src/strategies/stfx-vesting-contract/index.ts
+++ src/strategies/stfx-vesting-contract/index.ts
@@ -1,8 +1,9 @@
 import { BigNumberish } from '@ethersproject/bignumber';
 import { formatUnits } from '@ethersproject/units';
 import { Multicaller } from '../../utils';
+import { getAddress } from '@ethersproject/address';
 
-export const author = 'bonustrack';
+export const author = '0xItadori';
 export const version = '0.1.1';
 
 const abi = [
@@ -24,18 +25,56 @@ export async function strategy(
   // https://etherscan.io/address/0x7777bd6a0bFc050c3D4673f370E8746cc6b13Bb5
   // that returns the address of the vesting contract for a given address
   // If the address has no vesting contract, the method errors
-  const multi = new Multicaller(network, provider, abi, { blockTag });
-  addresses.forEach((address) =>
-    multi.call(address, options.address, 'balanceOf', [address])
+  const multi = new Multicaller(
+    network,
+    provider,
+    [
+      {
+        inputs: [
+          {
+            internalType: 'address',
+            name: '',
+            type: 'address'
+          }
+        ],
+        name: 'recipientVesting',
+        outputs: [
+          {
+            internalType: 'address',
+            name: '',
+            type: 'address'
+          }
+        ],
+        stateMutability: 'view',
+        type: 'function'
+      }
+    ],
+    { blockTag }
   );
-  const result: Record<string, BigNumberish> = await multi.execute();
+  const multi2 = new Multicaller(network, provider, abi, { blockTag });
+
+  addresses.forEach((address) =>
+    multi.call(address, options.vestingAddress, 'recipientVesting', [address])
+  );
+  const result: Record<string, string> = await multi.execute();
+
+  const addressList = Object.entries(result).map(([address, value]) => {
+    if (value === '0x0000000000000000000000000000000000000000') return address;
+    return value;
+  });
+
+  addressList.forEach((address) =>
+    multi2.call(address, options.tokenAddress, 'balanceOf', [address])
+  );
+
+  const balanceResult: Record<string, BigNumberish> = await multi2.execute();
 
   // Then for each address with a vesting contract, we need to query the balance of the vesting contract
-  // for the STFX token (https://etherscan.io/token/0x9343e24716659A3551eB10Aff9472A2dcAD5Db2d) 
+  // for the STFX token (https://etherscan.io/token/0x9343e24716659A3551eB10Aff9472A2dcAD5Db2d)
   // on the vesting contract and assign that to the address as voting score
   return Object.fromEntries(
-    Object.entries(result).map(([address, balance]) => [
-      address,
+    Object.entries(balanceResult).map(([address, balance]) => [
+      getAddress(address),
       parseFloat(formatUnits(balance, options.decimals))
     ])
   );
