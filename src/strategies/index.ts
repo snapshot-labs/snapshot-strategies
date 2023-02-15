3d5c225d090dff56b1d9294f0df13efe07ddfa24
Jay Maree <jaymaree@Jays-MacBook-M1-Pro.local>
2023-02-15 09:23:09 +0100

    added strategy to call dcb tiers contract and get totaldeposited tokens from address

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -303,6 +303,7 @@ import * as veRibbon from './ve-ribbon';
 import * as veRibbonVotingPower from './ve-ribbon-voting-power';
 import * as chubbykaijudao from './chubbykaijudao';
 import * as landDaoTiers from './landdao-token-tiers';
+import * as dcbCallTiers from './dcb-call-tiers';
 import * as defiplaza from './defiplaza';
 import * as stakingClaimedUnclaimed from './staking-claimed-unclaimed';
 import * as gysrStakingBalance from './gysr-staking-balance';
@@ -734,6 +735,7 @@ const strategies = {
   chubbykaijudao: chubbykaijudao,
   revest: revest,
   'staking-claimed-unclaimed': stakingClaimedUnclaimed,
+  'dcb-call-tiers': dcbCallTiers,
   'gysr-staking-balance': gysrStakingBalance,
   'gysr-pending-rewards': gysrPendingRewards,
   'gysr-lp-staking-balance': gysrLPStakingBalance,
