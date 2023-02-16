ec36abe45390018cf006c63fc36b8a47ed557c23
LpcAries <291482574@qq.com>
2023-02-16 12:14:26 +0800

    update izumi-veizi strategy

--- src/strategies/izumi-veizi/index.ts
+++ src/strategies/izumi-veizi/index.ts
@@ -1,10 +1,16 @@
 import { BigNumberish } from '@ethersproject/bignumber';
 import { formatUnits } from '@ethersproject/units';
 import { Multicaller } from '../../utils';
-import abi from './abi.json'
 
-export const author = 'izumi';
+export const author = 'izumiFinance';
 export const version = '0.1.0';
+
+const abi = [
+    "function balanceOf(address owner) external view returns (uint256)",
+    "function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256)",
+    "function nftVeiZiAt(uint256 nftId, uint256 timestamp) external view returns (uint256)",
+    "function stakedNft(address) external view returns (uint256)",
+]
 
 export async function strategy(
     space,
@@ -23,50 +29,33 @@ export async function strategy(
     addresses.forEach((address) => nftBalance.call(address, options.address, 'balanceOf', [address]));
     const balance: Record<string, BigNumberish> = await nftBalance.execute();
 
-    await Promise.all(Object.entries(balance).map(async ([address, balance]) => {
-        if (balance.toString() != '0'){
-            const nftId = new Multicaller(network, provider, abi, { blockTag })
-            const num = Number(balance.toString())
-            for (let i=0; i<num; i++){
-                nftId.call(i, options.address, 'tokenOfOwnerByIndex', [address, i])
-            }
-            const nftIds: Record<string, BigNumberish> = await nftId.execute()
-
-            const veizi = new Multicaller(network, provider, abi, { blockTag })
-            Object.entries(nftIds).map(([index, id])=>{
-                veizi.call(id, options.address, 'nftVeiZiAt', [id, timeStamp])
-            })
-            const score: Record<string, BigNumberish> = await veizi.execute()
-
-            Object.entries(score).map(([id, point])=>{
-                const decimalPoint = Number(Math.floor(parseFloat(formatUnits(point, options.decimals)) * 100) / 100)
-                const tmp = Object.keys(ans).find((t)=>{return t===address})
-                if (tmp !== undefined) {
-                    ans[address] = ans[address] + decimalPoint
-                } else {
-                    Object.assign(ans, {[address]:decimalPoint})
-                }
-          })   
-        }   
-    }))
-
-    const stakingCheck = new Multicaller(network, provider, abi, { blockTag })
-    addresses.forEach((address) => stakingCheck.call(address, options.address, 'stakedNft', [address]));
-    const staked: Record<string, BigNumberish> = await stakingCheck.execute()
-    const stakedNft = new Multicaller(network, provider, abi, { blockTag })
-
-    Object.entries(staked).map(([address,id])=>{
-        stakedNft.call(address, options.address, 'nftVeiZiAt', [id, timeStamp])
+    const nftIdCall = new Multicaller(network, provider, abi, { blockTag });
+    Object.entries(balance).map(async ([address, balance]) => {
+        const num = Number(balance.toString())
+        for (let i=0; i<num; i++){
+            const path = address + '-' + i
+            const stakedPath = address + '-' + 'staked'
+            nftIdCall.call(path, options.address, 'tokenOfOwnerByIndex', [address, i])
+            nftIdCall.call(stakedPath, options.address, 'stakedNft', [address])
+        } 
     })
-    const stakedPoint: Record<string, BigNumberish> = await stakedNft.execute()
+    const ids: Record<string, BigNumberish> = await nftIdCall.execute()
 
-    Object.entries(stakedPoint).map(([address, point]) => {
-        const tmp = Object.keys(ans).find((t)=>{return t===address})
-        const decimalStakePoint = Number(Math.floor(parseFloat(formatUnits(point, options.decimals)) * 100) / 100)
+    const pointCall = new Multicaller(network, provider, abi, { blockTag })
+    Object.entries(ids).map(([path, id])=>{
+        pointCall.call(path, options.address, 'nftVeiZiAt', [id, timeStamp])
+    })
+    const points: Record<string, BigNumberish> = await pointCall.execute()
+    
+    Object.entries(points).map(([path, point])=>{
+        const owner = path.split("-")[0]
+        const tmp = Object.keys(ans).find((t)=>{return t===owner})
+        const decimalPoint = Number(Math.floor(parseFloat(formatUnits(point, options.decimals)) * 100) / 100)
         if (tmp !== undefined) {
-            ans[address] = Number(ans[address]) + decimalStakePoint
-        } else {
-            Object.assign(ans, {[address]:decimalStakePoint})
+            ans[owner] = Number(ans[owner]) + decimalPoint
+        } 
+        else {
+            Object.assign(ans, {[owner]:decimalPoint})
         }
     })
 
