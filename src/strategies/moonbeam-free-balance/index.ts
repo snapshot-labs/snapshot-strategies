deb5cceb65af6dd30d208a7804e9acc8bf0dc838
crystalin <alan.sapede@gmail.com>
2023-02-21 12:49:57 +0100

    Support for querying in batch

--- src/strategies/moonbeam-free-balance/index.ts
+++ src/strategies/moonbeam-free-balance/index.ts
@@ -1,4 +1,5 @@
 import { getAddress } from '@ethersproject/address';
+import { fetchJson } from '@ethersproject/web';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { blake2bHex } from './blake2b';
 
@@ -18,39 +19,98 @@ export async function strategy(
   // Pre-encoded key prefix for "system.account" storage
   const accountPrefix = `0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9`;
 
-  return Object.fromEntries(
-    await Promise.all(
-      addresses.map(async (address) => {
-        // Computes "system.account" key for given address
-        const key = `${accountPrefix}${await blake2bHex(
-          Buffer.from(address.substring(2), 'hex'),
-          16
-        )}${address.substring(2)}`;
+  const batchSize = 100;
+  const batches = addresses.reduce((resultArray, item, index) => {
+    const chunkIndex = Math.floor(index / batchSize);
+    if (!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = []; // start a new chunk
+    }
+    resultArray[chunkIndex].push(item);
+    return resultArray;
+  }, [] as string[][]);
 
-        // Retrieves storage data for the "system.account" key
-        const result: string = await provider.send('state_getStorage', [key]);
+  // Stores all the retrieved balances
+  let balances: { [k: string]: number } = {};
 
-        // account data structure (little endian):
-        // {
-        //   nonce: 8 bits
-        //   consumers: 8 bits
-        //   providers: 8 bits
-        //   sufficients: 8 bits
-        //   data: {
-        //     free: 32 bits
-        //     reserved: 32 bits
-        //     miscFrozen: 32 bits
-        //     feeFrozen: 32 bits
-        //   }
-        // }
-        const free = readLittleEndianBigInt(
-          result.substring(2 + 8 + 8 + 8 + 8, 2 + 8 + 8 + 8 + 8 + 32)
-        );
+  for (let batchIndex = 0; batchIndex < batches.length; batchIndex++) {
+    const addressBatch = batches[batchIndex];
 
-        // Converts the bigint into number.
-        // Result won't be precise, but should be lower than real value.
-        return [getAddress(address), Number(free)];
-      })
-    )
-  );
+    // Convert address to storage key.
+    const keys = await Promise.all(
+      addressBatch.map(
+        async (address) =>
+          `${accountPrefix}${await blake2bHex(
+            Buffer.from(address.substring(2), 'hex'),
+            16
+          )}${address.substring(2)}`
+      )
+    );
+
+    // Build batch request for all the storage keys of the batch.
+    const reqs = keys.map((key, index) => ({
+      method: 'state_getStorage',
+      params: [key],
+      id: batchIndex * batchSize + index + 1,
+      jsonrpc: '2.0'
+    }));
+
+    // Query batch of storage items
+    await fetchJson(
+      provider.connection,
+      JSON.stringify(reqs),
+      (payload: {
+        error?: { code?: number; data?: any; message?: string };
+        result?: any;
+      }) => {
+        if (payload.error) {
+          const error: any = new Error(payload.error.message);
+          error.code = payload.error.code;
+          error.data = payload.error.data;
+          throw error;
+        }
+        return payload;
+      }
+    ).then(
+      (payloads: { id: number; result: string }[]) => {
+        for (const payload of payloads) {
+          // Computes "system.account" key for given address
+          // Retrieves storage data for the "system.account" key
+          // account data structure (little endian):
+          // {
+          //   nonce: 8 bits
+          //   consumers: 8 bits
+          //   providers: 8 bits
+          //   sufficients: 8 bits
+          //   data: {
+          //     free: 32 bits
+          //     reserved: 32 bits
+          //     miscFrozen: 32 bits
+          //     feeFrozen: 32 bits
+          //   }
+          // }
+          // Converts the bigint into number.
+          // Result won't be precise, but should be lower than real value.
+
+          const free =
+            payload.result.length >= 2 + 8 + 8 + 8 + 8 + 32
+              ? Number(
+                  readLittleEndianBigInt(
+                    payload.result.substring(
+                      2 + 8 + 8 + 8 + 8,
+                      2 + 8 + 8 + 8 + 8 + 32
+                    )
+                  )
+                )
+              : 0;
+          balances[getAddress(addressBatch[(payload.id - 1) % batchSize])] =
+            free;
+        }
+        return;
+      },
+      (error) => {
+        throw error;
+      }
+    );
+  }
+  return balances;
 }
