e21185734319723c52a10a8de8209f34599c06a7
Chaitanya <yourchaitu@gmail.com>
2023-02-28 00:37:05 +0530

    Update src/strategies/lqty-proxy-stakers/index.ts

--- src/strategies/lqty-proxy-stakers/index.ts
+++ src/strategies/lqty-proxy-stakers/index.ts
@@ -2,8 +2,8 @@ import { BigNumberish } from '@ethersproject/bignumber';
 import { Multicaller } from '../../utils';
 import { formatUnits } from '@ethersproject/units';
 
-export const author = 'bonustrack';
-export const version = '0.1.1';
+export const author = 'majkic99';
+export const version = '0.1.0';
 
 const abiStaking = ['function stakes(address) public view returns (uint256)'];
 const abiProxyRegistry = [
