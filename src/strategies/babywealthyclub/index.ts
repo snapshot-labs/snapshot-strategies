e96dd774ac55315b4c604634bef1af78bc8b7653
claibornej <>
2023-04-07 12:18:56 +0800

    Change babywealthyclub strategy

--- src/strategies/babywealthyclub/index.ts
+++ src/strategies/babywealthyclub/index.ts
@@ -1,14 +1,10 @@
-import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
-import { formatUnits } from '@ethersproject/units';
-import { multicall } from '../../utils';
+// import { formatUnits } from '@ethersproject/units';
+import { getProvider, getSnapshots } from '../../utils';
+import strategies from '..';
+// import { multicall } from '../../utils';
 
-export const author = 'gp6284';
-export const version = '0.1.0';
-
-const abi = [
-  'function balanceOf(address account) external view returns (uint256)'
-];
-const BWC_ADDRESS = '0xb7F7c7D91Ede27b019e265F8ba04c63333991e02';
+export const author = 'claibornej';
+export const version = '0.2.0';
 
 export async function strategy(
   space,
@@ -18,31 +14,45 @@ export async function strategy(
   options,
   snapshot
 ) {
-  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
-  const erc20Balance = await erc20BalanceOfStrategy(
-    space,
+  const promises: any = [];
+  const blocks = await getSnapshots(
     network,
+    snapshot,
     provider,
-    addresses,
-    options,
-    snapshot
-  );
-  const response = await multicall(
-    network,
-    provider,
-    abi,
-    addresses.map((address: any) => [BWC_ADDRESS, 'balanceOf', [address]]),
-    { blockTag }
+    options.strategies.map((s) => s.network || network)
   );
 
-  return Object.fromEntries(
-    addresses.map((address, i) => [
-      address,
-      parseFloat(formatUnits(response[i].toString(), 0)) > 0
-        ? Math.floor(
-            erc20Balance[addresses[i]] / (options.weighted || 10000000000)
-          )
-        : 0
-    ])
-  );
+  for (const strategy of options.strategies) {
+    // If snapshot is taken before a network is activated then ignore its strategies
+    if (
+      options.startBlocks &&
+      blocks[strategy.network] < options.startBlocks[strategy.network]
+    ) {
+      continue;
+    }
+
+    promises.push(
+      strategies[strategy.name].strategy(
+        space,
+        strategy.network,
+        getProvider(strategy.network),
+        addresses,
+        strategy.params,
+        blocks[strategy.network]
+      )
+    );
+  }
+
+  const results = await Promise.all(promises);
+  const resp = results.reduce((finalResults: any, strategyResult: any) => {
+    for (const [address, value] of Object.entries(strategyResult)) {
+      if (!finalResults[address]) {
+        finalResults[address] = 0;
+      }
+      finalResults[address] += value;
+    }
+    return finalResults;
+  }, {});
+
+  return resp;
 }
