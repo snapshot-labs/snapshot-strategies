78909b9181a17aca559c2d98e8816a6326ac8a91
Archethect <archethect@gmail.com>
2023-04-29 20:05:43 +0200

    [battlefly-vgfly-and-staked-gfly]Feature/battlefly dao voting upgrade for founders voting (#1146)
    * Add support for staked V1 and V2 voting
    
    * Fix math bug
    
    * Fix to use gFLY backing instead of Magic backing
    
    * Fix version number + description

--- src/strategies/battlefly-vgfly-and-staked-gfly/index.ts
+++ src/strategies/battlefly-vgfly-and-staked-gfly/index.ts
@@ -4,7 +4,7 @@ import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
 import { formatUnits } from '@ethersproject/units';
 
 export const author = 'Archethect';
-export const version = '0.0.1';
+export const version = '0.0.2';
 
 const abi = [
   'function balanceOf(address account) external view returns (uint256)',
@@ -20,9 +20,14 @@ const calcVotingPower = (
   stakedAsString: string,
   stakedLPAsString: string,
   vestingAsString: string,
+  founderStakes: { type: string }[],
   claimableOf: BigNumberish,
   decimals: number,
-  tokenWeight: number
+  tokenWeight: number,
+  v1VaultGFlyStaked: BigNumber,
+  v2VaultGFlyStaked: BigNumber,
+  totalV1FoundersStaked: BigNumber,
+  totalV2FoundersStaked: BigNumber
 ) => {
   const staked = parseFloat(
     formatUnits(BigNumber.from(stakedAsString), decimals)
@@ -33,7 +38,30 @@ const calcVotingPower = (
   const vesting = parseFloat(
     formatUnits(BigNumber.from(vestingAsString).sub(claimableOf), decimals)
   );
-  return staked + vesting + stakedLP * tokenWeight;
+
+  let v1 = 0;
+  let v2 = 0;
+
+  founderStakes.map((element) => {
+    if (element.type === 'v1') {
+      v1 += 1;
+    } else if (element.type === 'v2') {
+      v2 += 1;
+    }
+  });
+
+  const founders = parseFloat(
+    formatUnits(
+      BigNumber.from(v1)
+        .mul(v1VaultGFlyStaked.div(totalV1FoundersStaked))
+        .add(
+          BigNumber.from(v2).mul(v2VaultGFlyStaked.div(totalV2FoundersStaked))
+        ),
+      decimals
+    )
+  );
+
+  return staked + vesting + stakedLP * tokenWeight + founders;
 };
 
 export async function strategy(
@@ -60,6 +88,12 @@ export async function strategy(
         },
         first: 1000
       },
+      founderStakes: {
+        __args: {
+          first: 1000
+        },
+        type: true
+      },
       id: true,
       staked: true,
       stakedLP: true,
@@ -67,13 +101,40 @@ export async function strategy(
     }
   };
 
+  // Setup subgraph query to fetch amount of gFLY staked by V1 and V2 vaults
+  const paramsV1AndV2 = {
+    _meta: {
+      block: {
+        number: true
+      }
+    },
+    accounts: {
+      __args: {
+        where: {
+          id_in: [
+            options.v1FoundersVault.toLowerCase(),
+            options.v2FoundersVault.toLowerCase()
+          ]
+        }
+      },
+      id: true,
+      staked: true
+    }
+  };
+
   if (snapshot !== 'latest') {
     // @ts-ignore
     params.accounts.__args.block = { number: snapshot };
+    // @ts-ignore
+    paramsV1AndV2.accounts.__args.block = params.accounts.__args.block;
   }
 
   const result = await subgraphRequest(DF_SUBGRAPH_URL, {
     ...params
+  });
+
+  const resultV1andV2 = await subgraphRequest(DF_SUBGRAPH_URL, {
+    ...paramsV1AndV2
   });
 
   // Take the same block number than from subgraph for consistency
@@ -91,7 +152,9 @@ export async function strategy(
       [options.lpAddress, 'token1', []],
       [options.lpAddress, 'getReserves', []],
       [options.lpAddress, 'totalSupply', []],
-      [options.lpAddress, 'decimals', []]
+      [options.lpAddress, 'decimals', []],
+      [options.foundersToken, 'balanceOf', [options.v1FoundersVault]],
+      [options.foundersToken, 'balanceOf', [options.v2FoundersVault]]
     ],
     { blockTag }
   );
@@ -103,6 +166,8 @@ export async function strategy(
   const lpTokenReserves = fetchContractData[2];
   const lpTokenTotalSupply = fetchContractData[3][0];
   const lpTokenDecimals = fetchContractData[4][0];
+  const totalV1FoundersStaked = fetchContractData[5][0];
+  const totalV2FoundersStaked = fetchContractData[6][0];
 
   // calculate single lp token weight
 
@@ -128,6 +193,17 @@ export async function strategy(
   );
   const claimableOfResult: Record<string, BigNumberish> = await multi.execute();
 
+  let v1VaultGFlyStaked;
+  let v2VaultGFlyStaked;
+
+  resultV1andV2.accounts.map((element) => {
+    if (element.id === options.v1FoundersVault.toLowerCase()) {
+      v1VaultGFlyStaked = BigNumber.from(element.staked);
+    } else if (element.id === options.v2FoundersVault.toLowerCase()) {
+      v2VaultGFlyStaked = BigNumber.from(element.staked);
+    }
+  });
+
   return Object.fromEntries(
     result.accounts.map((a) => [
       getAddress(a.id),
@@ -135,9 +211,14 @@ export async function strategy(
         a.staked,
         a.stakedLP,
         a.vesting,
+        a.founderStakes,
         claimableOfResult[getAddress(a.id)],
         options.decimals,
-        tokenWeight
+        tokenWeight,
+        v1VaultGFlyStaked,
+        v2VaultGFlyStaked,
+        BigNumber.from(totalV1FoundersStaked),
+        BigNumber.from(totalV2FoundersStaked)
       )
     ])
   );
