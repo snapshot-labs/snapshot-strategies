eaac4aca92fd689d3ce0e3808e82a39b57292a0d
sophiahdew <59624772+sophiahdew@users.noreply.github.com>
2023-05-23 11:40:02 -0700

    Removed submitting and signing passport
    We updated the passport API to no longer require a signed message by the user to submit a passport. I updated that functionality in the validation functions

--- src/validations/passport-gated/index.ts
+++ src/validations/passport-gated/index.ts
@@ -1,4 +1,3 @@
-import snapshot from '@snapshot-labs/snapshot.js';
 import fetch from 'cross-fetch';
 import Validation from '../validation';
 import dotenv from 'dotenv';
@@ -6,9 +5,6 @@ dotenv.config({ path: '.env' });
 
 const API_KEY = process.env.NEXT_PUBLIC_GC_API_KEY 
 const SCORER_ID = process.env.NEXT_PUBLIC_GC_SCORER_ID
-
-const SIGNING_MESSAGE_URI = 'https://api.scorer.gitcoin.co/registry/signing-message'
-const SUBMIT_PASSPORT_URI = 'https://api.scorer.gitcoin.co/registry/submit-passport'
 
 const headers = API_KEY ? ({
   'Content-Type': 'application/json',
@@ -24,55 +20,8 @@ export default class extends Validation {
   public description =
     'Protect your proposals from spam and vote manipulation by requiring users to have a Gitcoin Passport.';
 
-  async getSigningMessage() {
-    try {
-        const response = await fetch(SIGNING_MESSAGE_URI, {
-        headers
-        })
-        const json = await response.json()
-        return json
-    } catch (err) {
-        console.log('error: ', err)
-    }
-  }
-
-  async submitPassport() {
-    try {
-      const { message, nonce } = await this.getSigningMessage()
-
-      const provider = snapshot.utils.getProvider(this.network);
-      const signer = await provider.getSigner()
-
-      let signature = '';
-      try {
-        signature = await signer.signMessage(message);
-      } catch (err) {
-        console.log('Error signing message:', err);
-      }
-      
-      const address = this.author;
-
-      const response = await fetch(SUBMIT_PASSPORT_URI, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-          address,
-          scorer_id: SCORER_ID,
-          signature,
-          nonce
-        })
-      })
-      const data = await response.json()
-      console.log('data:', data)
-    } catch (err) {
-      console.log('error: ', err)
-    }
-  }
-
   async validate(currentAddress = this.author): Promise<boolean> {
       const GET_PASSPORT_SCORE_URI = `https://api.scorer.gitcoin.co/registry/score/${SCORER_ID}/${currentAddress}`
-      await this.getSigningMessage();
-      await this.submitPassport();
 
       try {
         const response = await fetch(GET_PASSPORT_SCORE_URI, {
