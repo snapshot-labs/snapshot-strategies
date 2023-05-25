ab53d50b5d74dc9f23ef75a7b2b6bd2b76ccfe75
Wolf <wolfwarrier14@gmail.com>
2023-05-25 10:01:14 +0000

    get balance of locker id

--- src/strategies/starlay-ve-balance-of-locker-id/index.ts
+++ src/strategies/starlay-ve-balance-of-locker-id/index.ts
@@ -5,8 +5,45 @@ import { Multicaller } from '../../utils';
 export const author = 'bonustrack';
 export const version = '0.1.1';
 
-const locker_id_abi = [
-  'function ownerToId(address account) external view returns (uint256)'
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address'
+      }
+    ],
+    name: 'ownerToId',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_lockerId',
+        type: 'uint256'
+      }
+    ],
+    name: 'balanceOfLockerId',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    stateMutability: 'view',
+    type: 'function'
+  }
 ];
 
 export async function strategy(
@@ -19,14 +56,19 @@ export async function strategy(
 ): Promise<Record<string, number>> {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
-  const multi = new Multicaller(network, provider, locker_id_abi, { blockTag });
+  const multi = new Multicaller(network, provider, abi, { blockTag });
   addresses.forEach((address) =>
-    multi.call(address, options.address, 'balanceOf', [address])
+    multi.call(address, options.address, 'ownerToId', [address])
   );
   const locker_id_result: Record<string, BigNumberish> = await multi.execute();
 
+  Object.entries(locker_id_result).map(([address, locker_id]) =>
+    multi.call(address, options.address, 'balanceOfLockerId', [locker_id])
+  );
+  const balance_result: Record<string, BigNumberish> = await multi.execute();
+
   return Object.fromEntries(
-    Object.entries(locker_id_result).map(([address, balance]) => [
+    Object.entries(balance_result).map(([address, balance]) => [
       address,
       parseFloat(formatUnits(balance, options.decimals))
     ])
