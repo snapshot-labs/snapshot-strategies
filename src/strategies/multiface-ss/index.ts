f1c5f5b092c8f5f3ba493fc6b586b9acf4033174
Tatiana Zhak <vzhik_85@mail.ru>
2023-05-26 12:42:33 +0700

    add limit request

--- src/strategies/multiface-ss/index.ts
+++ src/strategies/multiface-ss/index.ts
@@ -10,6 +10,9 @@ const abi = [
   'function balanceOf(address account) external view returns (uint256)'
 ];
 
+const LIMIT_ADDRESSES = 500;
+const LIMIT_QUERY_REQUESTS = 4;
+
 export async function strategy(
   space,
   network,
@@ -33,20 +36,32 @@ export async function strategy(
   });
 
   if (res.status !== 200) {
-    throw new Error('Ошибка при выполнении запроса');
+    throw new Error('Error while executing request');
   }
 
   const addressList = await res.json();
 
+  let requestsCount: number =
+    Math.ceil(addressList.length / LIMIT_ADDRESSES) || 0;
+  if (requestsCount === 0) throw new Error('Empty array of addresses');
+  requestsCount =
+    requestsCount > LIMIT_QUERY_REQUESTS ? LIMIT_QUERY_REQUESTS : requestsCount;
+
   const addressListObjects: { address: string; balance: number }[] =
     addressList.map(([address, balance]) => ({ [address]: balance }));
 
-  addressList.forEach((address) =>
-    multi.call(address[0], options.address, 'balanceOf', [address[0]])
-  );
+  let addressListBalance: Record<string, BigNumberish> = {};
+  for (let i = 0; i < requestsCount; i++) {
+    addressList
+      .slice(i * LIMIT_ADDRESSES, (i + 1) * LIMIT_ADDRESSES)
+      .forEach((address) =>
+        multi.call(address[0], options.address, 'balanceOf', [address[0]])
+      );
 
-  const addressListBalance: Record<string, BigNumberish> =
-    await multi.execute();
+    const addressListBalancePart: Record<string, BigNumberish> =
+      await multi.execute();
+    addressListBalance = { ...addressListBalance, ...addressListBalancePart };
+  }
 
   const addressObject = addressListObjects.reduce(
     (accumulator: any, currentValue: any) => ({
@@ -56,11 +71,13 @@ export async function strategy(
     {}
   );
 
-  return Object.fromEntries(
+  const result = Object.fromEntries(
     Object.entries(addressListBalance).map(([address, balance]) => [
       address,
       parseFloat(addressObject[address] || 0) +
         parseFloat(formatUnits(balance, options.decimals))
     ])
   );
+
+  return result;
 }
