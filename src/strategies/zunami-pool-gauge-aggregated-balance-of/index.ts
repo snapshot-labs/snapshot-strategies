87de94b28888247b72b5e9e8fb54bd0bd2d5d80e
fextr <extr1811@gmail.com>
2023-05-29 14:13:53 +0400

    [zunami-pool-gauge-aggregated-balance-of] add frax staking balances

--- src/strategies/zunami-pool-gauge-aggregated-balance-of/index.ts
+++ src/strategies/zunami-pool-gauge-aggregated-balance-of/index.ts
@@ -1,4 +1,4 @@
-import { BigNumberish } from '@ethersproject/bignumber';
+import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
 import { formatUnits } from '@ethersproject/units';
 import { call, Multicaller } from '../../utils';
 
@@ -8,7 +8,9 @@ export const version = '1.0.0';
 const zunamiSnapshotHelperAbi = [
   'function aggregatedBalanceOf(address _account) external view returns (uint256)'
 ];
-
+const fraxStakingAbi = [
+  'function lockedLiquidityOf(address account) external view returns (uint256)'
+];
 const curveAbi = ['function get_virtual_price() view returns (uint256)'];
 
 interface StrategyOptions {
@@ -16,6 +18,7 @@ interface StrategyOptions {
   decimals: number;
   lpPriceDecimals: number;
   curvePoolAddress: string;
+  fraxStakingAddress: string;
   blackListAddresses: string[];
 }
 
@@ -41,23 +44,111 @@ export async function strategy(
     )
   );
 
-  const multi = new Multicaller(network, provider, zunamiSnapshotHelperAbi, {
-    blockTag
-  });
   const blackListAddressesArr = Array.from(options.blackListAddresses).map(
     (address) => address.toLowerCase()
   );
-  addresses
-    .filter((address) => !blackListAddressesArr.includes(address.toLowerCase()))
-    .forEach((address) =>
-      multi.call(address, options.address, 'aggregatedBalanceOf', [address])
+  const filteredAddresses = addresses.filter(
+    (address) => !blackListAddressesArr.includes(address.toLowerCase())
+  );
+
+  const aggregatedResult: Record<string, BigNumberish> =
+    await getAggregatedBalance(
+      network,
+      provider,
+      blockTag,
+      filteredAddresses,
+      options.address
     );
-  const result: Record<string, BigNumberish> = await multi.execute();
+
+  const fraxStakingResult: Record<string, BigNumberish> =
+    await getFraxStakingBalance(
+      network,
+      provider,
+      blockTag,
+      filteredAddresses,
+      options.fraxStakingAddress
+    );
+
+  const result: [string, BigNumberish][] = mergeResults(
+    aggregatedResult,
+    fraxStakingResult
+  );
 
   return Object.fromEntries(
-    Object.entries(result).map(([address, balance]) => [
+    result.map(([address, balance]) => [
       address,
       parseFloat(formatUnits(balance, options.decimals)) * lpPrice
     ])
   );
+}
+
+async function getFraxStakingBalance(
+  network,
+  provider,
+  blockTag,
+  filteredAddresses: string[],
+  fraxStakingAddress: string
+): Promise<Record<string, BigNumberish>> {
+  const fraxStakingMulti = new Multicaller(network, provider, fraxStakingAbi, {
+    blockTag
+  });
+  filteredAddresses.forEach((address) =>
+    fraxStakingMulti.call(address, fraxStakingAddress, 'lockedLiquidityOf', [
+      address
+    ])
+  );
+  const fraxStakingResult: Record<string, BigNumberish> =
+    await fraxStakingMulti.execute();
+
+  return fraxStakingResult;
+}
+
+async function getAggregatedBalance(
+  network,
+  provider,
+  blockTag,
+  filteredAddresses: string[],
+  contractAddress: string
+): Promise<Record<string, BigNumberish>> {
+  const multi = new Multicaller(network, provider, zunamiSnapshotHelperAbi, {
+    blockTag
+  });
+  filteredAddresses.forEach((address) =>
+    multi.call(address, contractAddress, 'aggregatedBalanceOf', [address])
+  );
+  const aggregatedResult: Record<string, BigNumberish> = await multi.execute();
+
+  return aggregatedResult;
+}
+
+function mergeResults(
+  aggregatedResult: Record<string, BigNumberish>,
+  fraxStakingResult: Record<string, BigNumberish>
+): [string, BigNumberish][] {
+  const fraxStakingEntries = Object.entries(fraxStakingResult);
+  const fraxStakingResultMap: Map<string, BigNumberish> = new Map(
+    fraxStakingEntries
+  );
+  const aggregatedPlusStaking: [string, BigNumberish][] = Object.entries(
+    aggregatedResult
+  ).map(([address, balance]) => {
+    const fraxStakingBalance = fraxStakingResultMap.get(address);
+    if (fraxStakingBalance) {
+      return [
+        address,
+        BigNumber.from(balance).add(BigNumber.from(fraxStakingBalance))
+      ];
+    }
+    return [address, balance];
+  });
+  const aggregatedPluStakingMap: Map<string, BigNumberish> = new Map(
+    aggregatedPlusStaking
+  );
+  fraxStakingEntries.map(([address, balance]) => {
+    if (!aggregatedPluStakingMap.has(address)) {
+      aggregatedPlusStaking.push([address, balance]);
+    }
+  });
+
+  return aggregatedPlusStaking;
 }
