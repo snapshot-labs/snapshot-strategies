ee7f4c34f285bb7cbc19cdda30bdb4501f9c9683
Asgeir <asgeir@cluda.com>
2023-06-30 09:54:33 +0200

    Make it possible to add a cutom backend URL

--- src/strategies/delegate-registry-v2/index.ts
+++ src/strategies/delegate-registry-v2/index.ts
@@ -4,22 +4,24 @@ import { StaticJsonRpcProvider } from '@ethersproject/providers';
 export const author = 'gnosis-guild';
 export const version = '0.0.1';
 
-const BACKEND_URL = 'https://delegate-registry-backend.vercel.app';
+const DEFAULT_BACKEND_URL = 'https://delegate-registry-backend.vercel.app';
 
-type Params = any;
+type Params = {
+  backendUrl: string;
+};
 
 export async function strategy(
   space: string,
   network: string,
   provider: StaticJsonRpcProvider,
   addresses: string[],
-  options: Params,
+  options: Params = { backendUrl: DEFAULT_BACKEND_URL },
   snapshot: string | number
 ): Promise<Record<string, number>> {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
   const response = await fetch(
-    `${BACKEND_URL}/api/${space}/snapshot/${blockTag}/strategy-formatted-vote-weights`,
+    `${options.backendUrl}/api/${space}/snapshot/${blockTag}/strategy-formatted-vote-weights`,
     {
       method: 'POST',
       headers: {
