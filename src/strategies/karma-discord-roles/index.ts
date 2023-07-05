1cda3c2b78202b6a94c17b99aac8e128d967223b
Arthh <arthur.ulhoa28@gmail.com>
2023-07-05 18:10:13 -0300

    feat: readme

--- src/strategies/karma-discord-roles/index.ts
+++ src/strategies/karma-discord-roles/index.ts
@@ -6,25 +6,24 @@ export const version = '0.0.1';
 
 const KARMA_API = 'https://api.karmahq.xyz/api/dao/discordUsers';
 
-export async function strategy(options): Promise<Record<string, number>> {
-  const { discordId, roles } = options;
+export async function strategy(
+  options: any,
+  addresses: any
+): Promise<Record<string, number>> {
+  const { name, roles } = options;
 
-  const response = await fetch(`${KARMA_API}/${discordId}/${roles.join(',')}`);
-
-  if (response.status >= 400) {
-    throw new Error('Bad response from server');
-  }
+  const response = await fetch(`${KARMA_API}/${name}/${roles.join(',')}`);
 
   const { data } = await response.json();
 
   const votingPower: Record<string, number> = {};
 
-  data.delegates?.forEach((user) => {
-    const checksumAddress = getAddress(user.publicAddress);
+  const userAddresses = data.delegates.map((user) => user.publicAddress);
+
+  [...addresses, userAddresses].forEach((address) => {
+    const checksumAddress = getAddress(address);
     votingPower[checksumAddress] = 1;
   });
 
   return votingPower;
 }
-
-(async () => await strategy({ discordId: 'apecoin', roles: ['assembly'] }))();
