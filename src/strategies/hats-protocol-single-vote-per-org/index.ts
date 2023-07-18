2b9eb49acddfe327639f840821b07645661103ea
Jacob Homanics <homanicsjake@gmail.com>
2023-07-18 01:48:47 -0500

    functional but dirty

--- src/strategies/hats-protocol-single-vote-per-org/index.ts
+++ src/strategies/hats-protocol-single-vote-per-org/index.ts
@@ -1,23 +1,20 @@
-// import { multicall } from '@snapshot-labs/snapshot.js/dist/utils';
+// import { multicall } from '../../utils';
 import { subgraphRequest } from '../../utils';
 // import { formatUnits } from '@ethersproject/units';
 // import { BigNumberish } from '@ethersproject/bignumber';
 // import { Multicaller } from '../../utils';
 import { utils } from 'ethers';
+import { Multicaller } from '../../utils';
 
 const subgraphURL = `https://api.thegraph.com/subgraphs/name/hats-protocol/hats-v1-optimism`;
 
 export const author = 'hotmanics';
 export const version = '0.1.1';
 
-// const abi = [
-//   'function balanceOf(address _wearer, uint256 _hatId) public view returns (uint256 balance)',
-//   'function viewHat(uint256 _hatId) public view returns (string memory details, uint32 maxSupply, uint32 supply, address eligibility, address toggle, string memory imageURI, uint16 lastHatId, bool mutable_, bool active)',
-//   'function isEligible(address _wearer, uint256 _hatId) external view returns (bool eligible)'
-// ];
+const abi = [
+  'function isEligible(address _wearer, uint256 _hatId) external view returns (bool eligible)'
+];
 
-//The goal:
-//Within 5 requests, get all hats and their wearers.
 async function subgraphRequestHats() {
   const params = {
     tree: {
@@ -38,16 +35,19 @@ async function subgraphRequestHats() {
 }
 
 function checkIfExists(address, tree) {
-  let flag = false;
+  const addressWithHats = <any>[];
   tree.hats.forEach((hat) => {
     hat.wearers.forEach((wearer) => {
       if (utils.getAddress(wearer.id) === address) {
-        flag = true;
+        const addressWithHat = {
+          address: utils.getAddress(wearer.id),
+          hat: BigInt(hat.id)
+        };
+        addressWithHats.push(addressWithHat);
       }
     });
   });
-
-  return flag;
+  return addressWithHats;
 }
 
 export async function strategy(
@@ -58,17 +58,31 @@ export async function strategy(
   options,
   snapshot
 ): Promise<Record<string, number>> {
-  // const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
   const result = await subgraphRequestHats();
-
-  const wearersInAddresses = {};
+  let wearersInAddresses = <any>[];
 
   addresses.forEach((address) => {
-    wearersInAddresses[address] = checkIfExists(address, result.tree) ? 1 : 0;
+    const res = checkIfExists(address, result.tree);
+    wearersInAddresses = wearersInAddresses.concat(res);
   });
 
-  console.log(wearersInAddresses);
-  // const response = await multicall(network, provider, abi, wearer)
-  return wearersInAddresses;
+  const multi = new Multicaller(network, provider, abi, { blockTag });
+
+  wearersInAddresses.forEach((wearer) => {
+    multi.call(wearer.address, options.address, 'isEligible', [
+      wearer.address,
+      wearer.hat
+    ]);
+  });
+
+  const multiResult: Record<any, any> = await multi.execute();
+
+  return Object.fromEntries(
+    Object.entries(multiResult).map(([address, isValid]) => [
+      address,
+      isValid ? 1 : 0
+    ])
+  );
 }
