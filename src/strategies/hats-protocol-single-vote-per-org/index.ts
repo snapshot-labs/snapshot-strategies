58b1a3c715affc8ae3f19bf2cf9fe6e6b579af24
Jacob Homanics <homanicsjake@gmail.com>
2023-07-18 03:03:45 -0500

    fully functional - still dirty

--- src/strategies/hats-protocol-single-vote-per-org/index.ts
+++ src/strategies/hats-protocol-single-vote-per-org/index.ts
@@ -1,25 +1,27 @@
-// import { multicall } from '../../utils';
 import { subgraphRequest } from '../../utils';
-// import { formatUnits } from '@ethersproject/units';
-// import { BigNumberish } from '@ethersproject/bignumber';
-// import { Multicaller } from '../../utils';
 import { utils } from 'ethers';
-import { Multicaller } from '../../utils';
-
-const subgraphURL = `https://api.thegraph.com/subgraphs/name/hats-protocol/hats-v1-optimism`;
+import { StaticJsonRpcProvider } from '@ethersproject/providers';
+import { multicall } from '../../utils';
 
 export const author = 'hotmanics';
 export const version = '0.1.1';
 
+const subgraphURL = `https://api.thegraph.com/subgraphs/name/hats-protocol/hats-v1-optimism`;
+
 const abi = [
-  'function isEligible(address _wearer, uint256 _hatId) external view returns (bool eligible)'
+  'function isWearerOfHat(address _user, uint256 _hatId) external view returns (bool isWearer)'
 ];
 
-async function subgraphRequestHats() {
+async function subgraphRequestHats(humanReadableTreeId) {
+  const str1 = '0x';
+  const length = humanReadableTreeId.toString().length;
+  let resultString = str1.padEnd(10 - length, '0');
+  resultString = resultString + humanReadableTreeId.toString();
+
   const params = {
     tree: {
       __args: {
-        id: `0x00000002`
+        id: resultString
       },
       id: true,
       hats: {
@@ -60,29 +62,83 @@ export async function strategy(
 ): Promise<Record<string, number>> {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
-  const result = await subgraphRequestHats();
+  const result = await subgraphRequestHats(options.humanReadableTreeId);
+
   let wearersInAddresses = <any>[];
 
   addresses.forEach((address) => {
-    const res = checkIfExists(address, result.tree);
-    wearersInAddresses = wearersInAddresses.concat(res);
+    const wearer = checkIfExists(address, result.tree);
+    wearersInAddresses = wearersInAddresses.concat(wearer);
   });
 
   const multi = new Multicaller(network, provider, abi, { blockTag });
 
   wearersInAddresses.forEach((wearer) => {
-    multi.call(wearer.address, options.address, 'isEligible', [
+    multi.call(wearer.address, options.address, 'isWearerOfHat', [
       wearer.address,
       wearer.hat
     ]);
   });
 
-  const multiResult: Record<any, any> = await multi.execute();
+  const multiResult = await multi.execute();
 
-  return Object.fromEntries(
-    Object.entries(multiResult).map(([address, isValid]) => [
-      address,
-      isValid ? 1 : 0
-    ])
-  );
+  const myObj = {};
+  wearersInAddresses.forEach((wearer) => {
+    myObj[wearer.address] = 0;
+    for (const result of multiResult) {
+      if (wearer.address === result.address) {
+        myObj[wearer.address] = 1;
+        break;
+      }
+    }
+  });
+
+  return myObj;
+}
+
+class Multicaller {
+  public network: string;
+  public provider: StaticJsonRpcProvider;
+  public abi: any[];
+  public options: any = {};
+  public calls: any[] = [];
+  public paths: any[] = [];
+
+  constructor(
+    network: string,
+    provider: StaticJsonRpcProvider,
+    abi: any[],
+    options?
+  ) {
+    this.network = network;
+    this.provider = provider;
+    this.abi = abi;
+    this.options = options || {};
+  }
+
+  call(path, address, fn, params?): Multicaller {
+    this.calls.push([address, fn, params]);
+    this.paths.push(path);
+    return this;
+  }
+
+  async execute(): Promise<any> {
+    const obj = <any>[];
+    const result = await multicall(
+      this.network,
+      this.provider,
+      this.abi,
+      this.calls,
+      this.options
+    );
+    result.forEach((r, i) => {
+      obj.push({
+        address: this.paths[i],
+        value: r
+      });
+    });
+    this.calls = [];
+    this.paths = [];
+    return obj;
+  }
 }
