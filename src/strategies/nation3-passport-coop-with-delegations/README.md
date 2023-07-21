a5dfea8305f37f7fe8bf62425f281356d3f7a544
johnmark13 <mark.findlater@googlemail.com>
2023-07-21 15:14:33 +0100

    Strip out the ERC20

--- src/strategies/nation3-passport-coop-with-delegations/README.md
+++ src/strategies/nation3-passport-coop-with-delegations/README.md
@@ -2,11 +2,7 @@
 
 Calculates voting power using a cooperative model (one person one vote) based on a user's ownership of nation3 passport (_erc721 NFT_). It also takes into account whether the NFT owner delegated his voting power to another account (using the `setSigner` function)
 
-## Requires 2 input parameters:
-
-**erc20**
-
-The address of veNation tokens contract
+## Requires 1 input parameters:
 
 **erc721**
 
--- src/strategies/nation3-passport-coop-with-delegations/index.ts
+++ src/strategies/nation3-passport-coop-with-delegations/index.ts
@@ -4,16 +4,6 @@ import { Multicaller } from '../../utils';
 export const author = 'nation3';
 export const version = '0.2.0';
 
-//if we use balances we need this
-//const DECIMALS = 18;
-
-//if we use balances we need this
-// const balanceAbi = [
-//   'function balanceOf(address account) external view returns (uint256)'
-// ];
-
-const ownerAbi = ['function ownerOf(uint256 id) public view returns (address)'];
-
 const signerAbi = [
   'function signerOf(uint256 id) external view  returns (address)'
 ];
@@ -30,12 +20,6 @@ export async function strategy(
 ): Promise<Record<string, number>> {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
-  //if we use balances we need this
-  //const formattedAddressesThatVoted = addresses.map((addr) => getAddress(addr));
-
-  const erc721OwnerCaller = new Multicaller(network, provider, ownerAbi, {
-    blockTag
-  });
   const erc721SignerCaller = new Multicaller(network, provider, signerAbi, {
     blockTag
   });
@@ -46,11 +30,6 @@ export async function strategy(
     { blockTag }
   );
 
-  //if we use balances we need this
-  // const erc20BalanceCaller = new Multicaller(network, provider, balanceAbi, {
-  //   blockTag
-  // });
-
   erc721LastTokenIdCaller.call('lastTokenId', options.erc721, 'getNextId');
 
   const lastIndex = await erc721LastTokenIdCaller.execute();
@@ -58,24 +37,19 @@ export async function strategy(
 
   for (let i = 0; i < lastTokenId; i++) {
     erc721SignerCaller.call(i, options.erc721, 'signerOf', [i]);
-    erc721OwnerCaller.call(i, options.erc721, 'ownerOf', [i]);
   }
 
-  const [erc721Signers/*, erc721Owners*/]: [
-    Record<string, string>,
-    //Record<string, string>
+  const [erc721Signers]: [
+    Record<string, string>
   ] = await Promise.all([
-    erc721SignerCaller.execute(),
-    //erc721OwnerCaller.execute()
+    erc721SignerCaller.execute()
   ]);
 
-  //if we use balances we need this
-  //const erc721OwnersArr = Object.entries(erc721Owners);
   const erc721SignersArr = Object.entries(erc721Signers);
 
-  //here is where we would/could filter out passports based on token balances
-  //using the ownerAddress. The signer defaults to the owner if has not been set.
-  const eligibleAddresses = erc721SignersArr.map(([id, address]) => address);
+  const eligibleAddresses = erc721SignersArr
+    .map(([_, address]) => address)
+    .filter((address) => addresses.includes(address));
 
   return Object.fromEntries(
     eligibleAddresses.map((value, i) => [
