f1b3312aa70dbb2ba233af032f86416f8913e4ed
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-08-14 15:29:12 +0300

    refactor: remove optional subGraphURL

--- src/strategies/sablier-v1-deposit/index.ts
+++ src/strategies/sablier-v1-deposit/index.ts
@@ -11,8 +11,10 @@ const SUBGRAPH_URL = {
   '42': 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-kovan', // kovan
   '56': 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-bsc', // bsc
   '137': 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-matic', // polygon
-  '42161': 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-arbitrum', // arbitrum
-  '43114': 'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-avalanche' // avalanche
+  '42161':
+    'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-arbitrum', // arbitrum
+  '43114':
+    'https://api.thegraph.com/subgraphs/name/sablier-labs/sablier-avalanche' // avalanche
 };
 
 export const author = 'sablier-labs';
@@ -48,10 +50,7 @@ export async function strategy(
     // @ts-ignore
     params.streams.__args.block = { number: snapshot };
   }
-  const result = await subgraphRequest(
-    options.subGraphURL ? options.subGraphURL : SUBGRAPH_URL[network],
-    params
-  );
+  const result = await subgraphRequest(SUBGRAPH_URL[network], params);
   const score = Object.fromEntries(
     addresses.map((address) => [getAddress(address), 0])
   );
