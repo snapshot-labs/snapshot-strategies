c2c82b32529d1edd83f2f7ac96e15167d29eed58
VargaElod23 <vargaelod23@gmail.com>
2023-08-16 14:01:57 +0300

    simplifies implementation

--- src/strategies/taraxa-delegation/index.ts
+++ src/strategies/taraxa-delegation/index.ts
@@ -1,96 +1,14 @@
-import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
-import { Contract } from '@ethersproject/contracts';
+import { BigNumberish } from '@ethersproject/bignumber';
 import { formatUnits } from '@ethersproject/units';
 import { getAddress } from '@ethersproject/address';
+import { Multicaller } from '../../utils';
 
 export const author = 'Taraxa-project';
 export const version = '0.1.0';
 
 const abi = [
-  'function getDelegations(address delegator, uint32 batch) view returns (tuple(address account, tuple(uint256 stake, uint256 rewards) delegation)[] delegations, bool end)'
+  'function getTotalDelegation(address delegator) external view returns (uint256 total_delegation)'
 ];
-
-export default interface Delegation {
-  address: string;
-  stake: BigNumber;
-}
-
-interface ContractDelegationInfo {
-  rewards: BigNumber;
-  stake: BigNumber;
-}
-
-export interface ContractDelegation {
-  account: string;
-  delegation: ContractDelegationInfo;
-}
-
-const getTotalDelegationOfAddress = async (
-  address: string,
-  provider: any,
-  blockTag: string | number
-): Promise<Delegation> => {
-  const mainnetDpos = new Contract(
-    '0x00000000000000000000000000000000000000fe',
-    abi,
-    provider
-  );
-
-  let newDelegations: ContractDelegation[] = [];
-  let page = 0;
-  let hasNextPage = true;
-  while (hasNextPage) {
-    try {
-      const allDelegations = await mainnetDpos.getDelegations(address, page, {
-        blockTag
-      });
-
-      newDelegations = [...newDelegations, ...allDelegations.delegations];
-      hasNextPage = !allDelegations.end;
-      page++;
-    } catch (e) {
-      console.error(e);
-      hasNextPage = false;
-    }
-  }
-
-  const finalDelegations = newDelegations.map(
-    (delegation: ContractDelegation) => ({
-      address: delegation.account,
-      stake: delegation.delegation.stake,
-      rewards: delegation.delegation.rewards
-    })
-  );
-  return {
-    address,
-    stake: finalDelegations
-      .map((s) => s.stake)
-      .reduce((a, b) => a.add(b), BigNumber.from(0))
-  };
-};
-
-const getDelegations = async (
-  addresses,
-  provider,
-  snapshot
-): Promise<Record<string, number>> => {
-  const result: Record<string, BigNumberish> = {};
-  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
-  for (const address of addresses) {
-    const delegation = await getTotalDelegationOfAddress(
-      address,
-      provider,
-      blockTag
-    );
-    result[address] = delegation.stake;
-  }
-  return Object.fromEntries(
-    Object.entries(result).map(([address, stake]) => [
-      address,
-      parseFloat(formatUnits(stake, 18))
-    ])
-  );
-};
 
 export async function strategy(
   space,
@@ -100,13 +18,17 @@ export async function strategy(
   options,
   snapshot
 ) {
-  const delegations = await getDelegations(addresses, provider, snapshot);
-  if (Object.keys(delegations).length === 0) return {};
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
+  const multi = new Multicaller(network, provider, abi, { blockTag });
+  addresses.forEach((address: string) =>
+    multi.call(address, options.address, 'getTotalDelegation', [address])
+  );
+  const result: Record<string, BigNumberish> = await multi.execute();
   return Object.fromEntries(
-    addresses.map((address) => {
-      const addressScore = delegations[address] ? delegations[address] : 0;
-      return [getAddress(address), addressScore];
-    })
+    Object.entries(result).map(([address, balance]) => [
+      getAddress(address),
+      parseFloat(formatUnits(balance, options.decimals))
+    ])
   );
 }
