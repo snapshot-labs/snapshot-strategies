99dc4ca5b489916a1acaa97c306db907e20c557d
Santiago Gonzalez <hernangt12re3@gmail.com>
2023-08-29 15:57:38 -0500

    [passport-gated]: validate address using passport stamps + score

--- src/validations/passport-gated/index.ts
+++ src/validations/passport-gated/index.ts
@@ -1,9 +1,12 @@
 import fetch from 'cross-fetch';
-import Validation from '../validation';
 import snapshot from '@snapshot-labs/snapshot.js';
+
+import STAMPS from './stampsMetadata.json';
+import Validation from '../validation';
 
 // Create one from https://scorer.gitcoin.co/#/dashboard/api-keys
 const API_KEY = process.env.PASSPORT_API_KEY || '';
+const SCORER_ID = process.env.PASSPORT_SCORER_ID || '';
 
 const headers = API_KEY
   ? {
@@ -12,9 +15,29 @@ const headers = API_KEY
     }
   : undefined;
 
+// const GET_STAMPS_METADATA_URI = `https://api.scorer.gitcoin.co/registry/stamp-metadata`;
 const GET_PASSPORT_STAMPS_URI = `https://api.scorer.gitcoin.co/registry/stamps/`;
+const GET_PASSPORT_SCORE_URI = `https://api.scorer.gitcoin.co/registry/score/${SCORER_ID}/`;
+const POST_SUBMIT_PASSPORT_URI = `https://api.scorer.gitcoin.co/registry/submit-passport`;
 
-function hasValidIssuanceAndExpiration(credential, proposalTs) {
+const PASSPORT_SCORER_MAX_ATTEMPTS = 2;
+const PASSPORT_NOT_SUBMITTED_ERROR = 'Unable to get score for provided scorer.';
+
+const stampCredentials = STAMPS.map((stamp) => {
+  return {
+    id: stamp.id,
+    name: stamp.name,
+    description: stamp.description,
+    credentials: stamp.groups
+      .flatMap((group) => group.stamps)
+      .map((credential) => credential.name)
+  };
+});
+
+// Useful to get stamp metadata and update `stampsMetata.json`
+// console.log('stampCredentials', JSON.stringify(stampCredentials.map((s) => ({"const": s.id, title: s.name}))));
+
+function hasValidIssuanceAndExpiration(credential: any, proposalTs: string) {
   const issuanceDate = Number(
     new Date(credential.issuanceDate).getTime() / 1000
   ).toFixed(0);
@@ -27,46 +50,146 @@ function hasValidIssuanceAndExpiration(credential, proposalTs) {
   return false;
 }
 
+function hasStampCredential(stampId: string, credentials: Array<string>) {
+  const stamp = stampCredentials.find((stamp) => stamp.id === stampId);
+  if (!stamp) {
+    console.log('[passport] Stamp not supported', stampId);
+    throw new Error('Stamp not supported');
+  }
+  return credentials.some((credential) =>
+    stamp.credentials.includes(credential)
+  );
+}
+
+async function validateStamps(
+  currentAddress: string,
+  operator: string,
+  proposalTs: string,
+  requiredStamps: Array<string> = []
+): Promise<boolean> {
+  if (requiredStamps.length === 0) return true;
+
+  const stampsResponse = await fetch(GET_PASSPORT_STAMPS_URI + currentAddress, {
+    headers
+  });
+  const stampsData = await stampsResponse.json();
+
+  if (!stampsData?.items) {
+    console.log('[passport] Stamps Unknown error', stampsData);
+    throw new Error('Unknown error');
+  }
+  if (stampsData.items.length === 0) return false;
+
+  // check expiration for all stamps
+  const validStamps = stampsData.items
+    .filter((stamp: any) =>
+      hasValidIssuanceAndExpiration(stamp.credential, proposalTs)
+    )
+    .map((stamp: any) => stamp.credential.credentialSubject.provider);
+
+  if (operator === 'AND') {
+    return requiredStamps.every((stampId) =>
+      hasStampCredential(stampId, validStamps)
+    );
+  } else if (operator === 'OR') {
+    return requiredStamps.some((stampId) =>
+      hasStampCredential(stampId, validStamps)
+    );
+  }
+  return false;
+}
+
+function evalPassportScore(scoreData: any, minimumThreshold = 0): boolean {
+  // scoreData.evidence?.type === 'ThresholdScoreCheck' -> Returned if using Boolean Unique Humanity Scorer (should not be used)
+  if (scoreData.evidence?.type === 'ThresholdScoreCheck') {
+    return (
+      Number(scoreData.evidence.rawScore) > Number(scoreData.evidence.threshold)
+    );
+  }
+  // scoreData.score -> Returned if using Unique Humanity Score
+  return Number(scoreData.score) > minimumThreshold;
+}
+
+async function validatePassportScore(
+  currentAddress: string,
+  scoreThreshold: number
+): Promise<boolean> {
+  const scoreResponse = await fetch(GET_PASSPORT_SCORE_URI + currentAddress, {
+    headers
+  });
+  const scoreData = await scoreResponse.json();
+
+  if (!scoreResponse.ok && scoreData.detail !== PASSPORT_NOT_SUBMITTED_ERROR) {
+    console.log('[passport] Scorer Unknown error', scoreData);
+    throw new Error(`Unkown error: ${scoreData.detail}`);
+  }
+
+  // If first time using scorer, address needs to submit passport for scoring
+  if (!scoreResponse.ok && scoreData.detail === PASSPORT_NOT_SUBMITTED_ERROR) {
+    const submittedPassport = await fetch(POST_SUBMIT_PASSPORT_URI, {
+      headers,
+      method: 'POST',
+      body: JSON.stringify({ address: currentAddress, scorer_id: SCORER_ID })
+    });
+    const submissionData = await submittedPassport.json();
+
+    // Scorer done calculating passport score during submission
+    if (submittedPassport.ok && submissionData.status === 'DONE') {
+      return evalPassportScore(submissionData, scoreThreshold);
+    }
+  }
+
+  // Passport Score was already calculated
+  if (scoreResponse.ok && scoreData.status === 'DONE') {
+    return evalPassportScore(scoreData, scoreThreshold);
+  }
+
+  // Try to fetch Passport Score if still processing (scoreData.status === 'PROCESSING')
+  for (let i = 0; i < PASSPORT_SCORER_MAX_ATTEMPTS; i++) {
+    const scoreResponse = await fetch(GET_PASSPORT_SCORE_URI + currentAddress, {
+      headers
+    });
+    const scoreData = await scoreResponse.json();
+
+    if (scoreResponse.ok && scoreData.status === 'DONE') {
+      return evalPassportScore(scoreData, scoreThreshold);
+    }
+    console.log(
+      `[passport] Waiting for scorer... (${i}/${PASSPORT_SCORER_MAX_ATTEMPTS})`
+    );
+    await snapshot.utils.sleep(3e3);
+  }
+  return false;
+}
+
 export default class extends Validation {
   public id = 'passport-gated';
   public github = 'snapshot-labs';
-  public version = '0.1.0';
+  public version = '1.0.0';
   public title = 'Gitcoin Passport Gated';
   public description =
-    'Protect your proposals from spam and vote manipulation by requiring users to have a Gitcoin Passport.';
+    'Protect your proposals from spam and vote manipulation by requiring users to have a valid Gitcoin Passport.';
 
   async validate(currentAddress = this.author): Promise<boolean> {
     const requiredStamps = this.params.stamps || [];
     const operator = this.params.operator;
+    const scoreThreshold = this.params.scoreThreshold;
     if (!operator) throw new Error('Operator is required');
-
-    const stampsResponse = await fetch(
-      GET_PASSPORT_STAMPS_URI + currentAddress,
-      { headers }
-    );
-
-    const stampsData = await stampsResponse.json();
-
-    if (!stampsData?.items) {
-      console.log('[passport] Unknown error', stampsData);
-      throw new Error('Unknown error');
-    }
-    if (stampsData.items.length === 0) return false;
+    if (!scoreThreshold) throw new Error('Score Threshold is required');
 
     const provider = snapshot.utils.getProvider(this.network);
     const proposalTs = (await provider.getBlock(this.snapshot)).timestamp;
-    // check expiration for all stamps
-    const validStamps = stampsData.items
-      .filter((stamp) =>
-        hasValidIssuanceAndExpiration(stamp.credential, proposalTs)
-      )
-      .map((stamp) => stamp.credential.credentialSubject.provider);
+    const validStamps = await validateStamps(
+      currentAddress,
+      operator,
+      proposalTs,
+      requiredStamps
+    );
+    const validScore = await validatePassportScore(
+      currentAddress,
+      scoreThreshold
+    );
 
-    if (operator === 'AND') {
-      return requiredStamps.every((stamp) => validStamps.includes(stamp));
-    } else if (operator === 'OR') {
-      return requiredStamps.some((stamp) => validStamps.includes(stamp));
-    }
-    return false;
+    return validStamps && validScore;
   }
 }
