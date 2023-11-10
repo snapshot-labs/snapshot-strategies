de1fd431a4108b39b3b0f6bca9003c2cded3c92c
Jacob Homanics <homanicsjake@gmail.com>
2023-11-09 21:28:54 -0600

    added hat ids stategy

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -472,6 +472,7 @@ import * as stationScoreIfBadge from './station-score-if-badge';
 import * as stationConstantIfBadge from './station-constant-if-badge';
 import * as mangroveStationQVScaledToMGV from './mangrove-station-qv-scaled-to-mgv';
 import * as floki from './floki';
+import * as hatsProtocolHatIds from "./hats-protocol-hat-ids";
 
 const strategies = {
   'cap-voting-power': capVotingPower,
@@ -952,7 +953,8 @@ const strategies = {
   'station-score-if-badge': stationScoreIfBadge,
   'station-constant-if-badge': stationConstantIfBadge,
   'mangrove-station-qv-scaled-to-mgv': mangroveStationQVScaledToMGV,
-  floki
+  floki,
+  'hats-protocol-hat-ids': hatsProtocolHatIds
 };
 
 Object.keys(strategies).forEach(function (strategyName) {
