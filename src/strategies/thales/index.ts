46b062b3e6f6823eae0d7f1622f086f88d130c3d
Vladan Poklopic <vladanpoklopic@gmail.com>
2023-11-17 14:31:40 +0100

    Update Thales strategy to include Base stakers

--- src/strategies/thales/index.ts
+++ src/strategies/thales/index.ts
@@ -1,19 +1,26 @@
 import { getAddress } from '@ethersproject/address';
 import { formatUnits } from '@ethersproject/units';
-import { subgraphRequest } from '../../utils';
+import { getSnapshots, subgraphRequest } from '../../utils';
 
 export const author = 'vpoklopic';
-export const version = '1.0.3';
+export const version = '1.0.4';
+
+enum NetworkId {
+  Optimism = 10,
+  Arbitrum = 42161,
+  Base = 8453
+}
 
 const THALES_SUBGRAPH_URL = {
   optimism:
     'https://api.thegraph.com/subgraphs/name/thales-markets/thales-token',
   arbitrum:
-    'https://api.thegraph.com/subgraphs/name/thales-markets/thales-token-arbitrum'
+    'https://api.thegraph.com/subgraphs/name/thales-markets/thales-token-arbitrum',
+  base: 'https://api.studio.thegraph.com/query/11948/thales-token-base/version/latest'
 };
 
-function returnGraphParams(addresses: string[]) {
-  return {
+function returnGraphParams(addresses: string[], block: string | number) {
+  const graphParams = {
     stakers: {
       __args: {
         first: 1000,
@@ -29,6 +36,15 @@ function returnGraphParams(addresses: string[]) {
       totalStakedAmount: true
     }
   };
+
+  if (block !== 'latest') {
+    // @ts-ignore
+    graphParams.stakers.__args.block = {
+      number: block
+    };
+  }
+
+  return graphParams;
 }
 
 export async function strategy(
@@ -36,29 +52,31 @@ export async function strategy(
   _network,
   _provider,
   addresses,
-  options
+  options,
+  snapshot
 ) {
-  const optimismGraphParams = returnGraphParams(addresses);
-  if (options.blockOptimism !== undefined) {
-    // @ts-ignore
-    optimismGraphParams.stakers.__args.block = {
-      number: options.blockOptimism
-    };
-  }
+  const blocks = await getSnapshots(_network, snapshot, _provider, [
+    NetworkId.Optimism,
+    NetworkId.Arbitrum,
+    NetworkId.Base
+  ]);
 
-  const arbitrumGraphParams = returnGraphParams(addresses);
-  if (options.blockArbitrum !== undefined) {
-    // @ts-ignore
-    arbitrumGraphParams.stakers.__args.block = {
-      number: options.blockArbitrum
-    };
-  }
+  const optimismGraphParams = returnGraphParams(
+    addresses,
+    blocks[NetworkId.Optimism]
+  );
+  const arbitrumGraphParams = returnGraphParams(
+    addresses,
+    blocks[NetworkId.Arbitrum]
+  );
+  const baseGraphParams = returnGraphParams(addresses, blocks[NetworkId.Base]);
 
   const score = {};
 
-  const [optimismStakers, arbitrumStakers] = await Promise.all([
+  const [optimismStakers, arbitrumStakers, baseStakers] = await Promise.all([
     subgraphRequest(THALES_SUBGRAPH_URL.optimism, optimismGraphParams),
-    subgraphRequest(THALES_SUBGRAPH_URL.arbitrum, arbitrumGraphParams)
+    subgraphRequest(THALES_SUBGRAPH_URL.arbitrum, arbitrumGraphParams),
+    subgraphRequest(THALES_SUBGRAPH_URL.base, baseGraphParams)
   ]);
 
   // We are starting by mapping all Optimism stakers
@@ -74,6 +92,22 @@ export async function strategy(
   // Otherwise, just set Arbitrum staked amount as a score
   if (arbitrumStakers && arbitrumStakers.stakers) {
     arbitrumStakers.stakers.forEach((staker) => {
+      const key = getAddress(staker.id);
+      const stakedAmount = parseFloat(
+        formatUnits(staker.totalStakedAmount, options.decimals)
+      );
+      if (!!score[key]) {
+        score[key] += stakedAmount;
+      } else {
+        score[key] = stakedAmount;
+      }
+    });
+  }
+
+  // If the Optimism or Arbitrum staker is also staker on Base, add an amount
+  // Otherwise, just set Base staked amount as a score
+  if (baseStakers && baseStakers.stakers) {
+    baseStakers.stakers.forEach((staker) => {
       const key = getAddress(staker.id);
       const stakedAmount = parseFloat(
         formatUnits(staker.totalStakedAmount, options.decimals)
