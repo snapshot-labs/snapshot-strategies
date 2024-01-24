facab50b4fc3d1ee176c14ac9b38a18255b1e8b0
SpaceFi <82010251+SpaceFinance@users.noreply.github.com>
2024-01-24 09:56:43 +0800

    Update src/strategies/spacefi-blp/index.ts
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/strategies/spacefi-blp/index.ts
+++ src/strategies/spacefi-blp/index.ts
@@ -3,7 +3,7 @@ import { formatUnits } from '@ethersproject/units';
 import { Multicaller } from '../../utils';
 
 export const author = 'icepoint';
-export const version = '0.1.1';
+export const version = '0.1.0';
 
 const abi = [
   'function userInfo(address account) external view returns (uint256,uint256,uint256)'
