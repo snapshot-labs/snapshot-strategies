48982cad882b15fe934512b6b4b54ccd59a137a2
Chaitanya <yourchaitu@gmail.com>
2024-02-06 00:18:24 +0530

    Update src/strategies/planet-finance-v2/index.ts

--- src/strategies/planet-finance-v2/index.ts
+++ src/strategies/planet-finance-v2/index.ts
@@ -4,7 +4,7 @@ import { Multicaller } from '../../utils';
 import { strategy as erc20BalanceOfStrategy } from '../erc20-balance-of';
 
 export const author = 'defininja';
-export const version = '0.0.2';
+export const version = '0.1.0';
 
 //abi
 // v3 farm abi for user-info
