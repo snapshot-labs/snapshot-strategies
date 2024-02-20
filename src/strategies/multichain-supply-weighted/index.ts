b6e7dae10005cc1b1b5a3057e657f6181611da94
Mitch Oz <divine_comedian@protonmail.com>
2024-02-20 14:11:42 -0600

    optimize some code

--- src/strategies/multichain-supply-weighted/index.ts
+++ src/strategies/multichain-supply-weighted/index.ts
@@ -13,32 +13,36 @@ export async function strategy(
   options,
   snapshot
 ) {
-  const promises: any = [];
-  const blocks = await getSnapshots(
+  const blocksPromise = getSnapshots(
     network,
     snapshot,
     provider,
     options.strategies.map((s) => s.network || network)
   );
-  // this gets the current circulating supply from Github
-  const response = await fetch(options.supplyApi, {
+  const responsePromise = fetch(options.supplyApi, {
     method: 'GET',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json'
     }
   });
+
+  const [blocks, response] = await Promise.all([
+    blocksPromise,
+    responsePromise
+  ]);
+
   const data = await response.json();
   const circulatingSupply = data[options.supplyField];
+
+  const promises: any = [];
   for (const strategy of options.strategies) {
-    // If snapshot is taken before a network is activated then ignore its strategies
     if (
       options.startBlocks &&
       blocks[strategy.network] < options.startBlocks[strategy.network]
     ) {
       continue;
     }
-    // this pushed the strategy results to the promises array
     promises.push(
       strategies[strategy.name].strategy(
         space,
@@ -52,18 +56,19 @@ export async function strategy(
   }
 
   const results = await Promise.all(promises);
+  console.log(results, 'results');
+  const finalResults: any = new Map(addresses.map((address) => [address, 0]));
 
-  return results.reduce((finalResults: any, strategyResult: any) => {
+  for (const strategyResult of results) {
     for (const [address, value] of Object.entries(strategyResult)) {
-      if (!finalResults[address]) {
-        finalResults[address] = 0;
-      }
-      // apply the sum of all tokens in strategies against the tokens circulating supply
-      // then apply the weight of the strategy to the result and save this as the final result
       const weightedPercentOfSupply =
         ((value as number) / circulatingSupply) * options.weight;
-      finalResults[address] += weightedPercentOfSupply;
+      finalResults.set(
+        address,
+        finalResults.get(address) + weightedPercentOfSupply
+      );
     }
-    return finalResults;
-  }, {});
+  }
+
+  return Object.fromEntries(finalResults);
 }
