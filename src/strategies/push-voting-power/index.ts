f6fa2941a161acc69783e9ca0d1abf67afe4098c
Shubham Patel <shubhamashishpatel@gmail.com>
2024-03-10 22:40:18 +0530

    [push-voting-power] Added support for tokens staked in Push V2 pool (#1425)
    * Added support for tokens staked in Push V2 pool
    
    * Update src/strategies/push-voting-power/index.ts
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    
    ---------
    
    Co-authored-by: Shubham <shubhamashishpatel@gmail.com"\n>
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/strategies/push-voting-power/index.ts
+++ src/strategies/push-voting-power/index.ts
@@ -2,8 +2,10 @@ import { formatUnits } from '@ethersproject/units';
 import { BigNumber } from '@ethersproject/bignumber';
 import { multicall } from '../../utils';
 
-export const author = 'mujtaba1747';
-export const version = '0.1.0';
+//0.1.0 was authered by mujtaba1747
+
+export const author = 'ethereum-push-notification-service';
+export const version = '0.2.0';
 
 const tokenBNtoNumber = (tokenBn) => {
   return (
@@ -14,7 +16,8 @@ const tokenBNtoNumber = (tokenBn) => {
 
 const sharedABI = [
   'function balanceOf(address user, address token) view returns (uint256)',
-  'function getAmountsOut(uint256 amountIn, address[] path) view returns (uint256[] amounts)'
+  'function getAmountsOut(uint256 amountIn, address[] path) view returns (uint256[] amounts)',
+  'function userFeesInfo(address user) view returns(uint256,uint256,uint256,uint256)'
 ];
 
 const wethABI = ['function balanceOf(address) view returns (uint256)'];
@@ -78,13 +81,36 @@ export async function strategy(
           [address.toLowerCase(), options.pushLPTokenAddr]
         ])
       ),
-
     { blockTag }
   );
 
-  const responseStakedPUSH = responseStaked.slice(0, addresses.length);
+   const responseStakedV2 = await multicall(
+    network,
+    provider,
+    sharedABI,
 
-  const responseStakedLP = responseStaked.slice(addresses.length);
+    addresses
+      .map((address: any) => [
+        options.pushStakingInfoCoreV2,
+        'userFeesInfo',
+        [address.toLowerCase()]
+      ]).concat(
+        addresses.map((address: any) => [
+          options.pushUniStakingV2,
+          'balanceOf',
+          [address.toLowerCase(), options.pushLPTokenAddr]
+        ])
+      ),
+    { blockTag }
+  ); 
+
+  const responseStakedPUSH = responseStaked.slice(0, addresses.length);
+  let responseStakedPUSHV2 = responseStakedV2.slice(0, addresses.length);
+  responseStakedPUSHV2 = responseStakedPUSHV2.map((value, i) => {return value[0].toString()}); 
+const responseStakedLP = responseStaked.slice(addresses.length);
+  const responseStakedLPV2 = responseStakedV2.slice(addresses.length);
+
+  
 
   const responseWETH = await multicall(
     network,
@@ -121,9 +147,7 @@ export async function strategy(
 
   // pushPrice and wethPrice are in terms of USDT
   const pushPrice = responseLPConversion[0]['amounts'][2].toNumber() / 1e6;
-
   const wethPrice = responseLPConversion[1]['amounts'][1].toNumber() / 1e6;
-
   const responseEPNSLPToken = await multicall(
     network,
     provider,
@@ -132,24 +156,30 @@ export async function strategy(
     { blockTag }
   );
 
+
   // Calculating price of EPNS-LP Tokens in terms of EPNS Tokens
   const uniLpTotalSupply = tokenBNtoNumber(responseEPNSLPToken[0][0]);
-
   const uniLpPrice =
     (pushAmountReserve * pushPrice + wethAmountReserve * wethPrice) /
     uniLpTotalSupply;
+const lpToPushRatio = uniLpPrice / pushPrice;
 
-  const lpToPushRatio = uniLpPrice / pushPrice;
-
+// console.log(uniLpTotalSupply)
   return Object.fromEntries(
     responseDelegatedPUSH.map((value, i) => [
       addresses[i],
 
-      // Voting Power = Delegated PUSH + Staked PUSH + Staked UNI-LP PUSH
+      // Voting Power = Delegated PUSH + Staked PUSH V1 + Staked PUSH V2 +Staked Uni-LP V2 + Staked UNI-LP PUSH
       parseFloat(formatUnits(value.toString(), options.decimals)) +
         parseFloat(
           formatUnits(responseStakedPUSH[i].toString(), options.decimals)
         ) +
+        parseFloat(formatUnits(responseStakedPUSHV2[i], options.decimals))+ 
+        parseFloat(
+          formatUnits(responseStakedLPV2[i].toString(), options.decimals)
+        ) *
+          lpToPushRatio
+        +
         parseFloat(
           formatUnits(responseStakedLP[i].toString(), options.decimals)
         ) *
