52b7ca5fd2468cd09e5042d362d3a9b5da328b76
Chaitanya <yourchaitu@gmail.com>
2024-06-02 22:29:00 +0530

    Update src/strategies/index.ts

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -867,8 +867,8 @@ const strategies = {
   'quickswap-v3': quickswapv3,
   'balance-of-with-bazaar-batch-auction-linear-vesting-power':
     balanceOfWithBazaarBatchAuctionLinearVestingPower,
-  'staking-balance-of-v1': stakingBalanceOfV1
-  'staking-balance-of-v2': stakingBalanceOfV2,
+  'staking-balance-of-v1': stakingBalanceOfV1,
+  'staking-balance-of-v2': stakingBalanceOfV2
 };
 
 Object.keys(strategies).forEach(function (strategyName) {
