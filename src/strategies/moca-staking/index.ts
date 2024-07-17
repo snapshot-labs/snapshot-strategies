f61cb953f04aed17de9c79dc306db61b3c0462ed
m1ngshum <140998506+m1ngshum@users.noreply.github.com>
2024-07-17 19:34:04 +0800

    fix: author name

--- src/strategies/moca-staking/index.ts
+++ src/strategies/moca-staking/index.ts
@@ -1,7 +1,7 @@
 import { formatUnits } from '@ethersproject/units';
 import { multicall } from '../../utils';
 
-export const author = 'm1ngshum';
+export const author = 'mocaverse';
 export const version = '0.1.0';
 
 const abi = [
