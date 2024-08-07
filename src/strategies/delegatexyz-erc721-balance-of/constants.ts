f927c99a1a77fd7c2fe5f0c3f57795b2489f73a9
0xAlchemist <0xalchemist@proton.me>
2024-08-07 16:03:36 -0700

    avoid exploiting voting power by restricting to latest delegation

--- src/strategies/delegatexyz-erc721-balance-of/constants.ts
+++ src/strategies/delegatexyz-erc721-balance-of/constants.ts
@@ -1,10 +1,6 @@
 export const abi = [
-<<<<<<< HEAD
   'function getIncomingDelegations(address to) view returns (tuple(uint8 type_, address to, address from, bytes32 rights, address contract_, uint256 tokenId, uint256 amount)[] delegations_)',
   'function getOutgoingDelegations(address from) view returns (tuple(uint8 type_, address to, address from, bytes32 rights, address contract_, uint256 tokenId, uint256 amount)[] delegations_)'
-=======
-  'function getIncomingDelegations(address to) view returns (tuple(uint8 type_, address to, address from, bytes32 rights, address contract_, uint256 tokenId, uint256 amount)[] delegations_)'
->>>>>>> aa4be45 (Update src/strategies/delegatexyz-erc721-balance-of/constants.ts)
 ];
 
 export const delegatexyzV2ContractAddress =
--- src/strategies/delegatexyz-erc721-balance-of/index.ts
+++ src/strategies/delegatexyz-erc721-balance-of/index.ts
@@ -12,11 +12,7 @@ import {
 } from './types';
 
 export const author = 'apesplus';
-<<<<<<< HEAD
 export const version = '0.1.1';
-=======
-export const version = '0.1.0';
->>>>>>> e199f87 (cleanup)
 
 export async function strategy(
   space,
