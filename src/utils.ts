b89f3d9a55eb3d25c0f0cb6c58a34c4c4c1f808e
Chaitanya <yourchaitu@gmail.com>
2024-10-07 00:52:34 +0530

    fix: Abort fetch in customFetch on timeout (#1601)

--- src/utils.ts
+++ src/utils.ts
@@ -72,10 +72,21 @@ export function customFetch(
   options: RequestInit = {},
   timeout = 20000
 ): Promise<any> {
+  const controller = new AbortController();
+  const { signal } = controller;
+  const fetchOptions = { ...options, signal };
+
   return Promise.race([
-    fetch(url, options),
-    new Promise((_, reject) =>
-      setTimeout(() => reject(new Error('API request timeout')), timeout)
+    fetch(url, fetchOptions).catch((error) => {
+      if (error.name === 'AbortError') {
+        throw new Error('API request timeout');
+      }
+      throw error;
+    }),
+    new Promise(() =>
+      setTimeout(() => {
+        controller.abort();
+      }, timeout)
     )
   ]);
 }
