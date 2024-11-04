256716e72ba445713105203367ec29ee6edd5108
johnnyonline <johnnyonline.eth@gmail.com>
2024-11-04 16:32:06 +0900

    feat: add token-locker-weight strategy

---
+++ src/strategies/token-locker-weight/README.md
@@ -0,0 +1,13 @@
+# ve-asf
+
+This strategy returns the veASF balance in token locker of the voters (it actually returns the weight)
+
+Here is an example of parameters:
+
+```json
+{
+  "address": "0xF119B5Aa93a7755b09952B3a88D04cdAf5329034",
+  "symbol": "veASF",
+  "decimals": 1
+}
+```
---
+++ src/strategies/token-locker-weight/index.ts
@@ -0,0 +1,36 @@
+import { BigNumberish } from '@ethersproject/bignumber';
+import { formatUnits } from '@ethersproject/units';
+import { Multicaller } from '../../utils';
+
+export const author = 'johnnyonline';
+export const version = '0.1.1';
+
+const TOKEN_LOCKER = '0xF119B5Aa93a7755b09952B3a88D04cdAf5329034';
+
+const abi = [
+  'function getAccountWeight(address account) external view returns (uint256)'
+];
+
+export async function strategy(
+  space,
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+): Promise<Record<string, number>> {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+
+  const multi = new Multicaller(network, provider, abi, { blockTag });
+  addresses.forEach((address) =>
+    multi.call(address, TOKEN_LOCKER, 'getAccountWeight', [address])
+  );
+  const result: Record<string, BigNumberish> = await multi.execute();
+
+  return Object.fromEntries(
+    Object.entries(result).map(([address, balance]) => [
+      address,
+      parseFloat(formatUnits(balance, options.decimals))
+    ])
+  );
+}
