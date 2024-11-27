884a34f17c8b2e2da531d876efa79f1f1f32f80e
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2024-11-27 20:28:21 +0100

    feat: update strategy

--- src/strategies/morpho-delegation/index.ts
+++ src/strategies/morpho-delegation/index.ts
@@ -1,12 +1,14 @@
-import { BigNumberish } from '@ethersproject/bignumber';
 import { formatUnits } from '@ethersproject/units';
 import { Multicaller } from '../../utils';
+import { ADDRESS_ZERO } from '@uniswap/v3-sdk';
 
 export const author = 'Jean-Grimal';
 export const version = '0.1.1';
 
 const abi = [
-  'function delegatedVotingPower(address account) external view returns (uint256)'
+  'function delegatedVotingPower(address account) external view returns (uint256)',
+  'function delegatee(address delegatee) public view returns (uint256)',
+  'function balanceOf(address account) external view returns (uint256)'
 ];
 
 export async function strategy(
@@ -19,16 +21,40 @@ export async function strategy(
 ): Promise<Record<string, number>> {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
-  const multi = new Multicaller(network, provider, abi, { blockTag });
-  addresses.forEach((address) =>
-    multi.call(address, options.address, 'delegatedVotingPower', [address])
-  );
-  const result: Record<string, BigNumberish> = await multi.execute();
+  const delegatedVotingPowerMulti = new Multicaller(network, provider, abi, {
+    blockTag
+  });
+  const delegateeMulti = new Multicaller(network, provider, abi, { blockTag });
+  const balanceOfMulti = new Multicaller(network, provider, abi, { blockTag });
+  addresses.forEach((address) => {
+    delegatedVotingPowerMulti.call(
+      address,
+      options.address,
+      'delegatedVotingPower',
+      [address]
+    );
+    delegateeMulti.call(address, options.address, 'delegatee', [address]);
+    balanceOfMulti.call(address, options.address, 'balanceOf', [address]);
+  });
+  const [delegatedVotingPowerResult, delegateeResult, balanceOfResult] =
+    await Promise.all([
+      delegatedVotingPowerMulti.execute(),
+      delegateeMulti.execute(),
+      balanceOfMulti.execute()
+    ]);
 
   return Object.fromEntries(
-    Object.entries(result).map(([address, delegatedVotingPower]) => [
+    Object.entries(delegatedVotingPowerResult).map(([address, votingData]) => [
       address,
-      parseFloat(formatUnits(delegatedVotingPower, options.decimals))
+      parseFloat(
+        formatUnits(
+          delegatedVotingPowerResult[address] +
+            (delegateeResult[address].delegatee === ADDRESS_ZERO
+              ? balanceOfResult[address].balanceOf
+              : 0n),
+          options.decimals
+        )
+      )
     ])
   );
 }
