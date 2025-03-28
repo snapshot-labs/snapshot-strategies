e1bbaac82725641886aeeb2584b53b0bd609ad81
Adrien Michaud <62593462+CubeLeopard5@users.noreply.github.com>
2025-03-28 05:30:39 +0100

    [nedao] Update strategy nedao (#1709)
    * add nedao strategy
    
    * update nedao strategy to add a new parameter proposalsOnly
    
    * update nedao strategy to add a new parameter proposalsOnly, add it to the required parameters
    
    * correct author name and upgrade version of neado strategy
    
    ---------
    
    Co-authored-by: Cubeleopard5 <dev@arcantel.ch>

--- src/strategies/nedao/README.md
+++ src/strategies/nedao/README.md
@@ -5,6 +5,7 @@
   Here is an example of parameters:
   ```json
   {
+    "proposalsOnly": false,
     "contractAddress": "0xB94E35600A925569ea2f4DC57D571657ccDb8FDc",
     "validateur": "0x7aaAd3167Ad03809aC61e667bdBC72285CEe3BF4",
     "typeValidation": "0xd36f63907c7fad13dd9dc69aa8b9af9c3296a3edad0b0b1eb50c10fd066fc910"
--- src/strategies/nedao/index.ts
+++ src/strategies/nedao/index.ts
@@ -1,8 +1,8 @@
 import { BigNumber } from '@ethersproject/bignumber';
 import { Multicaller } from '../../utils';
 
-export const author = 'nedao';
-export const version = '0.1.0';
+export const author = 'arcantel-sa';
+export const version = '0.1.1';
 
 const abi = [
   'function litProclamation(address validateur,address sujet, bytes32 typeValidation) public view returns (bytes32) '
@@ -10,7 +10,7 @@ const abi = [
 
 function bytes32ToBool(bytes32) {
   const hex = bytes32.startsWith('0x') ? bytes32.slice(2) : bytes32;
-  return hex != 0 ? 1 : 0;
+  return (hex != 0) ? 1 : 0;
 }
 
 export async function strategy(
@@ -24,6 +24,8 @@ export async function strategy(
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
   const multi = new Multicaller(network, provider, abi, { blockTag });
 
+  if (options.proposalsOnly === true) return Object.fromEntries(addresses.map((address) => [address, 0])); // return 0 for all addresses to prevent votes on proposals
+
   addresses.forEach((address) =>
     multi.call(address, options.contractAddress, 'litProclamation', [
       options.validateur,
@@ -34,6 +36,9 @@ export async function strategy(
   const result: Record<string, BigNumber> = await multi.execute();
 
   return Object.fromEntries(
-    Object.entries(result).map(([address, id]) => [address, bytes32ToBool(id)])
+    Object.entries(result).map(([address, id]) => [
+      address,
+      bytes32ToBool(id),
+    ])
   );
 }
