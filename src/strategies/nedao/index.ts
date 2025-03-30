8f53785faea3dbd01f9fc868e1012aef868c797c
ChaituVR <ChaituVR@users.noreply.github.com>
2025-03-30 10:01:31 +0000

    Automated lint

--- src/strategies/nedao/index.ts
+++ src/strategies/nedao/index.ts
@@ -10,7 +10,7 @@ const abi = [
 
 function bytes32ToBool(bytes32) {
   const hex = bytes32.startsWith('0x') ? bytes32.slice(2) : bytes32;
-  return (hex != 0) ? 1 : 0;
+  return hex != 0 ? 1 : 0;
 }
 
 export async function strategy(
@@ -24,7 +24,8 @@ export async function strategy(
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
   const multi = new Multicaller(network, provider, abi, { blockTag });
 
-  if (options.proposalsOnly === true) return Object.fromEntries(addresses.map((address) => [address, 0])); // return 0 for all addresses to prevent votes on proposals
+  if (options.proposalsOnly === true)
+    return Object.fromEntries(addresses.map((address) => [address, 0])); // return 0 for all addresses to prevent votes on proposals
 
   addresses.forEach((address) =>
     multi.call(address, options.contractAddress, 'litProclamation', [
@@ -36,9 +37,6 @@ export async function strategy(
   const result: Record<string, BigNumber> = await multi.execute();
 
   return Object.fromEntries(
-    Object.entries(result).map(([address, id]) => [
-      address,
-      bytes32ToBool(id),
-    ])
+    Object.entries(result).map(([address, id]) => [address, bytes32ToBool(id)])
   );
 }
