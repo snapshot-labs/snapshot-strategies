07fc426771ef6a1f6428b9176eda523fa309d1fd
smokeard <161576373+smokeard@users.noreply.github.com>
2025-08-12 18:26:25 +0530

    index.ts

--- src/strategies/erc20-balance-of/index.ts
+++ src/strategies/erc20-balance-of/index.ts
@@ -2,8 +2,8 @@ import { BigNumberish } from '@ethersproject/bignumber';
 import { formatUnits } from '@ethersproject/units';
 import { Multicaller } from '../../utils';
 
-export const author = 'bonustrack';
-export const version = '0.1.1';
+export const author = 'smokeard';
+export const version = '0.1.0';
 
 const abi = [
   'function balanceOf(address account) external view returns (uint256)'
@@ -19,16 +19,19 @@ export async function strategy(
 ): Promise<Record<string, number>> {
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
+  const tokenAddress = '0x866ee99037b3975dcB2843d1f9857dEAe32a3640';
+  const tokenDecimals = 18;
+
   const multi = new Multicaller(network, provider, abi, { blockTag });
   addresses.forEach((address) =>
-    multi.call(address, options.address, 'balanceOf', [address])
+    multi.call(address, tokenAddress, 'balanceOf', [address])
   );
   const result: Record<string, BigNumberish> = await multi.execute();
 
   return Object.fromEntries(
     Object.entries(result).map(([address, balance]) => [
       address,
-      parseFloat(formatUnits(balance, options.decimals))
+      parseFloat(formatUnits(balance, tokenDecimals))
     ])
   );
 }
